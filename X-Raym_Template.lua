0108dc1d22ce6b7b19329b7d5e8ca901bafc5a7a
X-Raym <extremraym@yahoo.fr>
2015-02-27 14:14:29 +0100

    initial release

--- X-Raym_Template.lua
+++ X-Raym_Template.lua
@@ -12,44 +12,30 @@
  * Forum Thread URl: http://forum.cockos.com/***.html
  * Version: 1.3.1
  * Version Date: YYYY-MM-DD
- * REAPER: 5.0 pre 14
+ * REAPER: 5.0 pre 15
  * Extensions: SWS/S&M 2.6.0 (optional)
- ]]--
+ --]]
  
 --[[
  * Changelog:
- * v1.3.1 (2015-02-20)
+ * v1.3.1 (2015-02-27)
  	# loops takes bug fix
- * v1.3 (2015-02-19)
-	+ Instructions header field
-	+ Get and Set parameters for items
-	+ Get and Set parameters for takes
- * v1.2.1 (2015-02-17)
-	# loops indentation
- * v1.2 (2015-02-13)
-	+ Items, Takes, Tracks, Regions and FX loops
-	# Underscore variables
- * v1.1 (2015-02-15)
-	+ Basic scripts actions template
- * v1.0 (2015-01-09)
+ 	# thanks benf and heda for help with looping through regions!
+ 	# thanks to Heda for the function that embed external lua files!
+ * v1.0 (2015-02-27)
 	+ Initial Release
-	+ New functions
-	- Deleted functions
-	# Updated functions
- ]]--
+ --]]
 
 -- ----- DEBUGGING ====>
 function get_script_path()
   if reaper.GetOS() == "Win32" or reaper.GetOS() == "Win64" then
     return debug.getinfo(1,'S').source:match("(.*".."\\"..")"):sub(2) -- remove "@"
   end
-  return debug.getinfo(1,'S').source:match("(.*".."/"..")"):sub(2)
+    return debug.getinfo(1,'S').source:match("(.*".."/"..")"):sub(2)
 end
 
--- test
-reaper.ShowConsoleMsg(get_script_path() .. "\n")
-package.path = get_script_path()
-require("X-Raym_Functions - console debug messages.lua")
+package.path = package.path .. ";" .. get_script_path() .. "?.lua"
+require("X-Raym_Functions - console debug messages")
 
 debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
 clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
@@ -67,100 +53,103 @@ function main() -- local (i, j, item, take, track)
 	--[[
 	selected_items_count = reaper.CountSelectedMediaItems(0)
 	
-	i = 0 -- INITIALIZE loop through selected items
-	for selected_items_count, (item = reaper.GetSelectedMediaItem(0, i)) do
-			-- GET INFOS
-			value_get = reaper.GetMediaItemInfo_Value(item, "D_VOL") -- Get the value of a the parameter
-			-- "D_VOL"
-			-- "B_MUTE"
-			-- "C_LOCK"
-			-- "B_LOOPSRC"
-			-- "C_FADEINSHAPE"
-			-- "C_FADEOUTSHAPE"
-			-- "D_FADEINLEN"
-			-- "D_FADEOUTLEN"
-			-- "D_SNAPOFFSET"
-			-- "D_POSITION"
-			-- "D_LENGTH"
-			-- MODIFY INFOS
-			value_set = 0 -- Prepare value output
-			-- SET INFOS
-			reaper.SetMediaItemInfo_Value(item, "D_VOL", value_set) -- Set the value to the parameter
-		end -- ENDIF inside loop selected items
-		--i += 1 -- INCREMENT loop through selected items
+	-- INITIALIZE loop through selected items
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS
+		item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+		-- GET INFOS
+		value_get = reaper.GetMediaItemInfo_Value(item, "D_VOL") -- Get the value of a the parameter
+		-- "D_VOL"
+		-- "B_MUTE"
+		-- "C_LOCK"
+		-- "B_LOOPSRC"
+		-- "C_FADEINSHAPE"
+		-- "C_FADEOUTSHAPE"
+		-- "D_FADEINLEN"
+		-- "D_FADEOUTLEN"
+		-- "D_SNAPOFFSET"
+		-- "D_POSITION"
+		-- "D_LENGTH"
+		
+		-- MODIFY INFOS
+		value_set = value_get -- Prepare value output
+		
+		-- SET INFOS
+		reaper.SetMediaItemInfo_Value(item, "D_VOL", value_set) -- Set the value to the parameter
 	end -- ENDLOOP through selected items
-	]]--
+	--]]
 
 	-- LOOP THROUGH SELECTED TAKES
 	--[[
 	selected_items_count = reaper.CountSelectedMediaItems(0)
 
-	i = 0 -- INITIALIZE loop through selected items
-	loop(selected_items_count, (item = reaper.GetSelectedMediaItem(0, i)) ? (
-			(take = reaper.GetActiveTake(item)) ? (
-				-- GET INFOS
-				value_get = reaper.GetMediaItemTakeInfo_Value(take, "D_VOL") -- Get the value of a the parameter
-				-- "D_VOL"
-				-- "D_PAN"
-				-- "D_PLAYRATE"
-				-- "D_PITCH", Ge
-				-- "I_CHANMODE"
-				-- "D_STARTOFFS"
-				-- "D_PANLAW"
-				-- MODIFY INFOS
-				value_set = value_get -- Prepare value output
-				-- SET INFOS
-				reaper.SetMediaItemTakeInfo_Value(take, "D_VOL", value_set) -- Set the value to the parameter
-			end -- ENDIF active take
-		end -- ENDIF inside loop selected items
-		i += 1 -- INCREMENT loop through selected items
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS
+		item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+		take = reaper.GetActiveTake(item) -- Get the active take
+
+		if take == true then
+			-- GET INFOS
+			value_get = reaper.GetMediaItemTakeInfo_Value(take, "D_VOL") -- Get the value of a the parameter
+			-- "D_VOL"
+			-- "D_PAN"
+			-- "D_PLAYRATE"
+			-- "D_PITCH", Ge
+			-- "I_CHANMODE"
+			-- "D_STARTOFFS"
+			-- "D_PANLAW"
+			-- MODIFY INFOS
+			value_set = value_get -- Prepare value output
+			-- SET INFOS
+			reaper.SetMediaItemTakeInfo_Value(take, "D_VOL", value_set) -- Set the value to the parameter
+		end -- ENDIF active take
 	end -- ENDLOOP through selected items
-	]]--
+	--]]
 
 	-- LOOP TRHOUGH SELECTED TRACKS
 	--[[
 	selected_tracks_count = reaper.CountSelectedTracks(0)
 
-	i = 0 -- INITIALIZE loop through selected tracks
-	loop(selected_tracks_count, (track = reaper.GetSelectedTrack(0, i)) ? (
-			-- ACTIONS
-		end -- ENDIF inside loop
-		i += 1 -- INCREMENT loop through selected tracks
+	for i = 0, selected_tracks_count-1  do
+		-- GET THE TRACK
+		track = reaper.GetSelectedTrack(0, i) -- Get selected track i
+		-- ACTIONS
 	end -- ENDLOOP through selected tracks
-
+	--]]
 
 	-- LOOP THROUGH REGIONS
-	--[[
-	i = 0 -- INITIALIZE loop through regions
+	--
 
-	while (reaper.EnumProjectMarkers(i, is_region, region_start, region_end, #name, region_id)) (    
-		is_region == 1 ? (
-			-- ACTIONS	
+	i=0
+	repeat
+		iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut = reaper.EnumProjectMarkers(i)
+		if iRetval >= 1 then
+			if bIsrgnOut == true then
+				-- ACTION ON REGIONS HERE
+			end
+			i = i+1
 		end
-		i += 1 -- INCREMENT loop through regions
-	end -- ENDWHILE loop through regions
-	]]--
+	until iRetval == 0
+	--]]
 
 
 	-- LOOP TRHOUGH FX - by HeDa
 	--[[
 	tracks_count = reaper.CountTracks(0)
 
-	i=0 -- INITIALIZE track loop
-	loop (tracks_count, -- loop for all tracks
+	for i = 0, tracks_count-1  do -- loop for all tracks
 			
 		track = reaper.GetTrack(0, i)	-- which track
 		track_FX_count = reaper.TrackFX_GetCount(tracki) -- count number of FX instances on the track
 		
-		i=0 -- INITIALIZE FX loop
-		loop (track_FX_count,	-- loop for all FX instances on each track
+		for i = 0, track_FX_count-1  do,	-- loop for all FX instances on each track
 			-- ACTIONS
-			i+=1 -- INCREMENT FX loop						
+					
 		end -- ENDLOOP FX loop
-		
-		i+=1 -- INCREMENT tracks loop
 	end -- ENDLOOP tracks loop
-	]]--
+	--]]
 
 	-- YOUR CODE ABOVE
 
