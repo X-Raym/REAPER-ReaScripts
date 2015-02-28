16a8294db6c38caa3cfc0f6fdca6892df19859a0
X-Raym <extremraym@yahoo.fr>
2015-02-28 14:04:11 +0100

    -color, -initial loop, -note

---
+++ X-Raym_beta_Create text items on selected track from selected takes.lua
@@ -0,0 +1,156 @@
+--[[
+ * ReaScript Name: Create text items on selected track from selected takes
+ * Description: A template script for REAPER ReaScript.
+ * Instructions: Here is how to use it. (optional)
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: Script: Script name
+ * Forum Thread URl: http://forum.cockos.com/***.html
+ * Version: 1.0
+ * Version Date: YYYY-MM-DD
+ * REAPER: 5.0 pre 15
+ * Extensions: SWS/S&M 2.6.0 (optional)
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-02-28)
+	+ Initial Release
+ --]]
+
+-- ----- DEBUGGING ====>
+function get_script_path()
+  if reaper.GetOS() == "Win32" or reaper.GetOS() == "Win64" then
+    return debug.getinfo(1,'S').source:match("(.*".."\\"..")"):sub(2) -- remove "@"
+  end
+    return debug.getinfo(1,'S').source:match("(.*".."/"..")"):sub(2)
+end
+
+package.path = package.path .. ";" .. get_script_path() .. "?.lua"
+require("X-Raym_Functions - console debug messages")
+
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+msg_clean()
+-- <==== DEBUGGING -----
+
+-- From Heda's HeDa_SRT to text items.lua ====>
+
+dbug_flag = 0 -- set to 0 for no debugging messages, 1 to get them
+function dbug (text) 
+	if dbug_flag==1 then  
+		if text then
+			reaper.ShowConsoleMsg(text .. '\n')
+		else
+			reaper.ShowConsoleMsg("nil")
+		end
+	end
+end
+
+function CreateTextItem(starttime, endtime, notetext) 
+	--ref: Lua: number startOut retval, number endOut reaper.GetSet_LoopTimeRange(boolean isSet, boolean isLoop, number startOut, number endOut, boolean allowautoseek)
+	reaper.GetSet_LoopTimeRange(1,0,starttime,endtime,0) -- define the time range for the empty item
+	--ref: Lua: reaper.Main_OnCommand(integer command, integer flag)
+	reaper.Main_OnCommand(40142,0) -- insert empty item
+	--ref: Lua: MediaItem reaper.GetSelectedMediaItem(ReaProject proj, integer selitem)
+	item = reaper.GetSelectedMediaItem(0,0) -- get the selected item
+	HeDaSetNote(item, notetext) -- set the note add | character to the beginning of each line. only 1 line for now.
+	reaper.SetEditCurPos(endtime, 1, 0) -- moves cursor for next item
+end
+
+function HeDaSetNote(item,newnote)  -- HeDa - SetNote v1.0
+	--ref: Lua: boolean retval, string str reaper.GetSetItemState(MediaItem item, string str)
+	retval, s = reaper.GetSetItemState(item, "")	-- get the current item's chunk
+	--dbug("\nChunk=" .. s .. "\n")
+	has_notes = s:find("<NOTES")  -- has notes?
+	if has_notes then
+		-- there are notes already
+		chunk, note, chunk2 = s:match("(.*<NOTES\n)(.*)(\n>\nIMGRESOURCEFLAGS.*)")
+		newchunk = chunk .. newnote .. chunk2
+		dbug(newchunk .. "\n")
+		
+	else
+		--there are still no notes
+		chunk,chunk2 = s:match("(.*IID%s%d+)(.*)")
+		newchunk = chunk .. "\n<NOTES\n" .. newnote .. "\n>\nIMGRESOURCEFLAGS 0" .. chunk2
+		dbug(newchunk .. "\n")
+	end
+	reaper.GetSetItemState(item, newchunk)	-- set the new chunk with the note
+end
+
+-- <==== From Heda's HeDa_SRT to text items.lua
+
+-- TABLE INIT
+local setSelectedMediaItem = {}
+
+-- MAIN
+function main()
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	selected_track = reaper.GetSelectedTrack(0, 0);
+
+	-- COUNT SELECTED ITEMS
+	selected_items_count = reaper.CountSelectedMediaItems(0)
+
+	-- SAVE TAKES SELECTION
+	for j = 0, selected_items_count-1  do
+		setSelectedMediaItem[j] = reaper.GetSelectedMediaItem(0, j)
+	end
+
+	-- LOOP THROUGH TAKE SELECTION
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS AND TAKES AND PARENT TRACK
+		item = setSelectedMediaItem[i] -- Get selected item i
+		take = reaper.GetActiveTake(item) -- Get the active take
+		track = reaper.GetMediaItemTake_Track(take)
+		
+		-- GET INFOS
+
+		-- NAME
+		take_name = reaper.GetTakeName(take)
+		
+		-- COLOR
+		takeColor = reaper.GetMediaItemTakeInfo_Value(take, "I_CUSTOMCOLOR")
+		if takeColor == 0 then -- if the item has no color...
+			takeColor = reaper.GetTrackColor(track) -- ... then take the track color
+		end
+			
+		-- TIMES
+		itemStart = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+		itemDuration = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+		itemEnd = itemStart + itemDuration
+
+		-- DEBUG
+		msg_s("itemName")
+		msg_s(take_name)
+		msg_s("itemStart")
+		msg_f(itemStart)
+		msg_s("itemEnd")
+		msg_f(itemEnd)
+
+		-- ACTION
+		CreateTextItem(itemStart, itemEnd, take_name)
+
+	end -- ENDLOOP through selected items
+
+	reaper.Undo_EndBlock("Create text items on selected track from selected takes", 0) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+msg_start() -- Display characters in the console to show you the begining of the script execution.
+
+reaper.PreventUIRefresh(1)
+
+main() -- Execute your main function
+
+reaper.PreventUIRefresh(-1)
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+msg_end() -- Display characters in the console to show you the end of the script execution.
