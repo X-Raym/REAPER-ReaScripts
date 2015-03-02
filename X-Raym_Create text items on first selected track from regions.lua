87fcb924597acffc98b65819c0f604fd04cbdf70
X-Raym <extremraym@yahoo.fr>
2015-03-02 18:13:02 +0100

    initial release

---
+++ X-Raym_Create text items on first selected track from regions.lua
@@ -0,0 +1,131 @@
+--[[
+ * ReaScript Name: Template Title (match file name without extension and author)
+ * Description: A template script for REAPER ReaScript.
+ * Instructions: Here is how to use it. (optional)
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: Script: Script name
+ * Forum Thread URl: http://forum.cockos.com/***.html
+ * Version: 1.3.1
+ * Version Date: YYYY-MM-DD
+ * REAPER: 5.0 pre 15
+ * Extensions: SWS/S&M 2.6.0 (optional)
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.3.1 (2015-02-27)
+ 	# loops takes bug fix
+ 	# thanks benf and heda for help with looping through regions!
+ 	# thanks to Heda for the function that embed external lua files!
+ * v1.0 (2015-02-27)
+	+ Initial Release
+ --]]
+
+-- ----- DEBUGGING ====>
+function get_script_path()
+  if reaper.GetOS() == "Win32" or reaper.GetOS() == "Win64" then
+    return debug.getinfo(1,'S').source:match("(.*".."\\"..")"):sub(2) -- remove "@"
+  end
+    return debug.getinfo(1,'S').source:match("(.*".."/"..")"):sub(2)
+end
+
+package.path = package.path .. ";" .. get_script_path() .. "?.lua"
+require("X-Raym_Functions - console debug messages")
+
+debug = 0 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 0 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+msg_clean()
+-- <==== DEBUGGING -----
+
+-- From Heda's HeDa_SRT to text items.lua ====>
+dbug_flag = 0 -- set to 0 for no debugging messages, 1 to get them
+function dbug (text) 
+	if dbug_flag==1 then  
+		if text then
+			reaper.ShowConsoleMsg(text .. '\n')
+		else
+			reaper.ShowConsoleMsg("nil")
+		end
+	end
+end
+
+function CreateTextItem(starttime, endtime, notetext, color) 
+	--ref: Lua: number startOut retval, number endOut reaper.GetSet_LoopTimeRange(boolean isSet, boolean isLoop, number startOut, number endOut, boolean allowautoseek)
+	reaper.GetSet_LoopTimeRange(1,0,starttime,endtime,0) -- define the time range for the empty item
+	--ref: Lua: reaper.Main_OnCommand(integer command, integer flag)
+	reaper.Main_OnCommand(40142,0) -- insert empty item
+	--ref: Lua: MediaItem reaper.GetSelectedMediaItem(ReaProject proj, integer selitem)
+	item = reaper.GetSelectedMediaItem(0,0) -- get the selected item
+	reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
+
+	HeDaSetNote(item, "|" .. notetext) -- set the note add | character to the beginning of each line. only 1 line for now.
+	reaper.SetEditCurPos(endtime, 1, 0) -- moves cursor for next item
+end
+
+function HeDaSetNote(item,newnote)  -- HeDa - SetNote v1.0
+	--ref: Lua: boolean retval, string str reaper.GetSetItemState(MediaItem item, string str)
+	retval, s = reaper.GetSetItemState(item, "")	-- get the current item's chunk
+	--dbug("\nChunk=" .. s .. "\n")
+	has_notes = s:find("<NOTES")  -- has notes?
+	if has_notes then
+		-- there are notes already
+		chunk, note, chunk2 = s:match("(.*<NOTES\n)(.*)(\n>\nIMGRESOURCEFLAGS.*)")
+		newchunk = chunk .. newnote .. chunk2
+		dbug(newchunk .. "\n")
+		
+	else
+		--there are still no notes
+		chunk,chunk2 = s:match("(.*IID%s%d+)(.*)")
+		newchunk = chunk .. "\n<NOTES\n" .. newnote .. "\n>\nIMGRESOURCEFLAGS 0" .. chunk2
+		dbug(newchunk .. "\n")
+	end
+	reaper.GetSetItemState(item, newchunk)	-- set the new chunk with the note
+end
+-- <==== From Heda's HeDa_SRT to text items.lua
+
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	track = reaper.GetSelectedTrack(0, 0) -- Get selected track i
+
+	-- LOOP THROUGH REGIONS
+	
+	i=0
+	repeat
+		iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut, iColorOut = reaper.EnumProjectMarkers3(0, i)
+		if iRetval >= 1 then
+			if bIsrgnOut == true then
+				CreateTextItem(iPosOut, iRgnendOut, sNameOut, iColorOut)
+			end
+			i = i+1
+		end
+	until iRetval == 0
+	
+
+	reaper.Undo_EndBlock("My action", 0) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+msg_start() -- Display characters in the console to show you the begining of the script execution.
+
+reaper.PreventUIRefresh(1)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_SAVELOOP5"), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_BR_SAVE_CURSOR_POS_SLOT_8"), 0)
+
+main() -- Execute your main function
+
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_SWS_RESTLOOP5"), 0)
+reaper.Main_OnCommand(reaper.NamedCommandLookup("_BR_RESTORE_CURSOR_POS_SLOT_8"), 0)
+reaper.PreventUIRefresh(-1)
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+msg_end() -- Display characters in the console to show you the end of the script execution.
