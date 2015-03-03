13b14b27a8f623681e0e3e658431da7a20a17807
X-Raym <extremraym@yahoo.fr>
2015-03-03 11:20:23 +0100

    loop region color

--- X-Raym_Template.lua
+++ X-Raym_Template.lua
@@ -18,6 +18,8 @@
  
 --[[
  * Changelog:
+ * v1.4.1 (2015-03-03)
+ 	# EnumProjectMarkers3 for regions loop
  * v1.4 (2015-03-02)
  	+ Infos for track, take and items values
  	+ Restore view, loop, edit cursor and UI
@@ -29,8 +31,7 @@
 	+ Initial Release
  --]]
 
--- ----- DEBUGGING ====>
-function get_script_path()
+-- ----- DEBUGGING ====>function get_script_path()
   if reaper.GetOS() == "Win32" or reaper.GetOS() == "Win64" then
     return debug.getinfo(1,'S').source:match("(.*".."\\"..")"):sub(2) -- remove "@"
   end
@@ -39,6 +40,7 @@ end
 
 package.path = package.path .. ";" .. get_script_path() .. "?.lua"
 require("X-Raym_Functions - console debug messages")
+
 
 debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
 clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
@@ -190,7 +192,7 @@ function main() -- local (i, j, item, take, track)
 	--[[
 	i=0
 	repeat
-		iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut = reaper.EnumProjectMarkers(i)
+		iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(0,i)
 		if iRetval >= 1 then
 			if bIsrgnOut == true then
 				-- ACTION ON REGIONS HERE
