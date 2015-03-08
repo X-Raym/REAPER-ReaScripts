5a82cde98b7f1449a729c15ce2b539f2f59e1c74
X-Raym <extremraym@yahoo.fr>
2015-03-08 10:45:14 +0100

    working

--- Text Items and Item Notes/Creation/X-Raym_Merge overlapping similar text items on selected tracks.lua
+++ Text Items and Item Notes/Creation/X-Raym_Merge overlapping similar text items on selected tracks.lua
@@ -11,13 +11,15 @@
  * Forum Thread: Script: Script name
  * Forum Thread URl: http://forum.cockos.com/***.html
  * Version: 1.1
- * Version Date: 2015-03-06
+ * Version Date: 2015-03-08
  * REAPER: 5.0 pre 15
  * Extensions: SWS/S&M 2.6.2
  --]]
  
 --[[
  * Changelog:
+ * v1.2 (2015-03-08)
+	+ Now working fine
  * v1.1 (2015-03-06)
 	+ Multi tracks selection support
  * v1.0 (2015-02-27)
@@ -35,26 +37,18 @@ end
 package.path = package.path .. ";" .. get_script_path() .. "?.lua"
 require("X-Raym_Functions - console debug messages")
 
-debug = 0 -- 0 => No console. 1 => Display console messages for debugging.
-clean = 0 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
 
 msg_clean()
 ]]-- <==== DEBUGGING -----
-
---INIT
-item_mark_as_delete = {}
-
-function main() -- local (i, j, item, take, track)
-
-	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
-
+function reset()
 	B_item_text = ""
 	B_item_start = 0
 	B_item_end = 0
 	B_item_color = 0
 	merge_items = 0
-	item_start = 0
-	B_track = 0
+	first_item_start = 0
 	first = true
 	item_mark_as_delete_lenght = 0
 	group_id = 1
@@ -62,13 +56,23 @@ function main() -- local (i, j, item, take, track)
 	B_group = 0
 	item_end = 0
 	j = 0
+end
+--INIT
+item_mark_as_delete = {}
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
 
 	selected_tracks_count = reaper.CountSelectedTracks(0)
 
 	if selected_tracks_count > 0 then
 
 		for l = 0, selected_tracks_count-1  do
-		-- GET THE TRACK
+			
+			reset()
+
+			-- GET THE TRACK
 			track = reaper.GetSelectedTrack(0, l)
 			track_idx = reaper.GetNumTracks()
 			
@@ -96,52 +100,55 @@ function main() -- local (i, j, item, take, track)
 					--msg_tvoldi("A_item_end =", A_item_end, "%f", 0, debug, 1)
 					A_item_text = reaper.ULT_GetMediaItemNote(A_item)
 					A_item_color = reaper.GetDisplayedMediaItemColor(A_item)
-					A_track = reaper.GetMediaItem_Track(A_item)
 
-					if i == 0 then --If first item in the loop
-						first_item = A_item
-						item_start = A_item_start
+					if first == true then --If first item in the loop
 						first = false -- le prochain item sera pas first
 					end
 					
-					if A_track == B_track and A_item_text == B_item_text and A_item_start < B_item_end and A_item_color == B_item_color then -- Compare the name, the start-end, and the color of the current item and the previous one
-						if first == true then -- If it is the first item of a group
-							--msg_s("First")
-							first_item = B_item
+					if first == false and A_item_text == B_item_text and A_item_start < B_item_end and A_item_color == B_item_color then -- Compare the name, the start-end, and the color of the current item and the previous one
 
-							item_start = B_item_start
-							--msg_tvoldi("item_start = ", item_start, "%f", 1, debug, 1)
-							item_mark_as_delete_lenght = item_mark_as_delete_lenght + 1
-							j = j + 1
-							item_mark_as_delete[j] = A_item
-							first = false -- le prochain item sera pas first
-						else -- SAME GROUP but not first
-
-							item_mark_as_delete_lenght = item_mark_as_delete_lenght + 1
-							j = j + 1
-							item_mark_as_delete[j] = A_item
-							
-						end -- END of check if first or no
+						item_mark_as_delete_lenght = item_mark_as_delete_lenght + 1
+						j = j + 1
+						item_mark_as_delete[j] = A_item
 						
 						if B_item_end > A_item_end then -- If the item is included inside the previous one
 								A_item_end = B_item_end -- then consider that the end of the actual item is the end of previous one
 						end
 
-						group_id = group_id -- keep the same group (not required but it helps)
+						if i == media_item_on_track-1 then -- If actual item is the last of the loop
+							first_item_length = A_item_end - first_item_start
+							--msg_tvoldi("item_length", A_item_end, "%f", 1, debug, 1)
+							reaper.SetMediaItemInfo_Value(first_item, "D_LENGTH", first_item_length)
+						end
 
 					else -- DIFFERENT GROUP
-						
-						if i > 0 then -- IF not the first item in the loop
 							
-							item_length = B_item_end - item_start
-							--msg_tvoldi("item_length", A_item_end, "%f", 1, debug, 1)
-							item_end = item_start + item_length
-							--msg_tvoldi("item_length", item_end, "%f", 1, debug, 1)
-							reaper.SetMediaItemInfo_Value(first_item, "D_LENGTH", item_length)
+						if i > 0 and first == false then -- Si ce n'était pa sle premier item
+
+							first_item_length = B_item_end - first_item_start
+							--msg_tvoldi("B_item_end = ", B_item_end, "%f", 0, debug, 1)
+							--msg_tvoldi("first_item_start = ", first_item_start, "%f", 0, debug, 1)
+
+							--msg_tvoldi("first_item_length", A_item_end, "%f", 1, debug, 1)
+							
+							--msg_tvoldi("first_item_length", item_end, "%f", 1, debug, 1)
+							
+							if i == media_item_on_track-1 then -- If actual item is the last of the loop
+							
+								first_item_length = B_item_end - first_item_start
+								--msg_tvoldi("first_item_length", A_item_end, "%f", 1, debug, 1)
+							
+							end
+
+							reaper.SetMediaItemInfo_Value(first_item, "D_LENGTH", first_item_length)
 							group_id = group_id + 1
 							
-							first = true
 						end
+
+						first = true
+						first_item = A_item
+						first_item_start = A_item_start
+						--msg_tvoldi("first_item_start = ", first_item_start, "%f", 0, debug, 1)
 
 					end
 
@@ -154,18 +161,7 @@ function main() -- local (i, j, item, take, track)
 					B_item_end = A_item_end
 					B_item_text = A_item_text
 					B_item_color = A_item_color
-					B_track = A_track
 					B_group = group_id
-					item_end = B_item_end
-
-					if i == media_item_on_track-1 then -- If actual item is the last of the loop
-							item_length = B_item_end - item_start
-							--msg_tvoldi("item_length", A_item_end, "%f", 1, debug, 1)
-							item_end = item_start + item_length
-							--msg_tvoldi("item_length", item_end, "%f", 1, debug, 1)
-							reaper.SetMediaItemInfo_Value(first_item, "D_LENGTH", item_length)
-							group_id = group_id + 1
-					end
 
 					end -- END IF a text item
 
@@ -177,7 +173,7 @@ function main() -- local (i, j, item, take, track)
 					reaper.DeleteTrackMediaItem(track, item_mark_as_delete[j]) --track is always A
 				end
 
-				reaper.Undo_EndBlock("Merge overlapping similar text items on first selected track", 0) -- End of the undo block. Leave it at the bottom of your main function.
+				reaper.Undo_EndBlock("Merge overlapping similar text items on selected tracks", 0) -- End of the undo block. Leave it at the bottom of your main function.
 			
 			else -- no selected item
 				reaper.ShowMessageBox("No item on track " .. track_idx,"Warning",0)
@@ -191,10 +187,12 @@ end -- of main
 
 --msg_start() -- Display characters in the console to show you the begining of the script execution.
 
+reaper.PreventUIRefresh(1)
+
 main() -- Execute your main function
+
+reaper.PreventUIRefresh(-1)
 
 reaper.UpdateArrange() -- Update the arrangement (often needed)
 
 --msg_end() -- Display characters in the console to show you the end of the script execution.
-
--- idea make it work for each selected track
