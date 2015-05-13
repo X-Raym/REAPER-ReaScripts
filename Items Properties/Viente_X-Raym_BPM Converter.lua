97dbc8a162ccfb88e196d7ef78359090fbc6b5ae
X-Raym <extremraym@yahoo.fr>
2015-05-13 23:13:16 +0200

    bug fix
    corrected tempo detection

--- Items Properties/Viente_X-Raym_BPM Converter.lua
+++ Items Properties/Viente_X-Raym_BPM Converter.lua
@@ -6,6 +6,7 @@
 -- http://forum.cockos.com/showthread.php?t=110780
 
 -- Mod 1.06 by X-Raym
+-- 2015-05-13
 -- http://extremraym.com
 -- # Now in Lua
 -- + Works on multiple selected items (BPM detection based on first selected item)
@@ -28,38 +29,30 @@ function getMusicalLenght(selItem)
 	itemStart = reaper.GetMediaItemInfo_Value(selItem, "D_POSITION")
 	itemEnd = itemStart + reaper.GetMediaItemInfo_Value(selItem, "D_LENGTH")
 
-	--msg("itemStart:" .. itemStart)
-	--msg("itemEnd:" .. itemEnd)
-
 	-- Check if there are any tempo changes during item's length (NextChangeTime will output -1 if there are no more tempo markers after the requested position)
 	if reaper.TimeMap2_GetNextChangeTime(0, itemStart) > itemEnd or reaper.TimeMap2_GetNextChangeTime(0, itemStart) == -1 then 
 		
 		-- There are no tempo markers over selected item so we continue
-		--timeSig = reaper.TimeMap_GetTimeSigAtTime(0, itemStart, 0, 0, 0)
 		timeSig, timesig_denomOut, startBPM = reaper.TimeMap_GetTimeSigAtTime(0, itemStart)
-		--msg("startBPM:" .. startBPM)
+
 		-- This is needed because then we don't have to worry about linear/square difference of tempo points
-		--endBPM = reaper.TimeMap_GetTimeSigAtTime(0, itemStart, 0, 0, 0) [4]
 		timeSig, timesig_denomOut, endBPM = reaper.TimeMap_GetTimeSigAtTime(0, itemEnd) -- we don't use TimeMap_GetDividedBpmAtTime because note in the API says: get the effective BPM at the time (seconds) position (i.e. 2x in /8 signatures) 
-		--msg("endBPM:" .. endBPM)
+		
 		-- This is the formula
-		--global musicalLenght
 		musicalLenght = ((startBPM + endBPM) * timesig_denomOut * (itemEnd - itemStart)) / (480 * timeSig)
-		--msg("musicalLenght:" .. musicalLenght)
+
 	else
 		-- if there are existing tempo markers during item's length, you have to get them and calculate everything separately
 		reaper.Main_OnCommand(65535, 0)
+
 	end
 end
 
 function getTempo()
 
-	--global tempo
-	--global bpm
-
 	timeSel = reaper.GetMediaItemInfo_Value(selItem, "D_LENGTH")
 	rawTempo = 4*60/timeSel
-	tempo = math.floor(rawTempo, 3)
+	tempo = math.floor(rawTempo+0.5, 2)
 
 	if musicalLenght < 1 or musicalLenght == 1 then
 
@@ -107,26 +100,16 @@ end	--reaper.ShowConsoleMsg(str(bpm) + "\n")
 function setTempo()
 
 	master_tempo = math.floor(reaper.Master_GetTempo())
-	--[[
-	names = "Original Tempo (BPM),Target Tempo (BPM)"
-	functionaults = "%d,%d" % (bpm, master_tempo)
-	user_tempo = reaper.GetUserInputs('BPM Converter', 2, names, functionaults, 1024);
-	--]]
+
 	functionaults = tostring(bpm)..","..tostring(master_tempo)
 	retval, retvals_csv = reaper.GetUserInputs("BPM Converter", 2, "Original Tempo (BPM),Target Tempo (BPM)", functionaults) 
 
 	if retval == true then
+		
 		-- PARSE THE STRING
-		--[[tempo_values = user_tempo[4].split(',')
-		answer1 = int(tempo_values[0])
-		answer2 = int(tempo_values[1])]]
-
 		answer1, answer2 = retvals_csv:match("([^,]+),([^,]+)")
 		answer1 = tonumber(answer1)
 		answer2 = tonumber(answer2)
-
-		--msg("answer1:"..answer1)
-		--msg("answer2:"..answer2)
 
 		if answer1 > 20 and answer1 < 299 and answer2 > 20 and answer2 < 299 then
 
@@ -139,6 +122,7 @@ function setTempo()
 					currentrate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
 					reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", currentrate + bpm_rate)
 				end
+			
 			elseif answer2 < answer1 then
 
 				bpm_rate = (answer2 / answer1)
@@ -149,6 +133,7 @@ function setTempo()
 					currentrate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
 					reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", currentrate - bpm_calc)
 				end
+			
 			end
 			
 			reaper.Main_OnCommand(40612,0) -- Fix item length
@@ -170,7 +155,9 @@ if numItems >= 1 then
 	reaper.UpdateArrange()
 
 else
+	
 	reaper.ShowMessageBox("Please select one item...", "Error", 0) -- 0=OK,2=OKCANCEL,2=ABORTRETRYIGNORE,3=YESNOCANCEL,4=YESNO,5=RETRYCANCEL : ret 1=OK,2=CANCEL,3=ABORT,4=RETRY,5=IGNORE,6=YES,7=NO
+
 end
 
 reaper.Undo_EndBlock("Convert BPM of selected item",-1)
