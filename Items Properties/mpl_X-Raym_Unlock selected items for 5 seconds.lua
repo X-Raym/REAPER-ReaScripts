2b452a0a760bf9fecf54ab75584f103089322bd3
X-Raym <extremraym@yahoo.fr>
2015-06-24 22:11:49 +0200

    initial release

---
+++ Items Properties/mpl_X-Raym_Unlock selected items for 5 seconds.lua
@@ -0,0 +1,49 @@
+--[[
+ * ReaScript Name: Unlock selected items for 5 seconds
+ * Description: A way to temporary unlock an item.
+ * Instructions: Select item. Run. Wait.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: unlock for a given time
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=162188
+ * REAPER: 5.0 pre 15
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-06-09)
+	+ Initial Release
+ --]]
+
+time = 5 -- time in seconds you need for pause
+time1 = reaper.time_precise()
+
+function timer()  
+ time2 = reaper.time_precise() 
+ if time2 - time1 < time then 
+ 
+ reaper.defer(timer) 
+ else 
+  reaper.atexit(unlock) -- stop running script and run unlock   
+ end   
+end
+
+function unlock()
+  reaper.Main_OnCommand(40579, 0)
+  reaper.Main_OnCommand(40582, 0)
+end 
+
+-- PERFORM:
+
+-- 1. Toggle locking
+reaper.Main_OnCommand(40579, 0) 
+reaper.Main_OnCommand(40582, 0)
+reaper.UpdateArrange()
+
+-- 2. Run
+timer()  
