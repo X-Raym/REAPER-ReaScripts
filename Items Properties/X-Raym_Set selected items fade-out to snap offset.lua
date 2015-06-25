f38b17625a768bdf53c590472621daeae77dad41
X-Raym <extremraym@yahoo.fr>
2015-06-26 01:22:15 +0200

    initial release

---
+++ Items Properties/X-Raym_Set selected items fade-out to snap offset.lua
@@ -0,0 +1,82 @@
+--[[
+ * ReaScript Name: Set selected items fade-out to snap offset
+ * Description: See title
+ * Instructions: Select items. Run.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: Script: Scripts: Item Fades (various)
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?p=1538659
+ * REAPER: 5.0 pre 36
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-06-26)
+	+ Initial Release
+ --]]
+
+--[[ ----- DEBUGGING ====>
+local info = debug.getinfo(1,'S');
+
+local full_script_path = info.source
+
+local script_path = full_script_path:sub(2,-5) -- remove "@" and "file extension" from file name
+
+if reaper.GetOS() == "Win64" or reaper.GetOS() == "Win32" then
+  package.path = package.path .. ";" .. script_path:match("(.*".."\\"..")") .. "..\\Functions\\?.lua"
+else
+  package.path = package.path .. ";" .. script_path:match("(.*".."/"..")") .. "../Functions/?.lua"
+end
+
+require("X-Raym_Functions - console debug messages")
+
+
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+time_os = reaper.time_precise()
+
+msg_clean()
+]]-- <==== DEBUGGING -----
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	-- LOOP THROUGH SELECTED ITEMS
+	
+	selected_items_count = reaper.CountSelectedMediaItems(0)
+	
+	-- INITIALIZE loop through selected items
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS
+		item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+		-- GET INFOS
+		item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+		item_snap = reaper.GetMediaItemInfo_Value(item, "D_SNAPOFFSET")
+		
+		fadein_len = reaper.GetMediaItemInfo_Value(item, "D_FADEINLEN")
+		
+		if fadein_len >= item_snap then reaper.SetMediaItemInfo_Value(item, "D_FADEINLEN", item_snap) end
+		
+		-- SET INFOS
+		reaper.SetMediaItemInfo_Value(item, "D_FADEOUTLEN", item_len - item_snap) -- Set the value to the parameter
+	end -- ENDLOOP through selected items
+
+	reaper.Undo_EndBlock("Set selected items fade-out to snap offset", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+-- reaper.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+-- reaper.PreventUIRefresh(-1)  -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
