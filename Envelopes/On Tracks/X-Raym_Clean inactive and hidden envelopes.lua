fc9d8268da70dbec50a2b597b0ff62f3968b7741
X-Raym <extremraym@yahoo.fr>
2015-07-22 11:04:11 +0200

    initial release

---
+++ Envelopes/On Tracks/X-Raym_Clean inactive and hidden envelopes.lua
@@ -0,0 +1,116 @@
+--[[
+ * ReaScript Name: Clean inactive and hidden envelopes
+ * Description: A way to reset clean envelopes.
+ * Instructions: Select tracks with visible and armed envelopes. Execute the script. Note that if there is an envelope selected, it will work only for it.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl:
+ * Licence: GPL v3
+ * Forum Thread: Scripts (Lua): Multiple Tracks and Multiple Envelope Operations
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=157483
+ * REAPER: 5.0 RC5
+ * Extensions: SWS 2.7.3 #0
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-07-22)
+  + Initial release
+ --]]
+
+function Msg(var)
+	reaper.ShowConsoleMsg(tostring(var))
+end
+
+function Action(env)
+  retval, xml_env = reaper.GetEnvelopeStateChunk(env, "", false)
+  xml_env = xml_env:gsub("\n", "¤¤")
+  retval, xml_env = reaper.SetEnvelopeStateChunk(env, xml_env, false)
+return xml_env
+
+end
+
+function main() -- local (i, j, item, take, track)
+
+  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+  
+  edit_pos = reaper.GetCursorPosition()
+
+ selected_tracks_count = reaper.CountSelectedTracks(0)
+    
+    -- if selected_tracks_count > 0 and UserInput() then
+    if selected_tracks_count > 0 then
+      for i = 0, selected_tracks_count-1  do
+        
+        -- GET THE TRACK
+        track = reaper.GetSelectedTrack(0, i) -- Get selected track i
+
+        -- LOOP THROUGH ENVELOPES
+        env_count = reaper.CountTrackEnvelopes(track)
+        for j = 0, env_count-1 do
+
+          -- GET THE ENVELOPE
+          env = reaper.GetTrackEnvelope(track, j)
+      
+      if env ~= nil then
+      br_env = reaper.BR_EnvAlloc(env, false)
+      
+      active, visible, armed, inLane, laneHeight, defaultShape, minValue, maxValue, centerValue, type, faderScaling = reaper.BR_EnvGetProperties(br_env, true, true, true, true, 0, 0, 0, 0, 0, 0, true)
+
+    -- IF ENVELOPE IS A CANDIDATE
+    if visible == false and active == false then
+          
+          retval, xml_track = reaper.GetTrackStateChunk(track, "", false)
+          
+          xml_track = xml_track:gsub("\n", "¤¤")
+          
+          xml_env =  Action(env)
+          
+          -- xml_track = xml_track:gsub(xml_env, "")
+          
+          -- xml_track = xml_track:gsub("¤¤", "\n")
+		   -- Msg(xml_track)
+          
+          --retval = reaper.SetTrackStateChunk(track, xml_env, false)
+      
+    end
+      
+      reaper.BR_EnvFree(br_env, 0)
+
+        end -- ENDLOOP through envelopes
+    
+    end
+
+      end -- ENDLOOP through selected tracks
+  
+  end -- endif sel envelope
+  
+  reaper.Undo_EndBlock("Clean inactive and hidden envelopes", 0) -- End of the undo block. Leave it at the bottom of your main function.
+
+end -- end main()
+
+-- reaper.PreventUIRefresh(-1)
+main()
+reaper.UpdateArrange()
+-- reaper.PreventUIRefresh(1)
+
+function HedaRedrawHack()
+  reaper.PreventUIRefresh(1)
+
+  track=reaper.GetTrack(0,0)
+
+  trackparam=reaper.GetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT")  
+  if trackparam==0 then
+    reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 1)
+  else
+    reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 0)
+  end
+  reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", trackparam)
+
+  reaper.PreventUIRefresh(-1)
+  
+end
+
+HedaRedrawHack()
---
+++ Envelopes/On Tracks/X-Raym_Convert envelope value at edit cursor into track parameters.lua
@@ -0,0 +1,174 @@
+--[[
+ * ReaScript Name: Convert envelope value at edit cursor into track parameters
+ * Description: A way to convert envelope into track parameters.
+ * Instructions: Select tracks with visible and armed envelopes. Execute the script. Note that if there is an envelope selected, it will work only for it.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl:
+ * Licence: GPL v3
+ * Forum Thread: Scripts (Lua): Multiple Tracks and Multiple Envelope Operations
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=157483
+ * REAPER: 5.0 RC5
+ * Extensions: SWS 2.7.3 #0
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-07-22)
+	+ Initial release
+ --]]
+ 
+-- ----- DEBUGGING ====>
+--[[
+local info = debug.getinfo(1,'S');
+
+local full_script_path = info.source
+
+local script_path = full_script_path:sub(2,-5) -- remove "@" and "file extension" from file name
+
+if reaper.GetOS() == "Win64" or reaper.GetOS() == "Win32" then
+  package.path = package.path .. ";" .. script_path:match("(.*".."\\"..")") .. "..\\Functions\\?.lua"
+else
+  package.path = package.path .. ";" .. script_path:match("(.*".."/"..")") .. "../Functions/?.lua"
+end
+
+require("X-Raym_Functions - console debug messages")
+
+
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+msg_clean()
+]]-- <==== DEBUGGING -----
+
+--[[
+function UserInput()
+	retval, user_input_str = reaper.GetUserInputs("Envelope Analysis", 1, "Interval ? (s)", interval) -- We suppose that the user know the scale he want
+    if retval then
+		interval = tonumber(user_input_str)
+	end
+	return retval
+end
+]]
+
+function Msg(val)
+	reaper.ShowConsoleMsg(tostring(val).."\n")
+end
+
+function Action(env, track)
+	
+	-- GET THE ENVELOPE
+	br_env = reaper.BR_EnvAlloc(env, false)
+
+	active, visible, armed, inLane, laneHeight, defaultShape, minValue, maxValue, centerValue, env_type, faderScaling = reaper.BR_EnvGetProperties(br_env, true, true, true, true, 0, 0, 0, 0, 0, 0, true)
+
+	-- IF ENVELOPE IS A CANDIDATE
+	if visible == true and armed == true then
+		
+		if track == nil then
+			track = reaper.BR_EnvGetParentTrack(br_env)
+		end
+		
+		-- OUTPUT VALUE ANALYSIS
+		retval_eval, value_eval, dVdSOut_eval, ddVdSOut_eval, dddVdSOut_eval = reaper.Envelope_Evaluate(env, edit_pos, 0, 0)
+		
+		retval, env_name = reaper.GetEnvelopeName(env, "")
+			--msg_stl("Envelope name", env_name, 1)
+			--reaper.ShowConsoleMsg(env_name)
+			
+		if env_name == "Volume" then
+			reaper.SetMediaTrackInfo_Value(track, "D_VOL", value_eval)
+		end -- ENDIF Volume
+		
+		if env_name == "Pan" then
+			reaper.SetMediaTrackInfo_Value(track, "D_PAN", value_eval)
+		end -- ENDIF Volume
+		
+		if env_name == "Mute" then
+			reaper.SetMediaTrackInfo_Value(track, "B_MUTE", value_eval)
+		end -- ENDIF Mute
+
+		if env_name == "Width" then
+			reaper.SetMediaTrackInfo_Value(track, "D_WIDTH", value_eval)
+		end -- ENDIF Pan or Width
+		
+		if env_name == "Pan (Left)" then
+			reaper.SetMediaTrackInfo_Value(track, "D_DUALPANL", value_eval)
+		end
+		
+		if env_name == "Pan (Right)" then
+			reaper.SetMediaTrackInfo_Value(track, "D_DUALPANR", value_eval)
+		end
+		
+		-- reaper.BR_EnvSetPoint(br_env, 0, 0, value_eval, 0, false, 0)
+		--reaper.BR_EnvSetProperties(BR_Envelope envelope, boolean active, boolean visible, boolean armed, boolean inLane, integer laneHeight, integer defaultShape, boolean faderScaling)
+		-- reaper.BR_EnvSetProperties(br_env, false, false, true, true, laneHeight, defaultShape, faderScaling)
+		-- reaper.BR_EnvSetProperties(br_env, active_out, visible_out, armed_out, inLane, laneHeight, defaultShape, faderScaling)
+	
+	end
+	
+	reaper.BR_EnvFree(br_env, 0)
+	reaper.Envelope_SortPoints(env)
+
+end
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+	
+	edit_pos = reaper.GetCursorPosition()
+	
+	-- LOOP TRHOUGH SELECTED TRACKS
+	env = reaper.GetSelectedEnvelope(0)
+
+	if env == nil then
+
+		selected_tracks_count = reaper.CountSelectedTracks(0)
+		
+		-- if selected_tracks_count > 0 and UserInput() then
+		if selected_tracks_count > 0 then
+			for i = 0, selected_tracks_count-1  do
+				
+				-- GET THE TRACK
+				local track = reaper.GetSelectedTrack(0, i) -- Get selected track i
+
+				-- LOOP THROUGH ENVELOPES
+				env_count = reaper.CountTrackEnvelopes(track)
+				for j = 0, env_count-1 do
+
+					-- GET THE ENVELOPE
+					env = reaper.GetTrackEnvelope(track, j)
+
+					Action(env, track)
+
+				end -- ENDLOOP through envelopes
+
+			end -- ENDLOOP through selected tracks
+			
+		end
+
+	else
+
+		-- if UserInput() then
+			Action(env)
+		-- end
+	
+	end -- endif sel envelope
+
+	reaper.Undo_EndBlock("Convert envelope value at edit cursor into track parameters", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end -- end main()
+
+--msg_start() -- Display characters in the console to show you the begining of the script execution.
+
+-- reaper.PreventUIRefresh(1)-- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+-- reaper.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+--msg_end() -- Display characters in the console to show you the end of the script execution.
