60cfba11b54196f6fdfa27f73fed898469a2ee8c
X-Raym <extremraym@yahoo.fr>
2015-07-25 15:18:09 +0200

    update

--- Track Properties/X-Raym_Rename tracks with first VSTi and its preset name.lua
+++ Track Properties/X-Raym_Rename tracks with first VSTi and its preset name.lua
@@ -15,8 +15,11 @@
  
 --[[
  * Changelog:
+ * v1.1 (2015-07-25)
+	+ Delete author and version in name
+	# bug fix
  * v1.0 (2015-07-22)
-  + Initial Release
+	+ Initial Release
  --]]
  
 -- ------ USER CONFIG AREA =====>
@@ -28,19 +31,24 @@ separator = "-"
 function main()
 	
 	for i = 0, tracks_count - 1 do
+		
 		track = reaper.GetSelectedTrack(0, i)
 		
 		vsti_id = reaper.TrackFX_GetInstrument(track)
 		
-		if vst_id >= 0 then
+		if vsti_id >= 0 then
 		
 			retval, fx_name = reaper.TrackFX_GetFXName(track, vsti_id, "")
 			
 			fx_name = fx_name:gsub("VSTi: ", "")
 			
+			fx_name = fx_name:gsub("%(.-%)", "")
+			
+			fx_name = fx_name:gsub("  ", "")
+			
 			retval, presetname = reaper.TrackFX_GetPreset(track, vsti_id, "")
 			
-			if preset_name == nil then
+			if retval == 0 then
 			
 				track_name_retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", fx_name, true)
 				
@@ -61,7 +69,7 @@ tracks_count = reaper.CountSelectedTracks(0)
 
 if tracks_count > 0 then
   
-  -- reaper.PreventUIRefresh(1)
+  reaper.PreventUIRefresh(1)
    
   reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
  
@@ -69,6 +77,6 @@ if tracks_count > 0 then
    
   reaper.Undo_EndBlock("Rename tracks with first VSTi and its preset name", -1) -- End of the undo block. Leave it at the bottom of your main function.
    
-  -- reaper.PreventUIRefresh(-1)
+  reaper.PreventUIRefresh(-1)
   
 end
