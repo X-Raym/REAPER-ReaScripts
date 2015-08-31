67ef1c3f50cc5ddb231cfadb1c0b63890ba8d9f1
X-Raym <extremraym@yahoo.fr>
2015-08-31 23:47:02 +0200

    initial release

---
+++ Items Properties/X-Raym_Reset selected items active take strech markers position.lua
@@ -0,0 +1,86 @@
+--[[
+ * ReaScript Name: Reset selected items active take strech markers position
+ * Description:
+ * Instructions: Select items with take. Run.
+ * Screenshot: http://i.giphy.com/3o85xowNTqf8CYQrba.gif
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl:
+ * Licence: GPL v3
+ * Forum Thread: REQ: Reset stretch markers value
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=165774
+ * REAPER: 5.0 pre 15
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-08-31)
+	+ Initial Release
+ --]]
+
+-- ----- DEBUGGING ====>
+--[[local info = debug.getinfo(1,'S');
+
+local full_script_path = info.source
+
+local script_path = full_script_path:sub(2,-5) -- remove "@" and "file extension" from file name
+
+if reaper.GetOS() == "Win64" or reaper.GetOS() == "Win32" then
+  package.path = package.path .. ";" .. script_path:match("(.*".."\\"..")") .. "..\\Functions\\?.lua"
+else
+  package.path = package.path .. ";" .. script_path:match("(.*".."/"..")") .. "../Functions/?.lua"
+end
+
+require("X-Raym_Functions - console debug messages")
+
+
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+msg_clean()
+]]
+-- <==== DEBUGGING -----
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	-- LOOP THROUGH SELECTED TAKES
+	selected_items_count = reaper.CountSelectedMediaItems(0)
+
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS
+		item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+		take = reaper.GetActiveTake(item) -- Get the active take
+
+		if take ~= nil then -- if ==, it will work on "empty"/text items only
+		
+			strech_count = reaper.GetTakeNumStretchMarkers(take)
+			
+			for j = 0, strech_count - 1 do
+			
+				idx, strech_pos, srcpos = reaper.GetTakeStretchMarker(take, j)
+			
+				reaper.SetTakeStretchMarker(take, idx, srcpos)
+				
+			end
+
+		end -- ENDIF active take
+	
+	end -- ENDLOOP through selected items
+
+	reaper.Undo_EndBlock("Reset selected items active take strech markers position", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+reaper.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+reaper.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
