c23a518efd934cc084fc173ae8bab02980952c04
X-Raym <extremraym@yahoo.fr>
2015-09-16 16:02:03 +0200

    initial release

---
+++ Envelopes/On Tracks/X-Raym_Add points on envelopes at regions.lua
@@ -0,0 +1,160 @@
+--[[
+ * ReaScript Name: Add points on envelopes at regions
+ * Description: A way to copy insert point on several tracks envelopes at one time.
+ * Instructions: Select a track. Execute the script.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: 
+ * Forum Thread URl: 
+ * REAPER: 5.0
+ * Extensions: SWS 2.8.1
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-09-16)
+	+ Initial release
+ --]]
+
+-- ----- DEBUGGING ====>
+--[[
+local info = debug.getinfo(1,'S');
+
+local full_script_path = info.source
+
+local script_path = full_script_path:sub(2,-5) -- remove "@" and "file extension" from file name
+
+if reaper.GetOS() == "Win64" or reaper.GetOS() == "Win32" then
+  package.path = package.path .. ";" .. script_path:match("(.*".."\\"..")") .. "..\\Functions\\?.lua"
+else
+  package.path = package.path .. ";" .. script_path:match("(.*".."/"..")") .. "../Functions/?.lua"
+end
+
+require("X-Raym_Functions - console debug messages")
+
+
+debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
+clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
+
+msg_clean()]]
+-- <==== DEBUGGING -----
+function AddPoints(env)
+		-- GET THE ENVELOPE
+	br_env = reaper.BR_EnvAlloc(env, false)
+
+	active, visible, armed, inLane, laneHeight, defaultShape, minValue, maxValue, centerValue, type, faderScaling = reaper.BR_EnvGetProperties(br_env, true, true, true, true, 0, 0, 0, 0, 0, 0, true)
+
+	if visible == true and armed == true then
+
+		env_points_count = reaper.CountEnvelopePoints(env)
+
+		if env_points_count > 0 then
+			for k = 0, env_points_count+1 do 
+				reaper.SetEnvelopePoint(env, k, timeInOptional, valueInOptional, shapeInOptional, tensionInOptional, false, true)
+			end
+		end
+			
+		-- LOOP IN REGIONS
+		p=0
+		repeat
+		
+			retval, isrgn, pos, rgnend, name, markrgnindex = reaper.EnumProjectMarkers2(0, p)
+			
+			if isrgn == true then -- if name mtach activate take name
+				
+				--GET POINT VALUE
+				retval, valueOut, dVdSOutOptional, ddVdSOutOptional, dddVdSOutOptional = reaper.Envelope_Evaluate(env, pos, 0, 0)
+				retval2, valueOut2, dVdSOutOptional2, ddVdSOutOptional2, dddVdSOutOptional2 = reaper.Envelope_Evaluate(env, rgnend, 0, 0)
+				
+				-- ADD POINTS ON LOOP START AND END
+				reaper.InsertEnvelopePoint(env, pos, valueOut, 0, 0, true, true) -- INSERT startLoop point
+				reaper.InsertEnvelopePoint(env, rgnend, valueOut, 0, 0, true, true) -- INSERT startLoop point
+			
+			end
+			
+			p = p+1
+			
+		until retval == 0 -- end loop regions and markers
+
+		reaper.BR_EnvFree(br_env, 0)
+		reaper.Envelope_SortPoints(env)
+
+	end
+end
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	-- GET CURSOR POS
+	offset = reaper.GetCursorPosition()
+		
+	-- LOOP TRHOUGH SELECTED TRACKS
+	env = reaper.GetSelectedEnvelope(0)
+
+	if env == nil then
+
+		selected_tracks_count = reaper.CountSelectedTracks(0)
+		for i = 0, selected_tracks_count-1  do
+			
+			-- GET THE TRACK
+			track = reaper.GetSelectedTrack(0, i) -- Get selected track i
+
+			-- LOOP THROUGH ENVELOPES
+			env_count = reaper.CountTrackEnvelopes(track)
+			for j = 0, env_count-1 do
+
+				-- GET THE ENVELOPE
+				env = reaper.GetTrackEnvelope(track, j)
+				
+				AddPoints(env)
+				
+			end -- ENDLOOP through envelopes
+
+		end -- ENDLOOP through selected tracks
+
+	else
+
+		AddPoints(env)
+	
+	end -- endif sel envelope
+
+reaper.Undo_EndBlock("Add points on envelopes at regions", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end -- end main()
+
+--msg_start() -- Display characters in the console to show you the begining of the script execution.
+
+--[[ reaper.PreventUIRefresh(1) ]]-- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+--[[ reaper.PreventUIRefresh(-1) ]] -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+--msg_end() -- Display characters in the console to show you the end of the script execution.
+
+-- Update the TCP envelope value at edit cursor position
+function HedaRedrawHack()
+	reaper.PreventUIRefresh(1)
+
+	track=reaper.GetTrack(0,0)
+
+	trackparam=reaper.GetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT")	
+	if trackparam==0 then
+		reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 1)
+	else
+		reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 0)
+	end
+	reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", trackparam)
+
+	reaper.PreventUIRefresh(-1)
+	
+end
+
+HedaRedrawHack()
