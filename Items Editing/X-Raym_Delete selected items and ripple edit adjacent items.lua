2f0b371961e947d2b282f2654fca5543551ad5cb
X-Raym <extremraym@yahoo.fr>
2015-12-02 10:07:47 +0100

    Initial Release

---
+++ Items Editing/X-Raym_Delete selected items and ripple edit adjacent items.lua
@@ -0,0 +1,79 @@
+--[[
+ * ReaScript Name: Delete selected items and ripple edit adjacent items
+ * Description: See title
+ * Instructions: Select items. Run.
+ * Screenshot: http://i.giphy.com/3o8doN6hJOcw77QX8Q.gif
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl:
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Items Editing (various)
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=163363
+ * REAPER: 5.0
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-12-02)
+	+ Initial Release
+ --]]
+ 
+function msg(val)
+	reaper.ShowConsoleMsg(tostring(val).."\n")
+end
+
+function main() -- local (i, j, item, take, track)
+
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+	-- LOOP THROUGH SELECTED TAKES
+	selected_items_count = reaper.CountSelectedMediaItems(0)
+	
+	delete_items = {}
+
+	for i = 0, selected_items_count-1  do
+		-- GET ITEMS
+		item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+		track = reaper.GetMediaItemTrack(item) -- Get the active take
+			
+		-- GET INFOS
+		item_idx = reaper.GetMediaItemInfo_Value(item, "IP_ITEMNUMBER")
+		item_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+		item_len =  reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+		item_end = item_pos + item_len
+		
+		next_item = reaper.GetTrackMediaItem(track, item_idx + 1)
+		
+		if next_item ~= nil then
+			next_item_pos = reaper.GetMediaItemInfo_Value(next_item, "D_POSITION")
+			
+			if next_item_pos <= item_end + 0.0000000000001 then
+				reaper.SetMediaItemInfo_Value(next_item, "D_POSITION", item_pos)
+				table.insert(delete_items, item)
+			end
+			
+		end
+			
+	
+	end -- ENDLOOP through selected items
+	
+	for i = 1, #delete_items do
+		track = reaper.GetMediaItemTrack(delete_items[i])
+		reaper.DeleteTrackMediaItem(track, delete_items[i])
+	end
+
+	reaper.Undo_EndBlock("Delete selected items and ripple edit adjacent items", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+reaper.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+reaper.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
---
+++ Items Editing/X-Raym_Delete selected items sections in time selection if cursor enter time selection and ripple new items.lua
@@ -0,0 +1,85 @@
+﻿--[[
+ * ReaScript Name: Delete selected items sections in time selection if cursor enter time selection and ripple new items
+ * Description: See title
+ * Instructions: Select items with take. Run.
+ * Screenshot: http://i.giphy.com/3o8doN6hJOcw77QX8Q.gif
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl:
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Items Editing (various)
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=163363
+ * REAPER: 5.0
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-12-02)
+	+ Initial Release
+ --]]
+
+-- Requested by Vanhaze
+-- Need X-Raym_Delete selected items and ripple edit adjacent items.lua
+
+function Msg(val)
+	reaper.ShowConsoleMsg(tostring(val).."\n")
+end
+
+ -- Set ToolBar Button ON
+function SetButtonON()
+  is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd );
+  reaper.SetToggleCommandState( sec, cmd, 1 ); -- Set ON
+  reaper.RefreshToolbar2( sec, cmd );
+end
+
+-- Set ToolBar Button OFF
+function SetButtonOFF()
+  is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd );
+  reaper.SetToggleCommandState( sec, cmd, 0 ); -- Set OFF
+  reaper.RefreshToolbar2( sec, cmd );
+end
+
+
+function main()
+	
+	mouse_pos = reaper.BR_PositionAtMouseCursor(false)
+	start_time, end_time =  reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
+	
+	if mouse_pos > start_time and mouse_pos < end_time then
+		
+		if in_time == false then
+		
+			in_time = true
+		
+			if in_time == true then
+				count_selected_items = reaper.CountSelectedMediaItems(0)
+				if count_selected_items > 0 then
+					reaper.PreventUIRefresh(1)
+					reaper.Main_OnCommand(40061, 0) -- Split Items at Time Selection // This action performs on all tracks if no items selected
+					reaper.Main_OnCommand( reaper.NamedCommandLookup( "_RSeae17866577002490d8a62b72bbe7f939970eb70" ), 0 ) -- Ripple Edit
+					reaper.PreventUIRefresh(-1)
+				end
+			end
+		
+		end
+	
+	else
+	
+		in_time = false
+	
+	end
+	
+	reaper.defer(main)
+end
+
+SetButtonON()
+
+in_time = false
+main()
+
+reaper.atexit( SetButtonOFF )
