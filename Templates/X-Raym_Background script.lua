a29c3201e0f7b4b52fe84ab7f78f905464b26ece
X-Raym <extremraym@yahoo.fr>
2015-12-02 09:15:06 +0100

    Initial Release

---
+++ Templates/X-Raym_Background script.lua
@@ -0,0 +1,52 @@
+--[[
+ * ReaScript Name: Background Script
+ * Description: A template script for running in background REAPER ReaScript, with toolbar button ON/OFF state.
+ * Instructions: Run.
+ * Author: X-Raym
+ * Author URl: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URl: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URl: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: Toolbar button toggle state for script actions?
+ * Forum Thread URl: http://forum.cockos.com/showthread.php?t=164034
+ * REAPER: 5.0
+ * Extensions: None
+ --]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2015-12-02)
+	+ Initial Release
+ --]]
+ 
+ -- Set ToolBar Button ON
+function SetButtonON()
+  is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd );
+  reaper.SetToggleCommandState( sec, cmd, 1 ); -- Set ON
+  reaper.RefreshToolbar2( sec, cmd );
+end
+
+-- Set ToolBar Button OFF
+function SetButtonOFF()
+  is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  state = reaper.GetToggleCommandStateEx( sec, cmd );
+  reaper.SetToggleCommandState( sec, cmd, 0 ); -- Set OFF
+  reaper.RefreshToolbar2( sec, cmd );
+end
+
+
+-- Main Function (which loop in background)
+function main()
+  
+  reaper.defer( main )
+  
+end
+
+
+
+-- RUN
+SetButtonON()
+main()
+reaper.atexit( SetButtonOFF )
