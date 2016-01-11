5f8681d32b6e9dd8fe59cd078ed07913f86333cc
X-Raym <extremraym@yahoo.fr>
2016-01-11 12:56:23 +0100

    updated

--- Items Properties/X-Raym_Reset stretch marker under mouse position.lua
+++ Items Properties/X-Raym_Reset stretch marker under mouse position.lua
@@ -13,42 +13,19 @@
  * Forum Thread: REQ: Reset stretch markers value
  * Forum Thread URI: http://forum.cockos.com/showthread.php?t=165774
  * REAPER: 5.0
- * Extensions: SWS 2.8.0
- * Version: 1.0
+ * Extensions: SWS 2.8.3
+ * Version: 1.0.1
 --]]
  
 --[[
  * Changelog:
+ * v1.0.1 (2016-01-11)
+	+ Initial Release
  * v1.0 (2015-09-01)
 	+ Initial Release
  --]]
 
--- ----- DEBUGGING ====>
---[[local info = debug.getinfo(1,'S');
-
-local full_script_path = info.source
-
-local script_path = full_script_path:sub(2,-5) -- remove "@" and "file extension" from file name
-
-if reaper.GetOS() == "Win64" or reaper.GetOS() == "Win32" then
-  package.path = package.path .. ";" .. script_path:match("(.*".."\\"..")") .. "..\\Functions\\?.lua"
-else
-  package.path = package.path .. ";" .. script_path:match("(.*".."/"..")") .. "../Functions/?.lua"
-end
-
-require("X-Raym_Functions - console debug messages")
-
-
-debug = 1 -- 0 => No console. 1 => Display console messages for debugging.
-clean = 1 -- 0 => No console cleaning before every script execution. 1 => Console cleaning before every script execution.
-
-msg_clean()
-]]
--- <==== DEBUGGING -----
-
 function main() -- local (i, j, item, take, track)
-
-	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
 	
 	window, segment, details = reaper.BR_GetMouseCursorContext()
 	--reaper.ShowConsoleMsg(details.."\n")
@@ -61,18 +38,20 @@ function main() -- local (i, j, item, take, track)
 			idx = reaper.BR_GetMouseCursorContext_StretchMarker()
 			--reaper.ShowConsoleMsg(idx.."\n")
 			if idx ~= nil then
+			
+				reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
 	
 				idx, strech_pos, srcpos = reaper.GetTakeStretchMarker(take, idx)
 				
 				reaper.SetTakeStretchMarker(take, idx, srcpos)
+				
+				reaper.Undo_EndBlock("Reset stretch marker under mouse position", -1) -- End of the undo block. Leave it at the bottom of your main function.
 			
 			end
 			
 		end
 				
 	end
-	
-	reaper.Undo_EndBlock("Reset stretch marker under mouse position", -1) -- End of the undo block. Leave it at the bottom of your main function.
 
 end
 
