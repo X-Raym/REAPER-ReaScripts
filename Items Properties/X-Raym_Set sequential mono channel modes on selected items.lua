05f00a36fb5b91ef4711cc32143785e205a9b286
Raymond Radet <extremraym@yahoo.fr>
2017-08-29 18:57:41 +0200

    update

--- Items Properties/X-Raym_Set sequential mono channel modes on selected items.lua
+++ Items Properties/X-Raym_Set sequential mono channel modes on selected items.lua
@@ -1,5 +1,5 @@
 --[[
- * ReaScript Name: Set selected takes names by columns according to selected takes on track under mouse
+ * ReaScript Name: Set sequential mono channel modes on selected items
  * Description: It sets each to next number. So first item will be channel 1, next channel 2, etc
  * Instructions: Select items. Run.
  * Screenshot: http://i.imgur.com/KTapWKU.gif
@@ -11,11 +11,13 @@
  * Forum Thread: Scripts: Items Properties (various)
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1574814
  * REAPER: 5.0
- * Version: 1.0
+ * Version: 1.0.1
 --]]
 
 --[[
  * Changelog:
+ * v1.0.1 (2017-08-29)
+	# Title
  * v1.0 (2017-08-29)
 	+ Initial Release
 --]]
@@ -32,7 +34,7 @@ for i = 0, reaper.CountSelectedMediaItems(0) - 1 do
   end
 end
 
-reaper.Undo_EndBlock("Set selected takes names by columns according to selected takes on track under mouse", -1)
+reaper.Undo_EndBlock("Set sequential mono channel modes on selected items", -1)
 
 reaper.UpdateArrange()
 
