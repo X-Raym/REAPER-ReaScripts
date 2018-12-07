c3d42b8ccf1ecef9af86dc715f3ec78ed3804942
Raymond Radet <extremraym@yahoo.fr>
2018-12-07 01:13:05 +0100

    update

--- Items Editing/X-Raym_Shift selected takes source audio content position randomly.lua
+++ Items Editing/X-Raym_Shift selected takes source audio content position randomly.lua
@@ -9,11 +9,13 @@
  * Forum Thread: Scripts: Items Editing (various)
  * Forum Thread URI: https://forum.cockos.com/showthread.php?t=163363
  * REAPER: 5.0
- * Version: 1.0
+ * Version: 1.1
 --]]
  
 --[[
  * Changelog:
+ * v1.1 (2018-12-07)
+  # Quantify snap offset if snap active
  * v1.0 (2018-12-06)
   + Initial Release
 --]]
@@ -28,6 +30,8 @@ end
 -- Main function
 function main()
 
+  snap = reaper.GetToggleCommandState(1157)
+
   for i = 0, reaper.CountSelectedMediaItems(0)-1 do
     local item = reaper.GetSelectedMediaItem(0, i)
     local take = reaper.GetActiveTake( item )
@@ -36,6 +40,20 @@ function main()
       local source = reaper.GetMediaItemTake_Source( take )
       local retval, lengthIsQN = reaper.GetMediaSourceLength( source )
       local value = randomFloat( 0, retval - reaper.GetMediaItemInfo_Value( item, "D_LENGTH") )
+      if snap == 1 then -- if Snap on 
+        -- NOTE: only for fix grid
+        -- Quantize the offset
+        offset = reaper.GetMediaItemTakeInfo_Value( take, "D_STARTOFFS" )
+        offset_quantized = reaper.SnapToGrid( 0, offset ) -- NOTE: Not friendly with variable grid size
+        grid_offset = offset - offset_quantized
+        value = reaper.SnapToGrid( 0, value ) + grid_offset
+        if value > retval then
+          value = reaper.BR_GetPrevGridDivision( value ) -- quantize left
+        end
+        if value < 0 then
+          value = reaper.BR_GetNextGridDivision( value ) -- quantize left
+        end
+      end
       reaper.SetMediaItemTakeInfo_Value( take, "D_STARTOFFS", value )
       
     end
