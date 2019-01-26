4a39de6711b647984c8997e633f74fdc73117211
X-Raym <extremraym@yahoo.fr>
2019-01-26 11:03:00 +0100

    initial release

---
+++ Regions/X-Raym_Export markers and regions as tab-delimited CSV file.lua
@@ -0,0 +1,78 @@
+--[[
+ * ReaScript Name: Export markers and regions from tab-delimited CSV file
+ * Description: See title.
+ * Instructions: Select a track. Run.
+ * Author: X-Raym
+ * Author URI: http://www.extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Links
+    Forum Thread https://forum.cockos.com/showthread.php?p=1670961
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2019-01-26)
+  + Initial Release
+--]]
+
+function export(f, variable)
+  f:write(variable)
+  f:write("\n")
+end
+
+function rgbToHex(r, g, b)
+    return string.format("#%0.2X%0.2X%0.2X", r, g, b)
+end
+
+function Main()
+
+  local f = io.open(file, "w")
+  
+  export(f, "#  Name  Start  End  Length  Color")
+  
+  i=0
+  repeat
+    iRetval, bIsrgnOut, iPosOut, iRgnendOut, name, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(0,i)
+    if iRetval >= 1 then
+      local t, duration
+      if bIsrgnOut then
+        t = "R"
+        duration = iRgnendOut - iPosOut
+      else 
+        t = "M"
+        duration = 0
+        iRgnendOut = iPosOut
+      end
+      
+      local color = 0
+      if iColorOur > 0 then
+        r,g,b = reaper.ColorFromNative(iColorOur)
+        color = rgbToHex(r, g, b)
+      end
+        -- [start time HH:MM:SS.F] [end time HH:MM:SS.F] [name]
+        line = t .. iMarkrgnindexnumberOut .. "\t\"" .. name .. "\"\t" .. iPosOut .. "\t" .. iRgnendOut .. "\t" .. duration .. "\t" .. color
+        export(f, line)
+      end
+      i = i+1
+  until iRetval == 0
+
+  -- CLOSE FILE
+  f:close() -- never forget to close the file
+end
+
+
+if not reaper.JS_Dialog_BrowseForSaveFile then
+  Msg("Please install JS_ReaScript REAPER extension")
+else
+
+ retval, file = reaper.JS_Dialog_BrowseForSaveFile( "Save Markers and Regions", '', "", '.csv' )
+
+ if retval and file ~= '' then
+  reaper.defer(Main)
+ end
+  
+end
---
+++ Regions/X-Raym_Import markers and regions from tab-delimited CSV file.lua
@@ -0,0 +1,187 @@
+--[[
+ * ReaScript Name: Import markers and regions from tab-delimited CSV file
+ * Description: See title.
+ * Instructions: Select a track. Run.
+ * Author: X-Raym
+ * Author URI: http://www.extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Links
+    Forum Thread https://forum.cockos.com/showthread.php?p=1670961
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2019-01-26)
+  + Initial Release
+--]]
+
+-- USER CONFIG AREA -----------------------------------------------------------
+-- Duplicate and Rename the script if you want to modify this.
+-- Else, a script update will erase your mods.
+
+console = true -- true/false: display debug messages in the console
+sep = "\t" -- default sep
+
+col_pos = 3 -- Position column index in the CSV
+col_pos_end = 4 -- Length column index in the CS
+col_len = 5 -- Length column index in the CSV
+col_name = 2 -- Name column index in the CSV
+col_color = 6
+
+------------------------------------------------------- END OF USER CONFIG AREA
+
+function ColorHexToInt(hex)
+  hex = hex:gsub("#", "")
+  local R = tonumber("0x"..hex:sub(1,2))
+  local G = tonumber("0x"..hex:sub(3,4))
+  local B = tonumber("0x"..hex:sub(5,6))
+  return reaper.ColorToNative(R, G, B)
+end
+
+-- Optimization
+local reaper = reaper
+
+-- CSV to Table
+-- http://lua-users.org/wiki/LuaCsv
+function ParseCSVLine (line,sep)
+  local res = {}
+  local pos = 1
+  sep = sep or ','
+  while true do
+    local c = string.sub(line,pos,pos)
+    if (c == "") then break end
+    if (c == '"') then
+      -- quoted value (ignore separator within)
+      local txt = ""
+      repeat
+        local startp,endp = string.find(line,'^%b""',pos)
+        txt = txt..string.sub(line,startp+1,endp-1)
+        pos = endp + 1
+        c = string.sub(line,pos,pos)
+        if (c == '"') then txt = txt..'"' end
+        -- check first char AFTER quoted string, if it is another
+        -- quoted string without separator, then append it
+        -- this is the way to "escape" the quote char in a quote. example:
+        --   value1,"blub""blip""boing",value3  will result in blub"blip"boing  for the middle
+      until (c ~= '"')
+      table.insert(res,txt)
+      assert(c == sep or c == "")
+      pos = pos + 1
+    else
+      -- no quotes used, just look for the first separator
+      local startp,endp = string.find(line,sep,pos)
+      if (startp) then
+        table.insert(res,string.sub(line,pos,startp-1))
+        pos = endp + 1
+      else
+        -- no separator found -> use rest of string and terminate
+        table.insert(res,string.sub(line,pos))
+        break
+      end
+    end
+  end
+  return res
+end
+
+
+-- UTILITIES -------------------------------------------------------------
+
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
+
+function ReverseTable(t)
+    local reversedTable = {}
+    local itemCount = #t
+    for k, v in ipairs(t) do
+        reversedTable[itemCount + 1 - k] = v
+    end
+    return reversedTable
+end
+
+--------------------------------------------------------- END OF UTILITIES
+
+function read_lines(filepath)
+
+  lines = {}
+
+  local f = io.input(filepath)
+  repeat
+
+    s = f:read ("*l") -- read one line
+
+    if s then  -- if not end of file (EOF)
+      table.insert(lines, ParseCSVLine (s,sep))
+    end
+
+  until not s  -- until end of file
+
+  f:close()
+
+end
+
+-- Main function
+function main()
+
+  folder = filetxt:match[[^@?(.*[\/])[^\/]-$]]
+
+  for i, line in ipairs( lines ) do
+    if i > 1 then
+
+      -- Name Variables
+      local pos = tonumber(line[col_pos])
+      local pos_end = tonumber(line[col_pos_end])
+      local len =  tonumber( line[col_len] )
+      local name = line[col_name]
+      local color = 0
+      if line[col_color] ~= "0" then
+        color = ColorHexToInt(line[col_color])|0x1000000
+      end
+      
+      local is_region = true
+      
+      if pos_end == pos then
+        is_region = false
+      end
+      
+      if pos and pos_end and name and color then
+        reaper.AddProjectMarker2( 0, is_region, pos, pos_end, name, -1, color )
+      end
+
+    end
+  end
+
+end
+
+-- INIT
+
+retval, filetxt = reaper.GetUserFileNameForRead("", "Import markers and regions", "csv")
+
+if retval then
+
+  reaper.PreventUIRefresh(1)
+
+  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+  
+  reaper.ClearConsole()
+
+  read_lines(filetxt)
+  
+  -- reaper.Main_OnCommand( reaper.NamedCommandLookup( "_SWSMARKERLIST10" ), -1) -- SWS: Delete all regions
+
+  main()
+
+  reaper.Undo_EndBlock("Import markers and regions from tab-delimited CSV", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+  reaper.UpdateArrange()
+
+  reaper.PreventUIRefresh(-1)
+
+end
