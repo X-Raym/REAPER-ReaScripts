fe4f352c134291fd57689fc6e013f3b999d67a59
X-Raym <extremraym@yahoo.fr>
2019-03-28 12:00:26 +0100

    update

--- View/X-Raym_Scroll arrange view if mouse reaches screen edges.lua
+++ View/X-Raym_Scroll arrange view if mouse reaches screen edges.lua
@@ -11,11 +11,14 @@
  * Forum Thread URI: https://forum.cockos.com/showthread.php?p=1523568#post1523568
  * REAPER: 5.0
  * Extensions: js_extension
- * Version: 1.1
+ * Version: 1.2
 --]]
  
 --[[
  * Changelog:
+ * v1.2 (2019-03-28)
+  + Shift Key for fast speed
+  + Ctrl key for slow speed
  * v1.1 (2019-03-23)
   + Scroll only if REAPER has focus (thanks Edgemeal!)
   + Scroll at quarter (no need to be right on screen corners for diagonal scroll)
@@ -53,26 +56,41 @@ function Main()
   if ReaperHasFocus() then
 
     mouse_x, mouse_y = reaper.GetMousePosition()
+    shift = reaper.JS_Mouse_GetState( 8 )
+    ctrl = reaper.JS_Mouse_GetState( 4 )
+    
+    multiplicator_x = 1
+    multiplicator_y = 3
+    
+    if shift == 8 then
+      multiplicator_x = 5
+      multiplicator_y = 10
+    end
+    
+    if ctrl == 4 then
+      multiplicator_x = 1
+      multiplicator_y = 1
+    end
     
     val_x = 0
-    val_y = 0
+    val_y = 0    
     
     if mouse_x == 0 or ((mouse_y == 0 or mouse_y >= screen_bottom - 1 ) and mouse_x <= screen_right * 0.25 ) then
-      val_x = -1
+      val_x = -1 * multiplicator_x
       reaper.JS_Mouse_SetCursor( cursor_left )
     end
     
     if mouse_x >= screen_right - 1 or ((mouse_y == 0 or mouse_y >= screen_bottom - 1 ) and mouse_x >= screen_right * 0.75 )then
-     val_x = 1
+     val_x = 1 * multiplicator_x
     end
     
     if mouse_y == 0 or ((mouse_x == 0 or mouse_x >= screen_right - 1 ) and mouse_y <= screen_bottom * 0.25 ) then
-      val_y = -1
+      val_y = -1 * multiplicator_y
       reaper.JS_Mouse_SetCursor( cursor_up )
     end
     
     if mouse_y >= screen_bottom - 1 or ((mouse_x == 0 or mouse_x >= screen_right - 1 ) and mouse_y >= screen_bottom * 0.75 ) then
-      val_y = 1
+      val_y = 1 * multiplicator_y
     end
     
     if val_x ~= 0 or val_y ~= 0 then
