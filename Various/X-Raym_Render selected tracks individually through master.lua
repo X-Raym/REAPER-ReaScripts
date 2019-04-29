0f89aa1fdca5619a46fae08f089e3a952b61d904
X-Raym <extremraym@yahoo.fr>
2019-04-29 12:28:29 +0200

    Update X-Raym_Render selected tracks individually through master.lua

--- Various/X-Raym_Render selected tracks individually through master.lua
+++ Various/X-Raym_Render selected tracks individually through master.lua
@@ -1,7 +1,8 @@
 --[[
  * ReaScript Name: Render selected tracks individually through master
  * Description: A way to render tracks to master chain.
- * Instructions: Select tracks. Set render settings to source = master tracks. Add incrementation ($tracks wildcard will not work). Run.
+ * Instructions: Select tracks. Set render settings to source = master tracks, time selection, or custom time range.
+ * Screenshot:
  * Author: X-Raym
  * Author URI: http://extremraym.com
  * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
@@ -11,12 +12,13 @@
  * Forum Thread: Render Stems (selected tracks) through master FX?
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1652366
  * REAPER: 5.0
- * Extensions: None
- * Version: 1.2
+ * Version: 2.0
 --]]
 
 --[[
  * Changelog:
+ * v2.0 (2019-04-29)
+	# New core: now works without regions. REAPER > v5.954 required.
  * v1.2 (2018-07-04)
 	# Better instructions
  * v1.1.1 (2018-03-15)
@@ -30,57 +32,44 @@
 
 -- USER CONFIG AREA -----------------------------------------------------------
 
-render = true -- true/false: display debug messages in the console
+add_queue = false -- Toggle to render right away
+render = true -- true/false: Toggle to render the queue is queue has been chosen
+
+-- Render action for the instant render
+render_action = 42230 -- -- File: Render project, using the most recent render settings, auto-close render dialog
 
 console = true -- display console messages
-
-function GetRegionsRender_Index()
-
-
-	-- LOOP THROUGH REGIONS
-	i=0
-	repeat
-		iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(0,i)
-		if iRetval >= 1 then
-			if bIsrgnOut == true and sNameOut == "=RENDER" then
-				region_idx = iMarkrgnindexnumberOut
-				break
-			end
-			i = i+1
-		end
-	until iRetval == 0
-
-	return region_idx
-
-end
 
 ------------------------------------------------------- END OF USER CONFIG AREA
 
 function main()
 
-	Msg("------")
-	Msg("Tracks added to render queue:")
+	retval, pattern = reaper.GetSetProjectInfo_String( 0, "RENDER_PATTERN", "", false )
 
 	-- LOOP TRHOUGH SELECTED TRACKS
-	total = 0
+	local total = 0
 	for i, track in ipairs(init_sel_tracks) do
 		reaper.SetOnlyTrackSelected(track)
 		reaper.Main_OnCommand(40340, 0) -- Unsolo all tracks
 		reaper.Main_OnCommand(40728, 0) -- Solo track
 
-		retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "new track name", false) -- Get track info
+		local retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false) -- Get track info
 
-		reaper.SetProjectMarker(region_idx, true, iPosOut, iRgnendOut, track_name) -- Set region to name of track
+		local new_pattern = pattern:gsub("$track", track_name)
 
-		reaper.Main_OnCommand(41823, 0) -- Add to render queue
-
-		Msg(track_name)
+		reaper.GetSetProjectInfo_String( 0, "RENDER_PATTERN", new_pattern, true )
+		
+		if add_queue then
+			reaper.Main_OnCommand(41823, 0) -- Add to render queue               
+		else
+			reaper.Main_OnCommand( render_action, 0)
+		end
 
 		total = total + 1
 
 	end
-
-	Msg("\nTotal tracks = " .. total)
+	
+	retval, pattern = reaper.GetSetProjectInfo_String( 0, "RENDER_PATTERN", pattern, true ) -- Restore initial pattern
 
 	reaper.Main_OnCommand(40340, 0) -- Unsolo all tracks
 
@@ -127,50 +116,27 @@ sel_tracks_count = reaper.CountSelectedTracks(0)
 
 if sel_tracks_count > 0 then
 
-	instructions = [[
-1. Create region for rendering project stems called =RENDER.
-2. In Render To File window, set source to 'Region render matrix'.
-3. In Region Matrix window, check the 'Master Mix' box for the '=RENDER' region
-4. Add the $region wildcard to the output file name.
-5. Press 'Save changes and close', then press Continue below.
-	]]
+	reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
 
-	retval = reaper.ShowMessageBox( instructions, "Render through master track - Instructions", 1 )
+	reaper.PreventUIRefresh(1)
 
-	if retval == 1 then
+	reaper.ClearConsole()
 
-		reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+	init_sel_tracks = {}
+	SaveSelectedTracks(init_sel_tracks)
 
-		reaper.PreventUIRefresh(1)
+	main() -- Execute your main function
 
-		reaper.ClearConsole()
-
-		init_sel_tracks = {}
-		SaveSelectedTracks(init_sel_tracks)
-
-		region_idx = GetRegionsRender_Index()
-
-		if region_idx == nil then
-			Msg('Create a region named "=RENDER", from the size of your project.')
-			return
-		end
-
-		main() -- Execute your main function
-
-		if render then
-			reaper.Main_OnCommand(41207, 0)
-		end
-
-		reaper.SetProjectMarker(region_idx, true, iPosOut, iRgnendOut, "=RENDER")
-
-		RestoreSelectedTracks(init_sel_tracks)
-
-		reaper.UpdateArrange() -- Update the arrangement (often needed)
-
-		reaper.PreventUIRefresh(-1)
-
-		reaper.Undo_EndBlock("Render selected tracks individually through master", -1) -- End of the undo block. Leave it at the bottom of your main function.
-
+	if render then
+		reaper.Main_OnCommand(41207, 0)
 	end
+
+	RestoreSelectedTracks(init_sel_tracks)
+
+	reaper.UpdateArrange() -- Update the arrangement (often needed)
+
+	reaper.PreventUIRefresh(-1)
+
+	reaper.Undo_EndBlock("Render selected tracks individually through master", -1) -- End of the undo block. Leave it at the bottom of your main function.
 
 end
