a51c12846041966736aae449ed759d5d5671258f
X-Raym <extremraym@yahoo.fr>
2019-06-23 16:52:26 +0200

    initial release

---
+++ Track Properties/X-Raym_Select track under mouse top level parent track.lua
@@ -0,0 +1,36 @@
+--[[
+ * ReaScript Name: Select track under mouse top level parent track
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Track Selection (various)
+ * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1569551
+ * REAPER: 5.0
+ * Version: 2.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2019-06-23)
+	+ Initial Release
+--]]
+
+function GetTopLevelParentTrack( track )
+  local parent = reaper.GetParentTrack( track )
+  if parent then
+   track = GetTopLevelParentTrack( parent )
+   end
+    return track
+end
+
+function main()
+  track, pos = reaper.BR_TrackAtMouseCursor()
+  if track then
+    top_parent = GetTopLevelParentTrack( track )
+    reaper.SetOnlyTrackSelected( top_parent )
+  end
+end
+
+main()
