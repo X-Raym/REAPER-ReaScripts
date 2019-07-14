9546645dd5849a6efac04db42e4d0fcd75e9de34
X-Raym <extremraym@yahoo.fr>
2019-07-14 23:39:25 +0200

    Create X-Raym_Snap edit cursor to nearest grid if snap is enabled.lua

---
+++ Transport/X-Raym_Snap edit cursor to nearest grid if snap is enabled.lua
@@ -0,0 +1,28 @@
+--[[
+ * ReaScript Name: Snap edit cursor to nearest grid if snap is enabled
+ * About: For custom action usage
+ * Author: X-Raym
+ * Author URI: https://www.extremraym.com
+ * Repository: GitHub > X-Raym > ReaScripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2019-07-14)
+  + Initial release
+--]]
+
+function main()
+  local pos = reaper.GetCursorPosition()
+  if reaper.GetToggleCommandState( 1157 ) then
+    pos = reaper.SnapToGrid( 0, pos )
+	reaper.SetEditCurPos( pos, false, false )
+  end
+end
+
+reaper.Main()
+
