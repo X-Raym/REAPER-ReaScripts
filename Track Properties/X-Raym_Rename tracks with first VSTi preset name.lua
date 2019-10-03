4b414c0d3466d81f3a9e9b995c69513c8f17f7bc
X-Raym <x-raym@extremraym.com>
2019-10-03 21:26:04 +0200

    Create X-Raym_Rename tracks with first VSTi preset name.lua

---
+++ Track Properties/X-Raym_Rename tracks with first VSTi preset name.lua
@@ -0,0 +1,79 @@
+--[[
+ * ReaScript Name: Rename tracks with first VSTi preset name
+ * Description: A way to quickly rename and recolor tracks in a REAPER project from its instrument.
+ * Instructions: Select tracks. Run.
+ * Screenshot: http://i.giphy.com/l41lMgnQVFZp2qfjW.gif
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URI:
+ * Licence: GPL v3
+ * Forum Thread: Video & Sound Editors Will Really Like This
+ * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1539710
+ * Extensions: None
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2019-10-03)
+	+ Initial Release
+--]]
+ 
+-- ------ USER CONFIG AREA =====>
+
+--separator = "-"
+
+-- <===== USER CONFIG AREA ------
+
+function main()
+	
+	for i = 0, tracks_count - 1 do
+		
+		track = reaper.GetSelectedTrack(0, i)
+		
+		vsti_id = reaper.TrackFX_GetInstrument(track)
+		
+		if vsti_id >= 0 then
+		
+			retval, fx_name = reaper.TrackFX_GetFXName(track, vsti_id, "")
+			
+			fx_name = fx_name:gsub("VSTi: ", "")
+			
+			fx_name = fx_name:gsub(" %(.-%)", "")
+			
+			retval, presetname = reaper.TrackFX_GetPreset(track, vsti_id, "")
+			
+			if retval == 0 then
+			
+				track_name_retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", fx_name, true)
+				
+			else
+			
+				track_name_retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", presetname, true)
+			
+			end
+		
+		end
+	
+	end
+
+end
+
+-- INIT
+tracks_count = reaper.CountSelectedTracks(0)
+
+if tracks_count > 0 then
+  
+  reaper.PreventUIRefresh(1)
+   
+  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+ 
+  main()
+   
+  reaper.Undo_EndBlock("Rename tracks with first VSTi preset name", -1) -- End of the undo block. Leave it at the bottom of your main function.
+   
+  reaper.PreventUIRefresh(-1)
+  
+end
