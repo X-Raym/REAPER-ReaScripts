5b3792358cdd53a9f04bb016ed380c18bdf65a31
X-Raym <x-raym@extremraym.com>
2019-11-02 10:53:13 +0100

    initial release and update
    Resolve Markers workflow

--- Regions/X-Raym_Export markers and regions as Davinci Resolve EDL file.lua
+++ Regions/X-Raym_Export markers and regions as Davinci Resolve EDL file.lua
@@ -10,28 +10,62 @@
     Forum Thread https://forum.cockos.com/showthread.php?p=1670961
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.0
+ * Version: 1.1
 --]]
 
 --[[
  * Changelog:
+ * v1.1 (2019-02-11)
+  + Marker
+  + User Input
+  + Resolve Color Support
+  + From Time Selection start if any
+  + Better file handling
  * v1.0 (2018-12-21)
   + Initial Release
 --]]
 
- 
---------------------------------------------------------
--- DEBUG
--- -----
+-- USER CONFIG AREA ------------------------
 
-frame_duration = 1 / 25
+vars = {}
+vars.frame_rate = 25
+vars.offset = 3600
+
+popup = true
+
+---- END OF USER CONFIG AREA ----------------
+
+-- TODO: More Resolve Color Support
+
+ext_name = "XR_EDL_Marker_Resolve"
 
 -- Console Message
 function Msg(g)
   reaper.ShowConsoleMsg(tostring(g).."\n")
 end
 
+-- SAVE PRESET
+function SaveState()
+  for k, v in pairs( vars ) do
+    SaveExtState( k, v )
+  end
+end
 
+function SaveExtState( var, val)
+  reaper.SetExtState( ext_name, var, tostring(val), true )
+end
+
+function GetExtState( var, val )
+  if reaper.HasExtState( ext_name, var ) then
+    local t = type( val )
+    val = reaper.GetExtState( ext_name, var )
+    if t == "boolean" then val = toboolean( val )
+    elseif t == "number" then val = tonumber( val )
+    else
+    end
+  end
+  return val
+end
 
 ---------------------------------------------------------
 -- NUMBER
@@ -100,7 +134,73 @@ end
 ------------------------------------------------------------------
 -- COLOR FUNCTIONS
 -- ---------------
+--[[
+ * Converts an RGB color value to HSL. Conversion formula
+ * adapted from http://en.wikipedia.org/wiki/HSL_color_space.
+ * Assumes r, g, and b are contained in the set [0, 255] and
+ * returns h, s, and l in the set [0, 1].
+ *
+ * @param   Number  r       The red color value
+ * @param   Number  g       The green color value
+ * @param   Number  b       The blue color value
+ * @return  Array           The HSL representation
+]]
+function rgbToHsl(r, g, b, a)
+  r, g, b = r / 255, g / 255, b / 255
 
+  local max, min = math.max(r, g, b), math.min(r, g, b)
+  local h, s, l
+
+  l = (max + min) / 2
+
+  if max == min then
+    h, s = 0, 0 -- achromatic
+  else
+    local d = max - min
+    if l > 0.5 then s = d / (2 - max - min) else s = d / (max + min) end
+    if max == r then
+      h = (g - b) / d
+      if g < b then h = h + 6 end
+    elseif max == g then h = (b - r) / d + 2
+    elseif max == b then h = (r - g) / d + 4
+    end
+    h = h / 6
+  end
+
+  return h, s, l, a or 255
+end
+
+color_names = {}
+color_names[9] = "Red"
+color_names[39] = "Yellow"
+color_names[120] = "Green"
+color_names[180] = "Cyan"
+color_names[206] = "Blue"
+color_names[271] = "Purple"
+color_names[317] = "Pink"
+color_names[369] = "Red"
+
+function minTableKey( array, key )
+  local min = math.huge
+  local key = ""
+  for k, v in pairs( array ) do
+    if v < min then
+      min = v
+      key = k
+    end
+  end
+  return key, min
+end
+
+function GetClosestColorNameByHue( hue )
+  local name = ''
+  local diffs = {}
+  for i, v in pairs(color_names) do
+    diffs[i] = math.abs( i - hue )
+  end
+  local k, min = minTableKey(diffs)
+  return color_names[k]
+end
 
 --------------------------------------------------------------
 -- CREATE FILE
@@ -114,26 +214,34 @@ end
 
 -- Create File
 function create(f)
-  -- CREATE THE FILE
-  io.output(file)
   
   title = "TITLE: Timeline 1"
   FCM = "FCM: NON-DROP FRAME"
   export(f, title)
   export(f, FCM .. "\n")
+
+  frame_duration = 1 / vars.frame_rate
+  
+  ts_start, ts_end = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)  
   
   i=0
   repeat
     iRetval, bIsrgnOut, iPosOut, iRgnendOut, name, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(0,i)
     if iRetval >= 1 then
-      if bIsrgnOut == false then
-        start_time = "0" .. reaper.format_timestr_pos(iPosOut + 3600, "",5)
-        start_time_1 = "0" .. reaper.format_timestr_pos(iPosOut + 3600 + frame_duration, "",5)
+      if bIsrgnOut == false and iPosOut - ts_start > 0 then
+        if ts_end ~= 0 and iPosOut > ts_end then break end
+        start_time = "0" .. reaper.format_timestr_pos(iPosOut + vars.offset - ts_start, "",5)
+        start_time_1 = "0" .. reaper.format_timestr_pos(iPosOut + vars.offset + frame_duration - ts_start, "",5)
         end_time =  reaper.format_timestr_pos(iRgnendOut, "",5)
+        
+        local h, s, l = rgbToHsl( reaper.ColorFromNative(iColorOur) )
+        color_name = GetClosestColorNameByHue( h * 255 )
         -- [start time HH:MM:SS.F] [end time HH:MM:SS.F] [name]
         -- 001  001      V     C        01:00:00:00 01:00:00:01 01:00:00:00 01:00:00:01  
--- |C:ResolveColorBlue |M:Marker 1 |D:1
-        line = i .. "  001      V     C        " .. start_time .. " " .. start_time_1 .. " " .. start_time .. " " .. start_time_1 .. "  " .. "\n |C:ResolveColorBlue |M:" .. name .. " |D:1\n"
+        -- |C:ResolveColorBlue |M:Marker 1 |D:1
+        if name == "" then name = "Marker " .. i+1 end
+        line = i+1 .. "  001      V     C        " .. start_time .. " " .. start_time_1 .. " " .. start_time .. " " .. start_time_1 .. "  " .. "\n |C:ResolveColor" .. color_name .. " |M:" .. name .. " |D:1\n"
+        --line = i .. "  001      V     C        " .. start_time .. " " .. start_time_1 .. " " .. start_time .. " " .. start_time_1 .. "  " .. "\n |C:ResolveColorBlue |M:" .. name .. " |D:1\n"
         export(f, line)
       end
       i = i+1
@@ -152,9 +260,9 @@ end
 -- MAIN FUNCTION
 -- -------------
 
-function main() -- local (i, j, item, take, track)
+function main()
 
-  local f = io.open(file, "w")
+  f = io.open(file, "w")
 
   -- HTML FOLDER EXIST
   if f ~= nil then
@@ -162,7 +270,7 @@ function main() -- local (i, j, item, take, track)
   end
   
   -- CLOSE FILE
-  io.close(f)
+  f:close()
 
 end -- ENDFUNCTION MAIN
 
@@ -170,17 +278,34 @@ end -- ENDFUNCTION MAIN
 ----------------------------------------------------------------------
 -- RUN
 -- ---
+reaper.ClearConsole()
 
 -- Check if there is selected Items
 retval, count_markers, count_regions = reaper.CountProjectMarkers(0)
-
-if count_regions > 0 then
+if count_regions > 0 or count_markers > 0 then
 
   project_saved = IsProjectSaved() -- See if Project has been save and determine file paths
   if project_saved then
-    reaper.defer(main) -- Execute your main function
+    if popup then
+      vars.offset = GetExtState( "offset", vars.offset )
+      vars.frame_rate = GetExtState( "frame_rate", vars.frame_rate )
+
+      retval, retval_csv = reaper.GetUserInputs( "Export Markers to EDL", 2, "Framerate (fps):, Offset (s)", vars.frame_rate .. "," .. vars.offset)
+      if retval then
+        vars.frame_rate, vars.offset = retval_csv:match("([^,]+),([^,]+)")
+        if vars.frame_rate then
+          vars.frame_rate = tonumber( vars.frame_rate )
+          vars.offset = tonumber( vars.offset )
+          SaveState()
+        end
+      end
+    end
+
+    if not popup or (retval and vars.frame_rate and vars.offset) then
+      reaper.defer(main) -- Execute your main function
+    end
   end
 
 else
-  Msg("No regions in the project.")
+  Msg("No regions or markers in the project.")
 end -- ENDIF Item in project
---
+++ Regions/X-Raym_Insert markers at grid lines in time selection.lua
@@ -0,0 +1,111 @@
+--[[
+ * ReaScript Insert markers at grid lines in time selection
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2019-11-02)
+  + Initial Release
+--]]
+
+-- USER CONFIG AREA --------------
+
+measure_color = 1
+measure_r = 0
+measure_g = 128
+measure_b = 0
+
+beat_color = 1
+beat_r = 128
+beat_g = 0
+beat_b = 128
+
+------- END OF USER CONFIG AREA --
+
+function Msg(val)
+	reaper.ShowConsoleMsg(tostring(val) .."\n")
+end
+
+-- Round at two decimal
+-- By Igor Skoric
+function round( val, num )
+	local mult = 10^(num or 0)
+	if val >= 0 then return math.floor(val * mult + 0.5) / mult
+	else return math.ceil(val*mult-0.5) / mult end
+end
+
+function main()
+  
+  last_time = ts_start
+  
+  if measure_color ~= 0 then
+	  measure_color = reaper.ColorToNative( measure_r, measure_g, measure_b )|0x1000000
+  end
+  if beat_color ~= 0 then
+	  beat_color = reaper.ColorToNative( beat_r, beat_g, beat_b )|0x1000000
+  end
+  
+  time = reaper.BR_GetClosestGridDivision( last_time )
+  if time == ts_start then
+	local color = 0
+	local retval, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( proj, time )
+	if beat_color ~=0 then
+	 color = beat_color
+	end
+	
+	if measure_color ~= 0 and round(retval, 10) % cml == 0 then
+	 color = measure_color
+	end
+	reaper.AddProjectMarker2(0, false, time, 0, "", -1, color)
+  end
+  
+  -- INITIALIZE loop through selected items
+  repeat
+
+    local time = reaper.BR_GetNextGridDivision( last_time )
+    local color = 0
+    local retval, measures, cml, fullbeats, cdenom = reaper.TimeMap2_timeToBeats( proj, time )
+    
+    if beat_color ~=0 then
+	color = beat_color
+    end
+    
+    if measure_color ~= 0 and round(retval, 10) % cml == 0 then
+	color = measure_color
+    end
+    
+    if time <= ts_end then
+	reaper.AddProjectMarker2(0, false, time, 0, "", -1, color)
+    end
+    last_time = time
+    
+  until last_time > ts_end -- ENDLOOP through selected items
+
+end
+
+ts_start, ts_end = reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
+
+if ts_start ~= ts_end then
+	
+	reaper.ClearConsole()
+	
+	reaper.PreventUIRefresh(1)
+
+	reaper.Undo_BeginBlock()
+
+	main()
+
+	reaper.Undo_EndBlock("Insert markers at grid lines in time selection", -1)
+
+	reaper.PreventUIRefresh(-1)
+
+	reaper.UpdateArrange()
+
+end
