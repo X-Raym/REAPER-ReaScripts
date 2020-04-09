8c1138c0d5e0af8d43ea100786068dd0985487ed
X-Raym <x-raym@extremraym.com>
2020-04-09 12:22:16 +0200

    inital release

---
+++ Items Editing/X-Raym_Implode selected items on same track into takes preserving position.lua
@@ -0,0 +1,109 @@
+--[[
+ * ReaScript Name: Implode selected items on same track into takes preserving position
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Screenshot: https://i.imgur.com/NCQM0YA.gifv
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * File URI: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Items Editing (various)
+ * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1538604
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2020-04-09)
+  + Initial Release
+--]]
+
+function main() -- local (i, j, item, take, track)
+
+  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+  -- LOOP THROUGH SELECTED ITEMS
+  selected_items_count = reaper.CountSelectedMediaItems(0)
+  
+  tracks = {}
+  -- INITIALIZE loop through selected items
+  -- Select tracks with selected items
+  for i = 0, selected_items_count - 1  do
+    -- GET ITEMS
+    item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+
+    -- GET ITEM PARENT TRACK AND SELECT IT
+    track = reaper.GetMediaItem_Track(item)
+    
+    track_GUID = reaper.GetTrackGUID( track ) 
+    
+    tracks[track_GUID] = track
+    
+  end -- ENDLOOP through selected items
+
+  reaper.Main_OnCommand(40547, 0) -- Item properties: Loop section of audio item source
+
+  -- LOOP TRHOUGH SELECTED TRACKS
+  for k,track in pairs( tracks )  do
+
+    count_items_on_track = reaper.CountTrackMediaItems(track)
+
+    -- REINITILIAZE THE TABLE
+    item_to_delete = {}
+    sel_items_on_tracks_end = 0
+    
+    min_pos = nil
+    max_end = nil    
+    
+    for j = 0, count_items_on_track - 1  do
+    
+      item = reaper.GetTrackMediaItem(track, j)
+      
+      item_pos  = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+      item_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+      item_end = item_pos + item_len
+      
+      if not min_pos then min_pos = item_pos else min_pos = math.min( min_pos, item_pos ) end
+      if not max_end then max_end = item_end else max_end = math.max( max_end, item_end ) end
+    
+    end
+
+
+    -- LOOP THROUGH ITEMS ON TRACKS AND STORE SELECTED ITEMS (for later moving) AND OFFSET
+    for j = 0, count_items_on_track - 1  do
+
+      item = reaper.GetTrackMediaItem(track, j)
+
+      if reaper.IsMediaItemSelected(item) then
+        
+        -- CHECK IF IT ITEM END IS AFTER PREVIOUS ITEM ENDS
+        item_on_tracks_pos = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+        item_on_tracks_len = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+        item_on_tracks_end = item_on_tracks_pos + item_on_tracks_len
+        
+        take = reaper.GetActiveTake( item )
+
+        reaper.BR_SetItemEdges(item, min_pos, max_end)
+        
+      end     
+
+    end    
+  
+  end -- ENDLOOP through selected tracks
+  
+  reaper.Main_OnCommand(40543,0 ) -- Take: Implode items on same track into takes
+
+  reaper.Undo_EndBlock("Expand first selected item per track to end of last selected ones and delete inbetween ones", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+end
+
+--[[ <==== INITIAL SAVE AND RESTORE ----- ]]
+
+reaper.PreventUIRefresh(1) -- Prevent UI refreshing. Uncomment it only if the script works.
+
+main() -- Execute your main function
+
+reaper.PreventUIRefresh(-1) -- Restore UI Refresh. Uncomment it only if the script works.
+
+reaper.UpdateArrange() -- Update the arrangement (often needed)
