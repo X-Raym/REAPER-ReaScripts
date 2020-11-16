9a09396c79645ed02395bc66be23e9e486aa6aa3
X-Raym <x-raym@extremraym.com>
2020-11-16 19:26:18 +0100

    Update X-Raym_Change last touched track layout_background.lua

--- Various/X-Raym_Change last touched track layout_background.lua
+++ Various/X-Raym_Change last touched track layout_background.lua
@@ -7,11 +7,13 @@
  * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.0.3
+ * Version: 2.0.3
 --]]
  
 --[[
  * Changelog:
+ * v2.0.3 (2020-11-16)
+  # ProjExtState base
  * v1.0.3 (2020-11-16)
   + Better project tab
  * v1.0.2 (2020-11-16)
@@ -22,10 +24,15 @@
   + Initial Release
 --]]
 
+-- NOTE: Known issue: Project tab have to be in focus for changing layout. https://forum.cockos.com/showthread.php?p=2365622#post2365622
+
 -- USER CONFIG AREA ---------------------
 mcp_layout = "1. Classic Default MCP - Blue Fader"
 tcp_layout = "1. Classic Default TCP (vertical meters) - Blue Fader"
+console = false
 -----------------------------------------
+
+ext_name = "XR_LastTouchedTrackLayout"
 
 function Msg(val)
   reaper.ShowConsoleMsg(tostring( val ).."\n")
@@ -42,58 +49,89 @@ end
 
 function Exit()
   SetButtonState()
-  if last_track and reaper.ValidatePtr(last_track, 'MediaTrack*') then
-    if mcp_layout and mcp_layout_last then
-      local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_MCP_LAYOUT", mcp_layout_last, true )
+  reaper.ClearConsole()
+  local cur_proj, projfn = reaper.EnumProjects( -1 )
+  local proj
+  local i = 0
+  repeat
+    proj, projfn = reaper.EnumProjects( i )
+    reaper.SelectProjectInstance( proj )
+    Msg("\n-------------")
+    Msg(projfn)
+    if proj then
+      local ext_state_retval, last_track_guid = reaper.GetProjExtState(proj, ext_name, "track_guid")
+      Msg(i)
+      Msg(last_track_guid)
+      local last_track = reaper.BR_GetMediaTrackByGUID( i, last_track_guid )
+      Msg(last_track)
+      if last_track and reaper.ValidatePtr2(i,last_track, 'MediaTrack*') then
+        Msg("VALID")
+        retval, name = reaper.GetTrackName( last_track )
+        Msg(name)
+        local ext_state_retval, last_track_mcp = reaper.GetProjExtState(i, ext_name, "mcp_layout")
+        local ext_state_retval, last_track_tcp = reaper.GetProjExtState(i, ext_name, "tcp_layout")
+        Msg(last_track_mcp)
+        Msg(last_track_tcp)
+        if last_track_mcp == "Default" then last_track_mcp = "" end
+        if last_track_tcp == "Default" then last_track_tcp = "" end
+        if mcp_layout and last_track_mcp then
+          local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_MCP_LAYOUT", last_track_mcp, true )
+          Msg(retval)
+        end
+        if tcp_layout and last_track_tcp then
+          local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_TCP_LAYOUT", last_track_tcp, true )
+          Msg(retval)
+        end
+      end
+      i = i + 1
     end
-    if tcp_layout and tcp_layout_last then
-      local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_TCP_LAYOUT", tcp_layout_last, true )
-    end
-  end
+  until not proj
+  reaper.SelectProjectInstance( cur_proj )
 end
 
 -- Main Function (which loop in background)
 function main()
-  cur_proj, _ = reaper.EnumProjects( -1 )
-  track = reaper.GetLastTouchedTrack()
-  if track and (track ~= last_track or cur_proj ~= last_proj) then
-    local is_last_track_valid = false
-    if last_track and reaper.ValidatePtr2(cur_proj, last_track, 'MediaTrack*') then
-      is_last_track_valid = true
-    elseif last_track and last_proj and reaper.ValidatePtr(last_proj, 'ReaProject*') and reaper.ValidatePtr2(last_proj, last_track, 'MediaTrack*') then
-      is_last_track_valid = true
+
+  local track = reaper.GetLastTouchedTrack()
+  local ext_state_retval, last_track_guid = reaper.GetProjExtState(-1, ext_name, "track_guid")
+  local last_track = reaper.BR_GetMediaTrackByGUID( -1, last_track_guid )
+  if track and (track ~= last_track or not first_run) then
+    if last_track and reaper.ValidatePtr(last_track, 'MediaTrack*') then
+      ext_state_retval, last_track_tcp = reaper.GetProjExtState(-1, ext_name, "tcp_layout")
+      ext_state_retval, last_track_mcp = reaper.GetProjExtState(-1, ext_name, "mcp_layout")
+      if last_track_mcp == "Default" then last_track_mcp = "" end
+      if last_track_tcp == "Default" then last_track_tcp = "" end
+      last_track =   reaper.GetTrack(0, reaper.GetMediaTrackInfo_Value(last_track, "IP_TRACKNUMBER") - 1 )
+      local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_MCP_LAYOUT", last_track_mcp, true )
+      local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_TCP_LAYOUT", last_track_tcp, true )
     end
     
-    if is_last_track_valid then
-      if mcp_layout_last then
-        local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_MCP_LAYOUT", mcp_layout_last, true )
-      end
-      if tcp_layout_last then
-        local retval, _ = reaper.GetSetMediaTrackInfo_String( last_track, "P_TCP_LAYOUT", tcp_layout_last, true )
-      end
-    end
+    -- Backup Track Layout
+    local retval, mcp_layout_last = reaper.GetSetMediaTrackInfo_String( track, "P_MCP_LAYOUT", "", false )
+    local retval, tcp_layout_last = reaper.GetSetMediaTrackInfo_String( track, "P_TCP_LAYOUT", "", false )
+    if mcp_layout_last == "" then mcp_layout_last = "Default" end
+    if tcp_layout_last == "" then tcp_layout_last = "Default" end
     
-    if track ~= last_track or cur_proj ~= last_proj then
-      retval, mcp_layout_last = reaper.GetSetMediaTrackInfo_String( track, "P_MCP_LAYOUT", "", false )
-      retval, tcp_layout_last = reaper.GetSetMediaTrackInfo_String( track, "P_TCP_LAYOUT", "", false )
-      if mcp_layout then
-        local retval, _ = reaper.GetSetMediaTrackInfo_String( track, "P_MCP_LAYOUT", mcp_layout, true )
-      end
-      if tcp_layout then
-        local retval, _ = reaper.GetSetMediaTrackInfo_String( track, "P_TCP_LAYOUT", tcp_layout, true )
-      end
+    reaper.SetProjExtState(cur_proj, ext_name, "tcp_layout", tcp_layout_last)
+    reaper.SetProjExtState(cur_proj, ext_name, "mcp_layout", mcp_layout_last)
+    reaper.SetProjExtState(cur_proj, ext_name, "track_guid", reaper.GetTrackGUID(track))
+    
+    if mcp_layout then
+      local retval, _ = reaper.GetSetMediaTrackInfo_String( track, "P_MCP_LAYOUT", mcp_layout, true )
+    end
+    if tcp_layout then
+      local retval, _ = reaper.GetSetMediaTrackInfo_String( track, "P_TCP_LAYOUT", tcp_layout, true )
     end
     
   end
   
-  last_track = track
-  last_proj = cur_proj
+  if first_run then first_run = false end
   
   reaper.defer( main )
   
 end
 
-
+first_run = true
 
 -- RUN
 function Init()
