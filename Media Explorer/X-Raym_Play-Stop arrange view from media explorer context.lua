86faa07438234450d3b9127d279bf9671f73b8f6
X-Raym <x-raym@extremraym.com>
2020-12-17 20:32:47 +0100

    Create X-Raym_Play-Stop arrange view from media explorer context.lua

---
+++ Media Explorer/X-Raym_Play-Stop arrange view from media explorer context.lua
@@ -0,0 +1,19 @@
+--[[
+ * ReaScript Name: Play-Stop arrange view from media explorer context
+ * Author: X-Raym
+ * Author URI: http://www.extremraym.com/
+ * Repository: GitHub > X-Raym > REAPER-ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+ * Main: mediaexplorer
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2020-12-17)
+  + Initial Release
+--]]
+
+reaper.Main_OnCommand(40044, 0) -- Preview: Play/stop
