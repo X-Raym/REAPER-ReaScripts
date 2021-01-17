0a77eee0bf213d326fbbbc67df19d17462b35bf7
X-Raym <x-raym@extremraym.com>
2021-01-17 19:04:17 +0100

    update

--- Items Editing/X-Raym_Set selected takes playrate keeping snap offset position and adjusting length.lua
+++ Items Editing/X-Raym_Set selected takes playrate keeping snap offset position and adjusting length.lua
@@ -2,7 +2,7 @@
  * ReaScript Name: Set selected takes playrate keeping snap offset position and adjusting length
  * Description: A way to expand items in selection without moving their synch point, determined by snap offset and content at snap offset.
  * Instructions: Run.
- * Screenshot: http://i.imgur.com/O02MKFr.gifv
+ * Screenshot: https://i.imgur.com/VrDKb9h.gif
  * Author: X-Raym
  * Author URI: https://www.extremraym.com
  * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
@@ -11,11 +11,13 @@
  * Forum Thread: Scripts: Items Editing (various)
  * Forum Thread URI: http://forum.cockos.com/showthread.php?t=163363
  * REAPER: 5.0
- * Version: 1.0.1
+ * Version: 2.0
 --]]
  
 --[[
  * Changelog:
+ * v2.0 (2021-01-18)
+  # Support relative mode
  * v1.0.1 (2020-01-18)
   # prompt variable fix
  * v1.0 (2017-01-26)
@@ -28,104 +30,145 @@
 --]]
 
 -- ------ USER AREA =====>
-
-coef = 1 -- number >= 0
-prompt = true -- true/false
-
+mod1 = "absolute" -- Set the primary mod that will be defined if no prefix character. Values are "absolute" or "relative".
+mod2 = "relative"
+mod2_prefix = "+" -- Prefix to enter the secondary mod
+input_default = "1" -- "" means no character aka relative per default.
+popup = true
 -- <===== USER AREA ------
 
-function main(new_rate)
-    
-  for i, item in ipairs ( items ) do
+ext_name = 'XR_SetTakePlayrate'
 
-    -- CHECK IF ITEM HAS TAKE OR IF IT IS EMPTY ITEM
-    take = reaper.GetActiveTake( item )
+function main()
+  
+  -- INITIALIZE loop through selected items
+  for i = 0, sel_items_count-1 do
+    -- GET ITEMS
+    item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+  
+    take = reaper.GetActiveTake(item)
+  
     if take then
-      take_rate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
-    else
-      take_rate = 1
-    end
-
-    k = take_rate / new_rate
-
-    -- INITIAL ITEM INFOS
-    item_fadein = reaper.GetMediaItemInfo_Value(item, "D_FADEINLEN")
-    item_fadeout = reaper.GetMediaItemInfo_Value(item, "D_FADEOUTLEN")
-    item_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
-    item_length = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
-    item_snap = reaper.GetMediaItemInfo_Value(item, "D_SNAPOFFSET")
-    
-    -- SNAP
-    item_snap_absolute = item_snap + item_position
-
-    new_length = item_length * k
-    
-    new_rate = new_rate
-    new_snap_offset = item_snap * k
-    new_fadein = item_fadein * k
-    new_fadeout = item_fadeout * k
-    
-    new_pos = item_snap_absolute - new_snap_offset
-    
-    if item_snap == 0 then
-      new_pos = item_position
-    end
-    
-    reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE", new_rate)
-    reaper.SetMediaItemInfo_Value(item, "D_POSITION", new_pos)
-    reaper.SetMediaItemInfo_Value(item, "D_LENGTH", new_length)
-    reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", new_snap_offset)
-    reaper.SetMediaItemInfo_Value(item, "D_FADEINLEN", new_fadein)
-    reaper.SetMediaItemInfo_Value(item, "D_FADEOUTLEN", new_fadeout)
-  
-  end
-
-end
-
-function SaveSelectedItems (table)
-  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
-    table[i+1] = reaper.GetSelectedMediaItem(0, i)
-  end
-end
-
-selected_items_count = reaper.CountSelectedMediaItems(0)
-
-if selected_items_count > 0 then
-    
-    if prompt then
-    retval, coef = reaper.GetUserInputs("Set Take Rate", 1, "Value ( > 0 )", tostring(coef) )
-  end 
-
-  if retval or prompt == false then
-    
-    coef = tonumber(coef)
-    
-    if coef ~= nil then
-    
-      coef = math.abs(coef)
-    
-      if coef ~= 0 then
-
-        reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
-
-        items = {}
-  
-        SaveSelectedItems( items )
-    
-        reaper.PreventUIRefresh(1)
-
-        main( coef ) -- Execute your main function
-
-        reaper.PreventUIRefresh(-1)
-
-        reaper.UpdateArrange() -- Update the arrangement (often needed)
-
-        reaper.Undo_EndBlock("Set selected takes playrate keeping snap offset position and adjusting length", -1) -- End of the undo block. Leave it at the bottom of your main function.
       
+      item_length  = reaper.GetMediaItemInfo_Value( item, 'D_LENGTH')
+      take_rate = reaper.GetMediaItemTakeInfo_Value(  take, 'D_PLAYRATE')
+      item_fade_in = reaper.GetMediaItemInfo_Value( item, "D_FADEINLEN" )
+      item_fade_out = reaper.GetMediaItemInfo_Value( item, "D_FADEOUTLEN" )
+
+      item_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+      item_snap = reaper.GetMediaItemInfo_Value(item, "D_SNAPOFFSET")
+      item_snap_absolute = item_snap + item_position
+      
+      rate = user_input_num
+      
+      if set then
+        original_rate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
+        new_rate = rate
+        if new_rate > 0 then
+          reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE",new_rate)
+          original_len = item_length * original_rate -- consider that original len is what item 1 should be
+          reaper.SetMediaItemInfo_Value( item, 'D_LENGTH', original_len / new_rate)
+          reaper.SetMediaItemInfo_Value( item, "D_FADEINLEN", item_fade_in  * original_rate / new_rate)
+          reaper.SetMediaItemInfo_Value( item, "D_FADEOUTLEN", item_fade_out * original_rate / new_rate)
+
+          k = take_rate / new_rate
+          new_snap_offset = item_snap * k
+          new_pos = item_snap_absolute - new_snap_offset
+          reaper.SetMediaItemInfo_Value(item, "D_POSITION", new_pos)
+          reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", new_snap_offset)
+
+        end
+      else
+        original_rate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
+        new_rate = rate + original_rate
+        if new_rate > 0 then
+          reaper.SetMediaItemTakeInfo_Value(take, "D_PLAYRATE",new_rate)
+          original_len = item_length * original_rate
+          reaper.SetMediaItemInfo_Value( item, 'D_LENGTH', original_len / new_rate)
+          reaper.SetMediaItemInfo_Value( item, "D_FADEINLEN", item_fade_in  * original_rate / new_rate)
+          reaper.SetMediaItemInfo_Value( item, "D_FADEOUTLEN", item_fade_out * original_rate / new_rate)
+
+          k = take_rate / new_rate
+          new_snap_offset = item_snap * k
+          new_pos = item_snap_absolute - new_snap_offset
+          reaper.SetMediaItemInfo_Value(item, "D_POSITION", new_pos)
+          reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", new_snap_offset)
+
+        end
+      end
+
+    end -- if take
+
+  end -- ENDLOOP through selected items  
+
+end
+
+-- START
+
+function Init()
+  
+  sel_items_count = reaper.CountSelectedMediaItems(0)
+  
+  if sel_items_count > 0 then
+  
+    if popup then
+      if reaper.HasExtState( ext_name, "input_default" ) then
+        input_default = reaper.GetExtState( ext_name, "input_default" )
+      end
+      retval, user_input_str = reaper.GetUserInputs("Set/Offset Take Rate Value", 1, "Value (" .. mod2_prefix .." for " .. mod2 .. ")", input_default) 
+    else
+      user_input_str = input_default
+    end
+        
+    if not popup or retval then -- if user complete the fields
+      
+      x, y = string.find(user_input_str, mod2_prefix)
+          
+      if mod1 == "absolute" then
+        if x ~= nil then -- set
+          set = false
+        else -- offset
+          set = true 
+        end
       end
       
-    end
+      if mod1 == "relative" then
+        if x ~= nil then -- set
+          set = true
+        else -- offset
+          set = false 
+        end
+      end
+      
+      user_input_num = user_input_str:gsub(mod2_prefix, "")
+      user_input_num = tonumber(user_input_num)
+  
+      if user_input_num and user_input_num ~= 0 then
+      
+        reaper.PreventUIRefresh(1)
+        
+        reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+    
+        main() -- Execute your main function
+
+        if popup then
+          reaper.SetExtState( ext_name, "input_default", user_input_str, true )
+        end
+        
+        reaper.Undo_EndBlock("Set selected takes playrate keeping snap offset position and adjusting length", -1) -- End of the undo block. Leave it at the bottom of your main function.
+    
+        reaper.PreventUIRefresh(-1)
+    
+        reaper.UpdateArrange() -- Update the arrangement (often needed)
+      
+      end
   
     end
+    
+  end
   
+end
+
+if not dofile_execution then
+  Init()
 end
--- Items Properties/X-Raym_Set or offset selected takes rate by semitone adjusting item length and fades and clearing preserve pitch.lua
+++ Items Properties/X-Raym_Set or offset selected takes rate by semitone adjusting item length and fades and clearing preserve pitch.lua
@@ -10,11 +10,14 @@
  * Forum Thread: Scripts: Items Properties (various)
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1574814
  * REAPER: 5.0
- * Version: 1.1.1
+ * Version: 1.2
 --]]
  
 --[[
  * Changelog:
+ * v1.2 (2020-01-17)
+  + Works from snap offset pos
+  + Save last input
  * v1.1.1 (2020-06-10)
   + popup variable in user area
  * v1.1 (2020-06-08)
@@ -35,6 +38,7 @@ input_default = "0" -- "" means no character aka relative per default.
 popup = true
 -- <===== USER AREA ------
  
+ext_name = 'XR_SetTakePlayrateSemitones'
 
 function main()
 
@@ -53,6 +57,10 @@ function main()
       take_rate = reaper.GetMediaItemTakeInfo_Value(  take, 'D_PLAYRATE')
       item_fade_in = reaper.GetMediaItemInfo_Value( item, "D_FADEINLEN" )
       item_fade_out = reaper.GetMediaItemInfo_Value( item, "D_FADEOUTLEN" )
+
+      item_position = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+      item_snap = reaper.GetMediaItemInfo_Value(item, "D_SNAPOFFSET")
+      item_snap_absolute = item_snap + item_position
       
       rate = 2^(user_input_num/12)
       
@@ -64,6 +72,13 @@ function main()
         reaper.SetMediaItemInfo_Value( item, 'D_LENGTH', original_len / new_rate)
         reaper.SetMediaItemInfo_Value( item, "D_FADEINLEN", item_fade_in  * original_rate / new_rate)
         reaper.SetMediaItemInfo_Value( item, "D_FADEOUTLEN", item_fade_out * original_rate / new_rate)
+
+    k = take_rate / new_rate
+        new_snap_offset = item_snap * k
+        new_pos = item_snap_absolute - new_snap_offset
+        reaper.SetMediaItemInfo_Value(item, "D_POSITION", new_pos)
+        reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", new_snap_offset)
+
       else
         original_rate = reaper.GetMediaItemTakeInfo_Value(take, "D_PLAYRATE")
         new_rate = rate * original_rate
@@ -72,6 +87,13 @@ function main()
         reaper.SetMediaItemInfo_Value( item, 'D_LENGTH', original_len / new_rate)
         reaper.SetMediaItemInfo_Value( item, "D_FADEINLEN", item_fade_in  * original_rate / new_rate)
         reaper.SetMediaItemInfo_Value( item, "D_FADEOUTLEN", item_fade_out * original_rate / new_rate)
+
+    k = take_rate / new_rate
+        new_snap_offset = item_snap * k
+        new_pos = item_snap_absolute - new_snap_offset
+        reaper.SetMediaItemInfo_Value(item, "D_POSITION", new_pos)
+        reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", new_snap_offset)
+
       end
 
     end -- if take
@@ -89,6 +111,9 @@ function Init()
   if sel_items_count > 0 then
   
     if popup then
+      if reaper.HasExtState( ext_name, "input_default" ) then
+        input_default = reaper.GetExtState( ext_name, "input_default" )
+      end
       retval, user_input_str = reaper.GetUserInputs("Set/Offset Take Rate Value", 1, "Value (" .. mod2_prefix .." for " .. mod2 .. ")", input_default) 
     else
       user_input_str = input_default
@@ -114,9 +139,8 @@ function Init()
         end
       end
       
-      user_input_str = user_input_str:gsub(mod2_prefix, "")
-      
-      user_input_num = tonumber(user_input_str)
+      user_input_num = user_input_str:gsub(mod2_prefix, "")  
+      user_input_num = tonumber(user_input_num)
   
       if user_input_num then
       
@@ -125,6 +149,10 @@ function Init()
         reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
     
         main() -- Execute your main function
+
+        if popup then
+          reaper.SetExtState( ext_name, "input_default", user_input_str, true )
+        end
         
         reaper.Undo_EndBlock("Set or offset selected takes rate by semitone adjusting item length and fades and clearing preserve pitch", -1) -- End of the undo block. Leave it at the bottom of your main function.
     
