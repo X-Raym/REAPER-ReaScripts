f964c2b1647d8235575aa60e8ef77e1bc298d7f7
X-Raym <x-raym@extremraym.com>
2021-02-06 11:23:56 +0100

    Update X-Raym_Toggle selected tracks solo (no undo).lua

--- Track Properties/X-Raym_Toggle selected tracks solo (no undo).lua
+++ Track Properties/X-Raym_Toggle selected tracks solo (no undo).lua
@@ -6,7 +6,7 @@
  * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.0
+ * Version: 1.0.1
 --]]
  
 
@@ -16,16 +16,18 @@
   + Initial Release
 --]]
 
-reaper.PreventUIRefresh(1)
-count_sel_tracks = reaper.CountSelectedTracks(0)
+function Main()
+  reaper.PreventUIRefresh(1)
+  count_sel_tracks = reaper.CountSelectedTracks(0)
 
-for i = 0, count_sel_tracks - 1 do
-  local track = reaper.GetSelectedTrack(0,i)
-  local solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
-  if solo == 0 then solo = 1 else solo = 0 end
-  reaper.SetMediaTrackInfo_Value(track, "I_SOLO",solo)
+  for i = 0, count_sel_tracks - 1 do
+    local track = reaper.GetSelectedTrack(0,i)
+    local solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
+    if solo == 0 then solo = 1 else solo = 0 end
+    reaper.SetMediaTrackInfo_Value(track, "I_SOLO",solo)
+  end
+  reaper.TrackList_AdjustWindows(false)
+  reaper.UpdateArrange()
+  reaper.PreventUIRefresh(-1)
 end
-
-reaper.TrackList_AdjustWindows(false)
-reaper.UpdateArrange()
-reaper.PreventUIRefresh(-1)
+reaper.defer(Main)
