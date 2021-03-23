074823f237ea4d041ef3d20b7b1251bab230fd6c
X-Raym <x-raym@extremraym.com>
2021-03-23 17:47:00 +0100

    Update X-Raym_Set or Offset selected envelope points value.lua

--- Envelopes/X-Raym_Set or Offset selected envelope points value.lua
+++ Envelopes/X-Raym_Set or Offset selected envelope points value.lua
@@ -1,7 +1,6 @@
 --[[
  * ReaScript Name: Set or Offset selected envelope point value in selected envelope
- * Description: A pop up to let you put offset values for selected item points.
- * Instructions: Write values you want. Use "+" sign for relative value (the value is added to the original), no sign for absolute Exemple: -6 is absolute, or +-6 is relative. Don't use percentage. Example: writte "60" for 60%. You can customize default behavior (relative or absolute mod and prefix character) in the User Area of this script.
+ * About: A pop up to let you put offset vals for selected item points. Write vals you want. Use "+" sign for relative val (the val is added to the original), no sign for absolute Exemple: -6 is absolute, or +-6 is relative. Don't use percentage. Example: writte "60" for 60%. You can customize default behavior (relative or absolute mod and prefix character) in the User Area of this script.
  * Author: X-Raym
  * Author URI: http://extremraym.com
  * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
@@ -9,13 +8,16 @@
  * Licence: GPL v3
  * Forum Thread: ReaScript: Set/Offset selected envelope points values
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1487882#post1487882
- * REAPER: 5.0 pre 9
- * Extensions: None
- * Version: 1.8.1
+ * Version: 2.0
 ]]
 
 --[[
  * Changelog:
+ * v2.0 (2020-03-23)
+  + new core
+  + Automation items support
+  + Save/restore last input
+  + preset file support
  * v1.8.1 (2020-01-07)
   # No debug
  * v1.8 (2020-01-07)
@@ -42,300 +44,170 @@
 -- ------ USER AREA =====>
 mod1 = "absolute" -- Set the primary mod that will be defined if no prefix character. Values are "absolute" or "relative".
 mod2_prefix = "+" -- Prefix to enter the secondary mod
-input_default = "" -- "" means no character aka relative per default.
+input = "" -- "" means no character aka relative per default.
 popup = true -- true/false
+console = true
 -- <===== USER AREA ------
 
-function set_point_value()
+other_mod = {
+  absolute = "offset",
+  relative = "set"
+}
 
-  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+ext_name = "XR_SetOffsetSelPointValue"
 
-  already_set = false
+local env_width_db_scale = {}
+env_width_db_scale["Volume"] = true
+env_width_db_scale["Volume (Pre-FX)"] = true
+env_width_db_scale["Send Volume"] = true
 
-  envelope = reaper.GetSelectedEnvelope(0)
+local env_no_mulitply = {}
+env_no_mulitply["Mute"] = true
+env_no_mulitply["Send Mute"] = true
+env_no_mulitply["Pitch"] = true
+env_no_mulitply["Tempo map"] = true
 
-  if envelope ~= nil then
+local env_multiply = {}
+env_multiply["Width"] = -1
+env_multiply["Width (Pre-FX)"] = -1
+env_multiply["Pan"] = -1
+env_multiply["Pan (Pre-FX)"] = -1
+env_multiply["Pan (Left)"] = -1
+env_multiply["Pan (Right)"] = -1
+env_multiply["Pan (Left, Pre-FX)"] = -1
+env_multiply["Pan (Right, Pre-FX)"] = -1
+env_multiply["Send Pan"] = -1
 
-    env_point_count = reaper.CountEnvelopePoints(envelope)
+function Msg(g)
+  if console then
+    reaper.ShowConsoleMsg(tostring(g).."\n")
+  end
+end
 
-    if popup then
-      retval, user_input_str = reaper.GetUserInputs("Set or Offset point value", 1, "Value ?", input_default) -- We suppose that the user know the scale he want
-    else
-      user_input_str = input_default
-    end
+function dBFromVal(val) return 20*math.log(val, 10) end
+function ValFromdB(dB_val) return 10^(dB_val/20) end
 
-    if retval or not popup then
+function LimitNumber( val, min, max )
+  return math.min(math.max(min, val), max)
+end
 
-    x, y = string.find(user_input_str, mod2_prefix)
-    --reaper.ShowConsoleMsg(user_input_str)
+function ProcessPoint( env, env_name, val, user_input_num, set, min, max )
+  -- Pre Process val
+  if env_width_db_scale[env_name] then
+    if reaper.GetEnvelopeScalingMode(env) == 1 then val = reaper.ScaleFromEnvelopeMode(1, val) end
+    val = dBFromVal( val )
+  end
 
-    if mod1 == "absolute" then
-      if x ~= nil then -- set
-        set = false
-      else -- offset
-        set = true
-      end
-    end
+  -- Human Format
+  local user_val = user_input_num * (env_multiply[env_name] or 1)
 
-    if mod1 == "relative" then
-      if x ~= nil then -- set
-        set = true
-      else -- offset
-        set = false
-      end
-    end
+  if set then val = 0 end
 
-    user_input_str = user_input_str:gsub(mod2_prefix, "")
-    --reaper.ShowConsoleMsg(user_input_str)
-    user_input_num = tonumber(user_input_str)
+  if env_no_mulitply[env_name] or env_width_db_scale[env_name] then
+    val = val + user_val
+  else
+    val = val + user_val/100
+  end
 
-    -- IF VALID INPUT
-    if user_input_num ~= nil then
+  -- post Process val
+  if env_width_db_scale[env_name] then
+    if reaper.GetEnvelopeScalingMode(env) == 1 then val = reaper.ScaleToEnvelopeMode(1, val) end
+    val = ValFromdB( val )
+  end
 
-      -- GET ENVELOPE RANGE -- HERE IT IS
-      envelopeName = ""
-      retval, envelopeName = reaper.GetEnvelopeName(envelope, envelopeName)
-      --msg_stl("Envelope name", envelopeName, 1)
-      --reaper.ShowConsoleMsg(envelopeName)
+  val = LimitNumber( val, min, max )
+  return val
+end
 
-      if envelopeName == "Volume" or envelopeName == "Volume (Pre-FX)" or envelopeName == "Send Volume" then
-        already_set = true
+function ProcessEnv(env, set, user_input_num)
 
-        env_scale = reaper.GetEnvelopeScalingMode(envelope)
+  local retval, env_name = reaper.GetEnvelopeName(env, "")
+  local env_point_count = reaper.CountEnvelopePoints(env)
 
-        for i = 0, env_point_count - 1 do
+  br_env = reaper.BR_EnvAlloc( env, false )
+  local active, visible, armed, inLane, laneHeight, defaultShape, min, max, centerValue, type_point, faderScaling, automationItemsOptions = reaper.BR_EnvGetProperties( br_env )
+  reaper.BR_EnvFree( br_env, false )
 
-        -- IDX 0 doesnt seem to work
-        retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
+  for i = 0, env_point_count - 1 do
+    local retval, time, val, shape, tension, selected = reaper.GetEnvelopePoint(env,i)
+    if selected then
 
-        if env_scale == 1 then valueOut = reaper.ScaleFromEnvelopeMode(1, valueOut) end
+      val = ProcessPoint( env, env_name, val, user_input_num, set, min, max )
 
-        if set == true then
-          valueOut = math.exp(0*0.115129254)
-        end
-
-        if selectedOut == true then
-
-          -- CALC
-          OldVol = valueOut
-          OldVolDB = 20*(math.log(OldVol, 10)) -- thanks to spk77!
-
-          --msg_ftl("Old vol db:", OldVolDB, 1)
-
-          calc = OldVolDB + user_input_num
-          --msg_ftl("Calc", calc, 1)
-          --reaper.ShowConsoleMsg(tostring(calc))
-
-          if calc <= -146 then
-          valueIn = 0
-          --msg_s("Volume <= -146")
-          end
-          if calc >= 6 then
-          valueIn = 2
-          --msg_s("+12 <= Volume")
-          end
-          if calc < 6 and calc > -146 then
-          valueIn = math.exp(calc*0.115129254)
-          --msg_s("-146 < Volume < +12")
-          end
-          ----msg_ftl("Value ouput", valueIn, 1)
-          -- SET POINT VALUE
-
-          if env_scale == 1 then valueIn = reaper.ScaleToEnvelopeMode(1, valueIn) end
-
-          reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-
-        end -- ENDIF point is selected
-        end -- END Loop
-      end -- ENDIF Volume
-
-      if envelopeName == "Mute" or envelopeName == "Send Mute" then
-        already_set = true
-
-        for i = 0, env_point_count - 1 do
-
-        -- IDX 0 doesnt seem to work
-        retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
-        if set == true then
-          valueOut = 0
-        end
-
-        if selectedOut == true then
-
-          -- CALC
-          calc = valueOut + user_input_num
-
-          if calc < 0 then
-          valueIn = 0
-          --msg_s("Mute = 0")
-          end
-          if calc >= 1 then
-          valueIn = 1
-          --msg_s("Mute = 1")
-          end
-          if calc < 0.5 then
-          valueIn = 0
-          --msg_s("Mute Floor < 0.5")
-          end
-          if calc >= 0.5 then
-          valueIn = 1
-          --msg_s("0.5 <= Mute Floor")
-          end
-
-          -- SET POINT VALUE
-          reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-        end -- ENDIF point is selected
-        end -- END Loop
-      end -- ENDIF Mute
-
-      if envelopeName == "Width" or envelopeName == "Width (Pre-FX)" or envelopeName == "Pan" or envelopeName == "Pan (Pre-FX)" or envelopeName == "Pan (Left)" or envelopeName == "Pan (Right)" or envelopeName == "Pan (Left, Pre-FX)" or envelopeName == "Pan (Right, Pre-FX)" or envelopeName == "Send Pan" then
-        already_set = true
-
-        for i = 0, env_point_count - 1 do
-
-        -- IDX 0 doesnt seem to work
-        retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
-        if set == true then
-          valueOut = 0
-
-        end
-
-        if selectedOut == true then
-
-          -- CALC
-          calc = valueOut*100 - user_input_num
-
-          if calc <= -100 then
-          valueIn = - 1.0
-          --msg_s("Pan/Width <= -100")
-          end
-          if calc >= 100 then
-          valueIn = 1.0
-          --msg_s("Pan/Width >= 100")
-          end
-          if calc < 100 and calc > -100 then
-          valueIn = calc / 100
-          --msg_s("-100 < Pan/Width < 100")
-          end
-
-          -- SET POINT VALUE
-          reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-        end -- ENDIF point is selected
-        end -- END Loop
-      end -- ENDIF Pan or Width
-
-      if envelopeName == "Pitch" then
-        already_set = true
-
-
-        for i = 0, env_point_count - 1 do
-
-        -- IDX 0 doesnt seem to work
-        retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
-        if set == true then
-          valueOut = 0
-        end
-
-        if selectedOut == true then
-
-          -- CALC
-          calc = valueOut + user_input_num
-          --msg_ftl("Old pitch:", valueOut, 1)
-          --msg_ftl("New pitch (before floor):", calc, 1)
-
-          if calc <= -12 then
-          valueIn = -3
-          --msg_s("Pitch <= -3")
-          end
-          if calc >= 12 then
-          valueIn = 3
-          --msg_s("Pitch <= +3")
-          end
-          if calc > -12 and calc < 12 then
-          valueIn = math.floor((calc)*20+0.5)/20
-          --msg_s("-3 < Pitch < 3")
-          end
-          -- SET POINT VALUE
-          reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-        end -- ENDIF point is selected
-        end -- END Loop
-      end -- ENDIF Pan or Width
-      
-      if envelopeName == "Tempo map" then
-
-        for i = 0, env_point_count - 1 do
-          retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
-            --reaper.ShowConsoleMsg(valueOut .. "\n")
-                    if set == true then
-                      valueOut = 0
-                    end
-            
-                    if selectedOut == true then
-  
-                      -- CALC
-                      calc = valueOut + user_input_num
-                      valueIn = calc
-                      if calc <= 1 then
-                        valueIn = 1
-                        --msg_s("FX <= 0")
-                      end
-                      if calc >= 280 then
-                        valueIn = 280
-                        --msg_s("100 <= FX")
-                      end
-
-                      -- SET POINT VALUE
-                      retval, timepos, measurepos, beatpos, bpm, timesig_num, timesig_denom, lineartempo = reaper.GetTempoTimeSigMarker( 0, i )
-                      reaper.SetTempoTimeSigMarker( 0, i, timepos, measurepos, beatpos, valueIn, timesig_num, timesig_denom, lineartempo )
-                      --reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-                    end -- ENDIF point is selected
-        end
-        
-        reaper.TrackList_AdjustWindows( false )
-        already_set = true
-        
+      if env_name == "Tempo map" then
+                -- SET POINT VALUE
+        local retval, timepos, measurepos, beatpos, bpm, timesig_num, timesig_denom, lineartempo = reaper.GetTempoTimeSigMarker( 0, i )
+        reaper.SetTempoTimeSigMarker( 0, i, timepos, measurepos, beatpos, val, timesig_num, timesig_denom, lineartempo )
+      else
+        reaper.SetEnvelopePoint(env, i, nil, val, nil, nil, nil, nil)
       end
 
-      if already_set == false then -- IF ENVELOPE HAS NO NAME PAS ICI LA BOUCL !!
+    end -- ENDIF point is selected
+    --reaper.Envelope_SortPoints(env)
+  end -- END Loop
 
-        for i = 0, env_point_count - 1 do
-
-          -- IDX 0 doesnt seem to work
-          retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(envelope,i)
-  
-          if set == true then
-            valueOut = 0
-          end
-  
-          if selectedOut == true then
-  
-            -- CALC
-            calc = valueOut*100 + user_input_num
-  
-            if calc <= 0 then
-              valueIn = 0
-              --msg_s("FX <= 0")
-            end
-            if calc >= 100 then
-              valueIn = 1.0
-              --msg_s("100 <= FX")
-            end
-            if calc < 100 and calc > -100 then
-              valueIn = calc / 100
-              --msg_s("0 < FX < 100")
-            end
-            -- SET POINT VALUE
-            reaper.SetEnvelopePoint(envelope, i, time, valueIn, shape, tension, 1, noSortInOptional)
-          end -- ENDIF point is selected
-        end -- END Loop
-      end -- ENDIF Fx
+  local count_ai = reaper.CountAutomationItems( env )
+  for i = 0, count_ai - 1 do
+    local count_ai_points = reaper.CountEnvelopePointsEx( env, i )
+    for j = 0, count_ai_points - 1 do
+      local retval, time, val, shape, tension, selected = reaper.GetEnvelopePointEx( env, i, j )
+      if selected then
+        val = ProcessPoint( env, env_name, val, user_input_num, set, min, max )
+        reaper.SetEnvelopePointEx( env, i, j, nil, val, nil, nil, nil, nil )
+      end
+      --reaper.Envelope_SortPointsEx(env, i )
     end
   end
 
-  end --if envelope is selected
-  reaper.Undo_EndBlock("Set or Offset selected envelope point value", -1) -- End of the undo block. Leave it at the bottom of your main function.
+end -- END Process function
+
+function Main()
+
+  if popup then
+    if not preset_file_init then
+      input = reaper.GetExtState( ext_name, "input", input, true )
+    end
+    retval, input = reaper.GetUserInputs("Set or Offset Selected Points Values", 1, "Value? (num, " .. mod2_prefix .." for " .. other_mod[mod1] .. ")", input) -- We suppose that the user know the scale he want
+  end
+
+  if retval or not popup then
+
+    if not preset_file_init then
+      reaper.SetExtState( ext_name, "input", input, true )
+    end
+
+    local x, y = string.find(input, mod2_prefix)
+
+    if mod1 == "absolute" then
+      set = (x == nil) or false
+    end
+
+    if mod1 == "relative" then
+      set = (x ~= nil) or false
+    end
+
+    input = input:gsub(mod2_prefix, "")
+    user_input_num = tonumber(input)
+
+    -- IF VALID INPUT
+    if user_input_num then
+      ProcessEnv(env, set, user_input_num)
+    end
+
+  end -- if ot retval or poup
 end -- END OF FUNCTION
 
-set_point_value() -- Execute your main function
+function Init()
+  env = reaper.GetSelectedEnvelope(0)
+  if env then
+    reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+    reaper.ClearConsole()
+    Main() -- Execute your main function
+    reaper.UpdateArrange() -- Update the arrangement (often needed)
+    reaper.Undo_EndBlock("Set or Offset selected envelope point value", -1) -- End of the undo block. Leave it at the bottom of your main function.
+  end
+end
 
-reaper.UpdateArrange() -- Update the arrangement (often needed)
+if not preset_init_file then
+  Init()
+end
