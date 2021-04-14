136cbc177e2fae0b3ed1ecd95a559b3a166d1426
X-Raym <x-raym@extremraym.com>
2021-04-15 01:09:50 +0200

    Update X-Raym_Select items with same source as first selected item.lua

--- Items Properties/X-Raym_Select items with same source as first selected item.lua
+++ Items Properties/X-Raym_Select items with same source as first selected item.lua
@@ -1,28 +1,33 @@
 --[[
  * ReaScript Name: Select items with same source as first selected item
- * Description:
- * Instructions: Select an audio item. Run.
  * Author: X-Raym
  * Author URI: http://extremraym.com
  * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
  * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
- * File URI: https://github.com/X-Raym/REAPER-EEL-Scripts/scriptName.eel
  * Licence: GPL v3
- * Forum Thread:
- * Forum Thread URI:
  * REAPER: 5.0
- * Extensions: None
- * Version: 1.0
+ * Version: 1.0.1
 --]]
  
 --[[
  * Changelog:
+ * v1.0.1 (2021-04-15)
+	+ Initial Release
  * v1.0 (2015-08-14)
 	+ Initial Release
 --]]
 
 function Msg(variable)
 	reaper.ShowConsoleMsg(tostring(variable).."\n")
+end
+
+function GetTakeFileSource( take )
+  local source = reaper.GetMediaItemTake_Source(take)
+  local source_type = reaper.GetMediaSourceType( source, '' )
+  if source_type == 'SECTION' then
+    source = reaper.GetMediaSourceParent( source )
+  end
+  return source
 end
 
 function main() -- local (i, j, item, take, track)
@@ -39,7 +44,7 @@ function main() -- local (i, j, item, take, track)
 			
 			if reaper.TakeIsMIDI(first_take) == false then
 			
-				first_take_source = reaper.GetMediaItemTake_Source(first_take)
+				first_take_source = GetTakeFileSource(first_take)
 				first_take_source_name = reaper.GetMediaSourceFileName(first_take_source, "")
 				
 				items_count = reaper.CountMediaItems(0)
@@ -54,7 +59,7 @@ function main() -- local (i, j, item, take, track)
 						
 						if reaper.TakeIsMIDI(first_take) == false then
 							
-							take_source = reaper.GetMediaItemTake_Source(take)
+							take_source = GetTakeFileSource( take )
 							take_source_name = reaper.GetMediaSourceFileName(take_source, "")
 							
 							if take_source_name == first_take_source_name then
