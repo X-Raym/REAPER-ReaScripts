1060ab287636099ded5a74010b4abfae8e084d04
X-Raym <x-raym@extremraym.com>
2021-07-20 10:05:22 +0200

    Update X-Raym_Move selected items to named tracks.lua

--- Items Properties/X-Raym_Move selected items to named tracks.lua
+++ Items Properties/X-Raym_Move selected items to named tracks.lua
@@ -6,11 +6,16 @@
  * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.0.1
+ * Version: 1.1
 --]]
 
 --[[
  * Changelog:
+ * v1.1 (2021-07-20)
+  + Track color label using custom render (thx cfillion!)
+  + Track depth character prefix
+  + Responsive button layouts
+  # Ok label is now Move
  * v1.0 (2021-07-19)
   + Initial Release
 --]]
@@ -109,7 +114,7 @@ end
 function Process()
   local sel_items = SaveSelectedItems(t)
   for i, item in ipairs(sel_items) do
-    reaper.MoveMediaItemToTrack(item, tracks[current_track+1])
+    reaper.MoveMediaItemToTrack(item, tracks[current_track].track)
   end
   vars.track_id = current_track
   SaveState()
@@ -120,19 +125,24 @@ function SaveTracks( t )
   if not t then t = {} end
   local count_track = reaper.CountTracks( 0 )
   for i = 0, count_track - 1 do
-    t[i+1] = reaper.GetTrack( 0, i )
+    local track = reaper.GetTrack(0,i)
+    local retval, track_name = reaper.GetTrackName( track )
+    local track_depth = reaper.GetTrackDepth(track)
+    local indent = ""
+    for j = 1, track_depth do
+      indent = indent .. "-"
+    end
+    track_name = i .. ": " .. indent .. track_name
+    local color = reaper.GetMediaTrackInfo_Value( track, "I_CUSTOMCOLOR")
+    t[i+1] = { 
+      track = track, 
+      color =  color > 0 and reaper.ImGui_ColorConvertNative( color ) or 0,
+      name = track_name
+    }
   end
   return t
 end
 
-function GetTrackNameList(t, char)
-  local out = {}
-  for i, track in ipairs( t ) do
-    local retval, track_name = reaper.GetTrackName(track)
-    out[i] = i .. ": " .. track_name
-  end
-  return table.concat(out, char) .. "\31"
-end
 
 function Main()
   reaper.PreventUIRefresh(1)
@@ -150,29 +160,92 @@ function Main()
   reaper.PreventUIRefresh(-1)
 end
 
-function Run()
+function colorSquare(ctx, color)
+  color = (color << 8) | 0xff -- RGB to RGBA with 100% opacity
 
+  local draw_list = reaper.ImGui_GetWindowDrawList(ctx)
+  local x, y = reaper.ImGui_GetCursorScreenPos(ctx)
+  local size = reaper.ImGui_GetTextLineHeight(ctx)
+  reaper.ImGui_DrawList_AddRectFilled(draw_list, x, y, x + size, y + size, color)
+  
+  local pad = reaper.ImGui_GetStyleVar(ctx, reaper.ImGui_StyleVar_FramePadding())
+  reaper.ImGui_SetCursorScreenPos(ctx, x + size + pad, y)
+end
+
+function Run()
+  
+  -- TODO: dock
+  --[[
+  dock_id = -2
+  if dock_id then
+    reaper.ImGui_SetNextWindowDockID(ctx, dock_id)
+    dock_id = nil
+  end
+  ]]
   local imgui_visible, imgui_open = reaper.ImGui_Begin(ctx, input_title, true, reaper.ImGui_WindowFlags_NoCollapse())
   if imgui_visible then
   
-    tracks = SaveTracks{}
+    count_sel_items = reaper.CountSelectedMediaItems(0)
   
     imgui_width, imgui_height = reaper.ImGui_GetWindowSize( ctx )
-    ------
+    
+    tracks = SaveTracks()
+    
+    -- CUSTOM COMBO
     reaper.ImGui_SetNextItemWidth( ctx, imgui_width -25 )
     if not current_track and vars.track_id and tracks[vars.track_id] then current_track = vars.track_id end
-    retval, current_track, tracks_name = reaper.ImGui_Combo( ctx, "##combo_tracks", current_track, GetTrackNameList(tracks, "\31"), 500 )
+        
+    local combo_pos = {reaper.ImGui_GetCursorScreenPos(ctx)}
+    
+    if reaper.ImGui_BeginCombo(ctx, '##combo_tracks', '') then
+    
+      for i,v in ipairs(tracks) do
+        reaper.ImGui_PushID(ctx, i)
+    
+        colorSquare(ctx, v.color)
+        
+        if reaper.ImGui_Selectable(ctx, v.name, current_track == i, reaper.ImGui_SelectableFlags_SpanAllColumns()) then
+          current_track = i
+        end
+        
+        reaper.ImGui_PopID(ctx)
+      end
+      
+      reaper.ImGui_EndCombo(ctx)
+    end
+    
+    -- move the cursor back to the beginning of the combo box
+    local backup_pos = {reaper.ImGui_GetCursorScreenPos(ctx)}
+    local pad_x, pad_y = reaper.ImGui_GetStyleVar(ctx, reaper.ImGui_StyleVar_FramePadding())
+    reaper.ImGui_SetCursorScreenPos(ctx, combo_pos[1] + pad_x, combo_pos[2] + pad_y)
+    
+    -- do the custom preview
+    local v = tracks[current_track]
+    if v then
+      colorSquare(ctx, v.color)
+      reaper.ImGui_PushClipRect(ctx, combo_pos[1] + pad_x, combo_pos[2] + pad_y, combo_pos[1] + pad_x + imgui_width-50, combo_pos[2] + pad_y + 200, true) -- Text need clipping based on imgui_width / combo width
+      reaper.ImGui_Text(ctx, v.name)
+      reaper.ImGui_PopClipRect( ctx ) -- remove clipping
+    end
+    
+    -- restore the cursor to the end of the combo box
+    reaper.ImGui_SetCursorScreenPos(ctx, table.unpack(backup_pos))
+  
+    ------
     
     -- OK BUTTON
-    local button_width = imgui_width / 3
+    local break_point = 270
+    local button_width = imgui_width > break_point and imgui_width / 3 or imgui_width - 15
     reaper.ImGui_Dummy( ctx, 50, 13*2 )
     reaper.ImGui_Spacing( ctx )
-    reaper.ImGui_SameLine(ctx, imgui_width / 6)
-    if reaper.ImGui_Button(ctx, 'OK', button_width, 35) or reaper.ImGui_IsKeyPressed(ctx, 13) then -- Ok or Enter Key
+    if imgui_width > break_point then reaper.ImGui_SameLine(ctx, imgui_width / 6) end
+    if reaper.ImGui_Button(ctx, 'Move', button_width, 35) or reaper.ImGui_IsKeyPressed(ctx, 13) then -- Ok or Enter Key
       Main()
     end
-    reaper.ImGui_SameLine(ctx)
-    if reaper.ImGui_Button(ctx, 'OK & Quit', button_width, 35) or reaper.ImGui_IsKeyPressed(ctx, 13) then -- Ok or Enter Key
+    reaper.ImGui_PushStyleColor( ctx, 1, 0x999999 )
+    reaper.ImGui_PopStyleColor( ctx, 1 )
+    if imgui_width > break_point then reaper.ImGui_SameLine(ctx) end
+    if reaper.ImGui_Button(ctx, 'Move & Quit', button_width, 35) or reaper.ImGui_IsKeyPressed(ctx, 13) then -- Ok or Enter Key
       Main()
       process = true
     end
@@ -195,7 +268,6 @@ function Init()
     
     SetButtonState( 1 )
     reaper.atexit( Exit )
-    
     ctx = reaper.ImGui_CreateContext(input_title)
     Run()
   else
