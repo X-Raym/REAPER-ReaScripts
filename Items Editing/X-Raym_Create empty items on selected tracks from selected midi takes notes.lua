e5e7101df8b1fb7ce759e921b1d434c3902a0b0a
X-Raym <x-raym@extremraym.com>
2021-09-06 17:10:21 +0200

    initial release

---
+++ Items Editing/X-Raym_Create empty items on selected tracks from selected midi takes notes.lua
@@ -0,0 +1,137 @@
+--[[
+ * ReaScript Name: Create empty items on selected tracks from selected midi takes notes
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Items Properties (various)
+ * Forum Thread URI: http://forum.cockos.com/showthread.php?t=166689
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2021-09-06)
+  + Initial Release
+--]]
+
+
+-- USER CONFIG AREA -----------------------------------------------------------
+
+console = true -- true/false: display debug messages in the console
+
+undo_text = "Create empty items on selected tracks from selected midi takes notes"
+------------------------------------------------------- END OF USER CONFIG AREA
+
+
+-- UTILITIES -------------------------------------------------------------
+
+-- Save item selection
+function SaveSelectedItems(t)
+  local t = t or {}
+  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+    t[i+1] = reaper.GetSelectedMediaItem(0, i)
+  end
+  return t
+end
+
+function RestoreSelectedItems( items )
+  reaper.SelectAllMediaItems(0, false)
+  for i, item in ipairs( items ) do
+    reaper.SetMediaItemSelected( item, true )
+  end
+end
+
+
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
+
+--------------------------------------------------------- END OF UTILITIES
+
+-- Create a Text Item.
+-- Return the item if success. Else, return nil.
+function CreateTextItem(track, position, length, text, color)
+
+  local item = reaper.AddMediaItemToTrack(track)
+
+  reaper.SetMediaItemInfo_Value(item, "D_POSITION", position)
+  reaper.SetMediaItemInfo_Value(item, "D_LENGTH", length)
+
+  if text then
+    reaper.ULT_SetMediaItemNote(item, text)
+  end
+
+  if color then
+    reaper.SetMediaItemInfo_Value(item, "I_CUSTOMCOLOR", color)
+  end
+
+  return item
+
+end
+
+-- Main function
+function Main()
+
+  for i, item in ipairs(init_sel_items) do
+    local take = reaper.GetActiveTake( item )
+
+    if take and reaper.TakeIsMIDI( take ) then
+      -- LOOP IN MIDI NOTES
+      local retval, notes, ccs, sysex = reaper.MIDI_CountEvts(take)
+
+      -- GET SELECTED NOTES (from 0 index)
+      for k = 0, notes-1 do -- Loop in notes
+
+        local _, sel, mute, startppq, endppq, chan, pitch, vel = reaper.MIDI_GetNote(take, k)
+
+        local start_time =  reaper.MIDI_GetProjTimeFromPPQPos( take, startppq )
+        local end_time =  reaper.MIDI_GetProjTimeFromPPQPos( take, endppq )
+
+        for j = 0, count_sel_tracks - 1 do
+          local track = reaper.GetSelectedTrack(0,j)
+          CreateTextItem(track, start_time, end_time-start_time)
+        end
+
+      end
+    end
+  end
+
+end
+
+
+-- INIT
+function Init()
+  -- See if there is items selected
+  count_sel_items = reaper.CountSelectedMediaItems(0)
+  if count_sel_items == 0 then return false end
+
+  count_sel_tracks = reaper.CountSelectedTracks(0)
+  if count_sel_tracks == 0 then return false end
+
+  reaper.PreventUIRefresh(1)
+
+  reaper.Undo_BeginBlock()
+
+  init_sel_items = SaveSelectedItems()
+
+  Main()
+
+  RestoreSelectedItems(init_sel_items)
+
+  reaper.Undo_EndBlock(undo_text, -1)
+
+  reaper.UpdateArrange()
+
+  reaper.PreventUIRefresh(-1)
+
+end
+
+if not preset_file_init then
+  Init()
+end
---
+++ Items Properties/X-Raym_Set selected items notes from multiline clipboard content.lua
@@ -0,0 +1,141 @@
+--[[
+ * ReaScript Name: Set selected items notes from multiline clipboard content
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Licence: GPL v3
+ * Forum Thread: Scripts: Items Properties (various)
+ * Forum Thread URI: http://forum.cockos.com/showthread.php?t=166689
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2021-09-06)
+  + Initial Release
+--]]
+
+-- USER CONFIG AREA -----------------------------------------------------------
+
+console = true -- true/false: display debug messages in the console
+sep = "\n" -- default sep
+names_csv = "" -- default name
+filter_empty_lines = false
+
+------------------------------------------------------- END OF USER CONFIG AREA
+
+-- https://helloacm.com/split-a-string-in-lua/
+function split(s, delimiter, preserve)
+  local result = {}
+  local i = 0
+  for match in (s..delimiter):gmatch("(.-)"..delimiter) do
+    if preserve and i > 0 then
+      table.insert(result, delimiter .. match)
+    else
+      table.insert(result, match)
+    end
+    i = i + 1
+  end
+  return result
+end
+
+
+-- UTILITIES -------------------------------------------------------------
+
+-- Save item selection
+function SaveSelectedItems (table)
+  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+    table[i+1] = reaper.GetSelectedMediaItem(0, i)
+  end
+end
+
+
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
+
+--------------------------------------------------------- END OF UTILITIES
+
+
+-- Main function
+function main()
+
+  if filter_empty_lines then
+    names_filtered= {}
+    for i, v in ipairs( names ) do
+      if v ~= "\r" and v ~= "" then table.insert( names_filtered, v ) end
+    end
+    names = names_filtered
+  end
+
+  for i, item in ipairs(init_sel_items) do
+      if names[i] then
+        name_out = names[i]:gsub("\r", "")
+        if name_out then
+          reaper.GetSetMediaItemInfo_String(item, "P_NOTES", name_out, true)
+        else
+          break
+        end
+      end
+  end
+
+end
+
+
+-- INIT
+function Init()
+  -- See if there is items selected
+  count_sel_items = reaper.CountSelectedMediaItems(0)
+
+  if count_sel_items > 0 then
+
+    if reaper.SNM_CreateFastString then
+
+      fs = reaper.SNM_CreateFastString('')
+
+      clipboard = reaper.CF_GetClipboardBig(fs)
+
+    else
+      clipboard = reaper.CF_GetClipboardBig('')
+    end
+
+    if clipboard and clipboard ~= "" then
+
+      names_csv = clipboard
+
+      reaper.ClearConsole()
+
+      reaper.PreventUIRefresh(1)
+
+      reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+      init_sel_items =  {}
+      SaveSelectedItems(init_sel_items)
+
+      names = split(names_csv,sep)
+
+      main()
+
+      reaper.Undo_EndBlock("Set selected items notes from multiline clipboard content", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+      reaper.UpdateArrange()
+
+      reaper.PreventUIRefresh(-1)
+
+    end
+
+    if reaper.SNM_DeleteFastString then
+     reaper.SNM_DeleteFastString(fs)
+    end
+
+  end
+end
+
+if not preset_file_init  then
+  Init()
+end
