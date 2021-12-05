eaa8bcb5b342b88926378142e9141716e5af5697
X-Raym <extremraym@yahoo.fr>
2021-12-05 20:17:55 +0100

    Create X-Raym_Toggle Sonarworks SoundID monitor FX instance Calibration state.lua

---
+++ Various/X-Raym_Toggle Sonarworks SoundID monitor FX instance Calibration state.lua
@@ -0,0 +1,63 @@
+--[[
+ * ReaScript Name: Toggle Sonarworks SoundID monitor FX instance Calibration state
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > REAPER-ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts/
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2021-12-05)
+  + Initial Release
+--]]
+
+-- USER CONFIG AREA ---------------------
+fx_name = "SoundID Reference Plugin (Sonarworks)"
+param_name = "Calibration state"
+-----------------------------------------
+
+function Msg( val )
+  reaper.ShowConsoleMsg(tostring(val).."\n")
+end
+
+-- Set ToolBar Button State
+function SetButtonState( set )
+  local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  reaper.SetToggleCommandState( sec, cmd, set or 0 )
+  reaper.RefreshToolbar2( sec, cmd )
+end
+
+function Main()
+  master_track = reaper.GetMasterTrack(0)
+  fx_id = reaper.TrackFX_AddByName( master_track, fx_name, true, 0)
+
+  if fx_id == -1 then return false end
+
+  fx_id = fx_id|0x1000000
+
+  count_param = reaper.TrackFX_GetNumParams(master_track, fx_id)
+
+  for i = 0, count_param do
+    local retval, fx_param_name = reaper.TrackFX_GetParamName(master_track, fx_id, i)
+    if fx_param_name == param_name then
+      param_val = reaper.TrackFX_GetParamNormalized(master_track, fx_id, i)
+      if param_val == 0 then param_val = 1 else param_val = 0 end
+      reaper.TrackFX_SetParamNormalized(master_track, fx_id, i, param_val)
+      mouse_x, mouse_y = reaper.GetMousePosition()
+      reaper.TrackCtl_SetToolTip("SoundID Calibration set to " .. param_val, mouse_x + 17, mouse_y + 17, true)
+      SetButtonState( param_val )
+      break
+    end
+  end
+
+end
+
+reaper.ClearConsole()
+
+if not preset_file_init then
+  reaper.defer(Main)
+end
