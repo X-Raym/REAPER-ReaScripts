252b92d47bdb776f42a0ab9eaa230851d0085dda
X-Raym <x-raym@extremraym.com>
2021-12-23 00:12:21 +0100

    Create X-Raym_Add envelope markers from selected envelope points.lua

---
+++ Regions/X-Raym_Add envelope markers from selected envelope points.lua
@@ -0,0 +1,29 @@
+--[[
+ * ReaScript: Add envelope markers from selected envelope points
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0.3
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2021-12-23)
+  + Initial Release
+--]]
+
+env = reaper.GetSelectedTrackEnvelope(0)
+
+if not env then return false end
+
+count_env_points = reaper.CountEnvelopePoints(env)
+
+for i = 0, count_env_points do
+  local retval, time, value, shape, tension, selected = reaper.GetEnvelopePoint( env, i )
+  if selected then
+    reaper.AddProjectMarker(0, false, time, 0, value, -1)
+  end
+end
