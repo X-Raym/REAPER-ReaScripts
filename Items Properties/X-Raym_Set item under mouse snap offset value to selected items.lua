8a20f6d12d0b9c0fc60aac1063ad6e3dfd2fd127
X-Raym <x-raym@extremraym.com>
2022-01-03 12:52:35 +0100

    Create X-Raym_Set item under mouse snap offset value to selected items.lua

---
+++ Items Properties/X-Raym_Set item under mouse snap offset value to selected items.lua
@@ -0,0 +1,70 @@
+--[[
+ * ReaScript Name: Set item under mouse snap offset value to selected items
+ * Screenshot: https://i.imgur.com/bzSneI8.gif
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > EEL Scripts for Cockos REAPER
+ * Repository URI: https://github.com/X-Raym/REAPER-EEL-Scripts
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2022-01-23)
+	+ Initial Release
+--]]
+
+
+-- USER CONFIG AREA -----------------------------------------------------------
+
+console = true -- true/false: display debug messages in the console
+
+------------------------------------------------------- END OF USER CONFIG AREA
+
+
+-- UTILITIES -------------------------------------------------------------
+
+
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
+
+--------------------------------------------------------- END OF UTILITIES
+
+
+-- Main function
+function Main()
+  snap_offset = reaper.GetMediaItemInfo_Value(mouse_item, "D_SNAPOFFSET")
+  for i = 0, count_sel_items - 1 do
+    local item = reaper.GetSelectedMediaItem(0, i)
+    reaper.SetMediaItemInfo_Value(item, "D_SNAPOFFSET", snap_offset)
+  end
+
+end
+
+
+-- INIT
+
+-- See if there is items selected
+count_sel_items = reaper.CountSelectedMediaItems(0)
+mouse_item, position = reaper.BR_ItemAtMouseCursor()
+
+if count_sel_items > 0 and mouse_item then
+
+  reaper.PreventUIRefresh(1)
+
+  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+
+  Main()
+
+  reaper.Undo_EndBlock("Set item under mouse snap offset value to selected items", -1) -- End of the undo block. Leave it at the bottom of your main function.
+
+  reaper.UpdateArrange()
+
+  reaper.PreventUIRefresh(-1)
+
+end
