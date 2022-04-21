110efc94408906bd6bfac2b91b325ee4eb3dfcb6
X-Raym <x-raym@extremraym.com>
2022-04-21 12:20:43 +0200

    Update X-Raym_Paste value in clipboard as selected items volume.lua

--- Items Properties/X-Raym_Paste value in clipboard as selected items volume.lua
+++ Items Properties/X-Raym_Paste value in clipboard as selected items volume.lua
@@ -5,18 +5,32 @@
  * Repository: GitHub > X-Raym > REAPER ReaScripts
  * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
  * Licence: GPL v3
- * Version: 1.0
+ * Version: 1.0.1
 --]]
 
 --[[
  * Changelog:
+ * v1.0.1 (2022-04-21)
+  + Limit value
  * v1.0 (2022-04-21)
   + Initial Release
 --]]
 
+-----------------------------------------------------------
+
+min_limit = 0
+max_limit = 2
+
+-----------------------------------------------------------
+
 undo_text = "Paste value in clipboard as selected items volume"
 
 function ValFromdB(dB_val) return 10^(dB_val/20) end
+
+function LimitNumber( val, min, max )
+  if not val then return nil end
+  return math.min(math.max(min, val), max)
+end
 
 if not reaper.CF_GetClipboard then
   reaper.ShowMessageBox( 'Please Install last SWS extension.', 'Missing Dependency', 0 )
@@ -44,7 +58,9 @@ function Init()
   if not vol then return false end
 
   vol = tonumber( vol )
-  if not vol then return false end  
+  if not vol then return false end
+
+  vol = LimitNumber( vol, min_limit, max_limit ) 
 
   reaper.PreventUIRefresh(1)
 
