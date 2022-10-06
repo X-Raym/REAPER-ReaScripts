de984c021e7f3f6e36d081e314e00af7085826db
X-Raym <extremraym@yahoo.fr>
2022-10-06 19:59:04 +0200

    updated
     * v2.0 (2022-10-06)
      + New core
      + Escape pattern field
      + Save last input
      # Updated preset system

--- Items Properties/X-Raym_Search and replace in selected active takes names.lua
+++ Items Properties/X-Raym_Search and replace in selected active takes names.lua
@@ -9,11 +9,16 @@
  * Forum Thread: Scripts: Items Properties (various)
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1574814#post1574814
  * REAPER: 5.0
- * Version: 1.2.2
+ * Version: 2.0
 --]]
 
 --[[
  * Changelog:
+ * v2.0 (2022-10-06)
+  + New core
+  + Escape pattern field
+  + Save last input
+  # Updated preset system
  * v1.2.2 (2022-03-29)
   # Fix defaut csv (thx nicola!)
  * v1.2.1 (2022-03-21)
@@ -28,139 +33,227 @@
   + Initial Release
 --]]
 
--- USER CONFIG AREA ---------------------------------------------
--- Do you want a pop up to appear ?
-popup = true -- true/false
+-----------------------------------------------------------
+-- USER CONFIG AREA --
+-----------------------------------------------------------
 
--- Define here your default variables values
-search = "word" -- % for escaping characters
-replace = "/del" -- "/del" for deletion
-truncate_start = "0"
-truncate_end = "0"
-ins_start_in = "/no" -- "/no" for no insertion, "/E" for item number in selection, "/T" for track name
-ins_end_in = "/no" -- "/no" for no insertion, "/E" for item number in selection, "/T" for track name
-select_renamed = "y" -- y/n for select item with renamed
------------------------------------------------------------------
-function Msg(val)
-  reaper.ShowConsoleMsg(tostring(val) .. "\n")
+-- Preset file: https://gist.github.com/X-Raym/f7f6328b82fe37e5ecbb3b81aff0b744#file-preset-lua
+
+console = true
+popup = true -- User input dialog box
+
+vars = vars or {}
+vars.search = ""
+vars.replace = ""
+vars.use_lua_pattern = "n"
+vars.truncate_start = 0
+vars.truncate_end = 0
+vars.ins_start_in = "" -- "/E" for item number in selection, "/T" for track name
+vars.ins_end_in = "" -- "/E" for item number in selection, "/T" for track name
+vars.select_renamed = "y" -- y/n to select item which have been actually renamed
+
+input_title = "Search & Replace in Item Take Names"
+undo_text = "Search and replace in selected items active take names"
+
+-----------------------------------------------------------
+                              -- END OF USER CONFIG AREA --
+-----------------------------------------------------------
+
+-----------------------------------------------------------
+-- GLOBALS --
+-----------------------------------------------------------
+
+vars_order = {"search", "replace", "use_lua_pattern", "truncate_start", "truncate_end", "ins_start_in", "ins_end_in", "select_renamed"}
+
+instructions = instructions or {}
+instructions.search = "Search?"
+instructions.replace = "Replace?"
+instructions.use_lua_pattern = "Use Lua Pattern? (y/n)"
+instructions.truncate_start = "Truncate from start? (>0)"
+instructions.truncate_end = "Truncate from end? (>0)"
+instructions.ins_start_in = "Insert at start? (/E=Sel Num...)"
+instructions.ins_end_in = "Insert at end? (.../T=Track)"
+instructions.select_renamed = "Select renamed items only? (y/n)"
+
+sep = "\n"
+extrawidth = "extrawidth=120"
+separator = "separator=" .. sep
+
+ext_name = "XR_SearchReplaceTakeNames"
+
+-----------------------------------------------------------
+-- DEBUGGING --
+-----------------------------------------------------------
+function Msg(g)
+  if console then
+    reaper.ShowConsoleMsg(tostring(g).."\n")
+  end
 end
 
+-----------------------------------------------------------
+-- STATES --
+-----------------------------------------------------------
+function SaveState()
+  for k, v in pairs( vars ) do
+    reaper.SetExtState( ext_name, k, tostring(v), true )
+  end
+end
 
-function main()
+function GetExtState( var, val )
+  local t = type( val )
+  if reaper.HasExtState( ext_name, var ) then
+    val = reaper.GetExtState( ext_name, var )
+  end
+  if t == "boolean" then val = toboolean( val )
+  elseif t == "number" then val = tonumber( val )
+  else
+  end
+  return val
+end
 
-  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+function GetValsFromExtState()
+  for k, v in pairs( vars ) do
+    vars[k] = GetExtState( k, vars[k] )
+  end
+end
+
+function ConcatenateVarsVals(t, sep, vars_order)
+  local vals = {}
+  for i, v in ipairs( vars_order ) do
+    vals[i] = t[v]
+  end
+  return table.concat(vals, sep)
+end
+
+function ParseRetvalCSV( retvals_csv, sep, vars_order )
+  local t = {}
+  local i = 0
+  for line in retvals_csv:gmatch("[^" .. sep .. "]*") do
+  i = i + 1
+  t[vars_order[i]] = line
+  end
+  return t
+end
+
+function ValidateVals( vars, vars_order )
+  local validate = true
+  for i, v in ipairs( vars_order ) do
+    if vars[v] == nil then
+      validate = false
+      break
+    end
+  end
+  return validate
+end
+
+-----------------------------------------------------------
+-- MAIN --
+-----------------------------------------------------------
+-- https://stackoverflow.com/questions/29072601/lua-string-gsub-with-a-hyphen
+function EscapePatternStr(str)
+    return string.gsub(str, "[%(%)%.%+%-%*%?%[%]%^%$%%]", "%%%1") -- escape pattern
+end
+
+function Main()
+
+  search = (vars.use_lua_pattern == "y" and vars.search) or EscapePatternStr(  vars.search )
 
   select_renamed_items = {}
 
-  -- INITIALIZE loop through selected items
+  -- INITIALIZE
   for i = 0, sel_items_count-1  do
-    -- GET ITEMS
-    item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
 
-  take = reaper.GetActiveTake(item)
+    local item = reaper.GetSelectedMediaItem(0, i)
+    local take = reaper.GetActiveTake(item)
 
-  if take ~= nil then
+    if take then
 
-    -- GET NAMES
-    take_name = reaper.GetTakeName(take)
-    original_take_name = take_name
+      -- GET NAMES
+      local take_name = reaper.GetTakeName(take)
+      local original_take_name = take_name
 
-    track = reaper.GetMediaItem_Track(item)
-    retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
+      local track = reaper.GetMediaItem_Track(item)
+      local retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
 
+      -- MODIFY NAMES
+      take_name = take_name:gsub(search, vars.replace)
 
-    -- MODIFY NAMES
-    replace = replace:gsub("/T", track_name)
-    take_name = take_name:gsub(search, replace)
+      if vars.truncate_start > 0 then take_name = take_name:sub(vars.truncate_start+1) end
+      if vars.truncate_end > 0 then
+        take_name_len = take_name:len()
+        take_name = take_name:sub(0, take_name_len-vars.truncate_end)
+      end
 
-    truncate_start = tonumber(truncate_start)
-    truncate_end = tonumber(truncate_end)
-    if truncate_start > 0 and truncate_start ~= nil then take_name = take_name:sub(truncate_start+1) end
-    if truncate_end > 0 and truncate_end ~= nil then
-      take_name_len = take_name:len()
-      take_name = take_name:sub(0, take_name_len-truncate_end)
+      local ins_start = vars.ins_start_in:gsub("/E", tostring(i + 1))
+      local ins_end = vars.ins_end_in:gsub("/E", tostring(i + 1))
+      ins_start = ins_start:gsub("/T", track_name)
+      ins_end = ins_end:gsub("/T", track_name)
+
+      take_name = ins_start..take_name..ins_end
+
+      if select_renamed == "y" and original_take_name ~= take_name then
+        table.insert(select_renamed_items, item)
+      end
+
+      -- SETNAMES
+      reaper.GetSetMediaItemTakeInfo_String(take, "P_NAME", take_name, true)
+
     end
-    
-    ins_start = ins_start_in:gsub("/E", tostring(i + 1))
-    ins_start = ins_start:gsub("/T", track_name)
-
-    ins_end = ins_end_in:gsub("/E", tostring(i + 1))
-    ins_end = ins_end:gsub("/T", track_name)
-
-    new_take_name = ins_start..take_name..ins_end
-
-    if select_renamed == "y" and original_take_name ~= new_take_name then
-      table.insert(select_renamed_items, item)
-    end
-
-    -- SETNAMES
-    reaper.GetSetMediaItemTakeInfo_String(take, "P_NAME", new_take_name, true)
 
   end
-
-  end -- ENDLOOP through selected items
 
   if select_renamed == "y" then
-  reaper.SelectAllMediaItems(0, false)
-  for i, item in ipairs(select_renamed_items) do
-    reaper.SetMediaItemSelected( item, true )
+    reaper.SelectAllMediaItems(0, false)
+    for i, item in ipairs(select_renamed_items) do
+      reaper.SetMediaItemSelected( item, true )
+    end
   end
-  end
-
-  reaper.Undo_EndBlock("Search and replace in selected active takes names", -1) -- End of the undo block. Leave it at the bottom of your main function.
 
 end
 
+-----------------------------------------------------------
+-- INIT --
+-----------------------------------------------------------
 function Init()
-  -- START
+
   sel_items_count = reaper.CountSelectedMediaItems(0)
+  if sel_items_count == 0 then return false end
 
-  if sel_items_count > 0 then
+  if popup then
 
-    if popup == true then
-
-      defaultvals_csv = search .. "," .. replace .. "," .. truncate_start .. "," .. truncate_end .. "," .. ins_start_in .. "," .. ins_end_in .. "," .. select_renamed
-
-      retval, retvals_csv = reaper.GetUserInputs("Search & Replace", 7, "Search (% for escape char),Replace (/del for deletion),Truncate from start,Truncate from end,Insert at start (/E = Sel Num),Insert at end (/T = track name),Select renamed only? (y/n)", defaultvals_csv)
-
-      if retval then -- if user complete the fields
-
-        search, replace, truncate_start, truncate_end, ins_start_in, ins_end_in, select_renamed = retvals_csv:match("([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+),([^,]+)")
-
-        if replace == "/del" then replace = "" end
-        if ins_start_in == "/no" then ins_start_in = "" end
-        if ins_end_in == "/no" then ins_end_in = "" end
-
-        if search ~= nil then
-
-        reaper.PreventUIRefresh(1)
-        
-        reaper.ClearConsole()
-
-        main() -- Execute your main function
-
-        reaper.PreventUIRefresh(-1)
-
-        reaper.UpdateArrange() -- Update the arrangement (often needed)
-        end
-
-      end
-
-    else
-
-      reaper.PreventUIRefresh(1)
-
-      if replace == "/del" then replace = "" end
-      if ins_start_in == "/no" then ins_start_in = "" end
-      if ins_end_in == "/no" then ins_end_in = "" end
-
-      main() -- Execute your main function
-
-      reaper.PreventUIRefresh(-1)
-
-      reaper.UpdateArrange() -- Update the arrangement (often needed)
-
+    if not preset_file_init and not reset then
+      GetValsFromExtState()
     end
+
+    retval, retvals_csv = reaper.GetUserInputs(input_title, #vars_order, ConcatenateVarsVals(instructions, sep, vars_order) .. sep .. extrawidth .. sep .. separator, ConcatenateVarsVals(vars, sep, vars_order) )
+    if retval then
+      vars = ParseRetvalCSV( retvals_csv, sep, vars_order )
+      if vars.ins_start_in == "/no" then vars.ins_start_in = "" end
+      if vars.ins_end_in == "/no" then vars.ins_end_in = "" end
+      vars.truncate_start = tonumber(vars.truncate_start) or vars.truncate_start:len()
+      vars.truncate_start = math.max( 0, vars.truncate_start )
+      vars.truncate_end = tonumber(vars.truncate_end) or vars.truncate_end:len()
+      vars.truncate_end = math.max( 0, vars.truncate_end )
+    end
+  end
+
+  if not popup or ( retval and ValidateVals(vars, vars_order) ) then -- if user complete the fields
+
+    reaper.PreventUIRefresh(1)
+
+    reaper.Undo_BeginBlock()
+
+    if not clear_console_init then reaper.ClearConsole() end
+
+    if popup then SaveState() end
+
+    Main() -- Execute your main function
+
+    reaper.Undo_EndBlock(undo_text, -1)
+
+    reaper.UpdateArrange()
+
+    reaper.PreventUIRefresh(-1)
 
   end
 end
--- Text Items and Item Notes/Formatting/X-Raym_Search and replace in selected items notes.lua
+++ Text Items and Item Notes/Formatting/X-Raym_Search and replace in selected items notes.lua
@@ -9,67 +9,243 @@
  * Licence: GPL v3
  * Forum Thread: Scripts (LUA): Text Items Formatting Actions (various)
  * Forum Thread URI: http://forum.cockos.com/showthread.php?t=156757
- * REAPER: 5.0 pre 32
- * Extensions: SWS/S&M 2.7.1
- * Version: 1.0
+ * REAPER: 6.0
+ * Version: 2.0
 --]]
 
 --[[
  * Changelog:
+ * Changelog:
+ * v2.0 (2022-10-06)
+  + New core
+  + Escape pattern field
+  + Save last input
+  + Truncate fields
+  # Updated preset system
  * v1.0 (2015-05-25)
   + Initial Release
 --]]
 
- -- TO DO: MAke it Work with - chracter in search
+-----------------------------------------------------------
+-- USER CONFIG AREA --
+-----------------------------------------------------------
 
-function main()
+-- Preset file: https://gist.github.com/X-Raym/f7f6328b82fe37e5ecbb3b81aff0b744#file-preset-lua
 
-  reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
+console = true
+popup = true -- User input dialog box
 
+vars = vars or {}
+vars.search = ""
+vars.replace = ""
+vars.use_lua_pattern = "n"
+vars.truncate_start = 0
+vars.truncate_end = 0
+vars.ins_start_in = "" -- "/E" for item number in selection, "/T" for track name
+vars.ins_end_in = "" -- "/E" for item number in selection, "/T" for track name
+vars.select_renamed = "y" -- y/n to select item which have been actually renamed
 
-  -- LOOP THROUGH SELECTED ITEMS
-  selected_items_count = reaper.CountSelectedMediaItems(0)
+input_title = "Search & Replace in Item Notes"
+undo_text = "Search and replace in selected items notes"
 
-  -- INITIALIZE loop through selected items
-  for i = 0, selected_items_count-1  do
-    -- GET ITEMS
-    item = reaper.GetSelectedMediaItem(0, i) -- Get selected item i
+-----------------------------------------------------------
+                              -- END OF USER CONFIG AREA --
+-----------------------------------------------------------
 
-    -- GET NOTES
-    note = reaper.ULT_GetMediaItemNote(item)
+-----------------------------------------------------------
+-- GLOBALS --
+-----------------------------------------------------------
 
-    -- MODIFY NOTES
-    note = note:gsub(search, replace)
+vars_order = {"search", "replace", "use_lua_pattern", "truncate_start", "truncate_end", "ins_start_in", "ins_end_in", "select_renamed"}
 
-    -- SET NOTES
-    note = reaper.ULT_SetMediaItemNote(item, note)
+instructions = instructions or {}
+instructions.search = "Search?"
+instructions.replace = "Replace?"
+instructions.use_lua_pattern = "Use Lua Pattern? (y/n)"
+instructions.truncate_start = "Truncate from start? (>0)"
+instructions.truncate_end = "Truncate from end? (>0)"
+instructions.ins_start_in = "Insert at start? (/E=Sel Num...)"
+instructions.ins_end_in = "Insert at end? (.../T=Track)"
+instructions.select_renamed = "Select renamed items only? (y/n)"
 
-  end -- ENDLOOP through selected items
+sep = "\n"
+extrawidth = "extrawidth=120"
+separator = "separator=" .. sep
 
-  reaper.Undo_EndBlock("Search and replace in selected items notes", 0) -- End of the undo block. Leave it at the bottom of your main function.
+ext_name = "XR_SearchReplaceItemNotes"
+
+-----------------------------------------------------------
+-- DEBUGGING --
+-----------------------------------------------------------
+function Msg(g)
+  if console then
+    reaper.ShowConsoleMsg(tostring(g).."\n")
+  end
+end
+
+-----------------------------------------------------------
+-- STATES --
+-----------------------------------------------------------
+function SaveState()
+  for k, v in pairs( vars ) do
+    reaper.SetExtState( ext_name, k, tostring(v), true )
+  end
+end
+
+function GetExtState( var, val )
+  local t = type( val )
+  if reaper.HasExtState( ext_name, var ) then
+    val = reaper.GetExtState( ext_name, var )
+  end
+  if t == "boolean" then val = toboolean( val )
+  elseif t == "number" then val = tonumber( val )
+  else
+  end
+  return val
+end
+
+function GetValsFromExtState()
+  for k, v in pairs( vars ) do
+    vars[k] = GetExtState( k, vars[k] )
+  end
+end
+
+function ConcatenateVarsVals(t, sep, vars_order)
+  local vals = {}
+  for i, v in ipairs( vars_order ) do
+    vals[i] = t[v]
+  end
+  return table.concat(vals, sep)
+end
+
+function ParseRetvalCSV( retvals_csv, sep, vars_order )
+  local t = {}
+  local i = 0
+  for line in retvals_csv:gmatch("[^" .. sep .. "]*") do
+  i = i + 1
+  t[vars_order[i]] = line
+  end
+  return t
+end
+
+function ValidateVals( vars, vars_order )
+  local validate = true
+  for i, v in ipairs( vars_order ) do
+    if vars[v] == nil then
+      validate = false
+      break
+    end
+  end
+  return validate
+end
+
+-----------------------------------------------------------
+-- MAIN --
+-----------------------------------------------------------
+-- https://stackoverflow.com/questions/29072601/lua-string-gsub-with-a-hyphen
+function EscapePatternStr(str)
+    return string.gsub(str, "[%(%)%.%+%-%*%?%[%]%^%$%%]", "%%%1") -- escape pattern
+end
+
+function Main()
+
+  search = (vars.use_lua_pattern == "y" and vars.search) or EscapePatternStr(  vars.search )
+
+  select_renamed_items = {}
+
+  -- INITIALIZE
+  for i = 0, sel_items_count-1  do
+
+    local item = reaper.GetSelectedMediaItem(0, i)
+
+    -- GET NAMES
+    local retval, item_notes = reaper.GetSetMediaItemInfo_String(item, "P_NOTES", "", false)
+    local original_item_notes = item_notes
+
+    local track = reaper.GetMediaItem_Track(item)
+    local retval, track_name = reaper.GetSetMediaTrackInfo_String(track, "P_NAME", "", false)
+
+    -- MODIFY NAMES
+    item_notes = item_notes:gsub(search, vars.replace)
+
+    if vars.truncate_start > 0 then item_notes = item_notes:sub(vars.truncate_start+1) end
+    if vars.truncate_end > 0 then
+      item_notes_len = item_notes:len()
+      item_notes = item_notes:sub(0, item_notes_len-vars.truncate_end)
+    end
+
+    local ins_start = vars.ins_start_in:gsub("/E", tostring(i + 1))
+    local ins_end = vars.ins_end_in:gsub("/E", tostring(i + 1))
+    ins_start = ins_start:gsub("/T", track_name)
+    ins_end = ins_end:gsub("/T", track_name)
+
+    local item_notes = ins_start..item_notes..ins_end
+
+    if select_renamed == "y" and original_item_notes ~= item_notes then
+      table.insert(select_renamed_items, item)
+    end
+
+    -- SETNAMES
+    reaper.GetSetMediaItemInfo_String(item, "P_NOTES", item_notes, true)
+
+  end
+
+  if select_renamed == "y" then
+    reaper.SelectAllMediaItems(0, false)
+    for i, item in ipairs(select_renamed_items) do
+      reaper.SetMediaItemSelected( item, true )
+    end
+  end
 
 end
 
--- START
-defaultvals_csv = ","
+-----------------------------------------------------------
+-- INIT --
+-----------------------------------------------------------
+function Init()
 
-retval, retvals_csv = reaper.GetUserInputs("Search & Replace", 2, "Search (% for escape char),Replace (/del for deletion)", defaultvals_csv)
+  sel_items_count = reaper.CountSelectedMediaItems(0)
+  if sel_items_count == 0 then return false end
 
-if retval then -- if user complete the fields
+  if popup then
 
-  search, replace = retvals_csv:match("([^,]+),([^,]+)")
+    if not preset_file_init and not reset then
+      GetValsFromExtState()
+    end
 
-  if replace == "/del" then replace = "" end
+    retval, retvals_csv = reaper.GetUserInputs(input_title, #vars_order, ConcatenateVarsVals(instructions, sep, vars_order) .. sep .. extrawidth .. sep .. separator, ConcatenateVarsVals(vars, sep, vars_order) )
+    if retval then
+      vars = ParseRetvalCSV( retvals_csv, sep, vars_order )
+      if vars.ins_start_in == "/no" then vars.ins_start_in = "" end
+      if vars.ins_end_in == "/no" then vars.ins_end_in = "" end
+      vars.truncate_start = tonumber(vars.truncate_start) or vars.truncate_start:len()
+      vars.truncate_start = math.max( 0, vars.truncate_start )
+      vars.truncate_end = tonumber(vars.truncate_end) or vars.truncate_end:len()
+      vars.truncate_end = math.max( 0, vars.truncate_end )
+    end
+  end
 
-  if search ~= nil then
+  if not popup or ( retval and ValidateVals(vars, vars_order) ) then -- if user complete the fields
 
     reaper.PreventUIRefresh(1)
 
-    main() -- Execute your main function
+    reaper.Undo_BeginBlock()
+
+    if not clear_console_init then reaper.ClearConsole() end
+
+    if popup then SaveState() end
+
+    Main() -- Execute your main function
+
+    reaper.Undo_EndBlock(undo_text, -1)
+
+    reaper.UpdateArrange()
 
     reaper.PreventUIRefresh(-1)
 
-    reaper.UpdateArrange() -- Update the arrangement (often needed)
   end
+end
 
+if not preset_file_init then
+  Init()
 end
