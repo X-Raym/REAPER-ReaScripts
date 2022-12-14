c0c74fb363e7b2fc6cbdf6c9007e3ab26e0701cc
X-Raym <extremraym@yahoo.fr>
2022-12-14 17:45:20 +0100

    initial release

---
+++ Various/X-Raym_Copy filtered action names list to clipboard.lua
@@ -0,0 +1,48 @@
+--[[
+ * ReaScript Name: Copy filtered action names list to clipboard
+ * About: Copy actions names to clipboard if they pass the filter.
+ * Author: X-Raym
+ * Author URI: http://extremraym.com
+ * Repository: GitHub > X-Raym > REAPER ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 6.0
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2022-12-14)
+  + Initial Release
+ --]]
+ 
+contexts = { ["Main"] = 0, ["Main (alt recording)"]=100, ["MIDI Editor"] = 32060, ["MIDI Event List Editor"] = 32061, ["MIDI Inline Editor"] = 32062, ["Media Explorer"] = 32063 }
+
+lines = {}
+
+function Msg( val )
+  reaper.ShowConsoleMsg( tostring( val ) .. "\n" )
+end
+
+function Main()
+  for k, v in pairs( contexts ) do
+    local i = 0
+    repeat
+      local retval, name = reaper.CF_EnumerateActions( v, i )
+      -- Filter actions starting with "beta_" or having "test" in their name
+      if retval > 0 and not name:lower():find("beta_") and not name:lower():find("test") then
+        table.insert( lines, name )
+      end
+      i = i + 1
+    until retval <= 0
+  end
+  
+  reaper.CF_SetClipboard( table.concat( lines, "\n" ) )
+end
+
+if not reaper.CF_GetClipboard then
+  reaper.ShowMessageBox( 'Please Install last SWS extension.', 'Missing Dependency', 0 )
+  return false
+end
+
+reaper.defer(Main)
