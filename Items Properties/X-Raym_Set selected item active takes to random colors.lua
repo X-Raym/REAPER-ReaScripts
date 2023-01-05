b99c9ae72f793b499c38205aa193d9848f6b0837
X-Raym <extremraym@yahoo.fr>
2023-01-05 02:12:24 +0100

    Create X-Raym_Set selected item active takes to random colors.lua

---
+++ Items Properties/X-Raym_Set selected item active takes to random colors.lua
@@ -0,0 +1,102 @@
+--[[
+ * ReaScript Name: Set selected item active takes to random colors
+ * About: Also works on empty items
+ * Author: X-Raym
+ * Author URI: https://www.extremraym.com
+ * Repository: GitHub > X-Raym > REAPER-ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+ 
+--[[
+ * Changelog:
+ * v1.0 (2016-01-29)
+  + Initial Release
+--]]
+
+
+-- USER CONFIG AREA -----------------------------------------------------------
+
+console = true -- true/false: display debug messages in the console
+
+undo_text = "Set selected item active takes to random colors" 
+------------------------------------------------------- END OF USER CONFIG AREA
+
+
+-- UTILITIES -------------------------------------------------------------
+
+-- Save item selection
+function SaveSelectedItems(t)
+  local t = t or {}
+  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+    t[i+1] = reaper.GetSelectedMediaItem(0, i)
+  end
+  return t
+end
+
+function RestoreSelectedItems( items )
+  reaper.SelectAllMediaItems(0, false)
+  for i, item in ipairs( items ) do
+    reaper.SetMediaItemSelected( item, true )
+  end
+end
+
+
+-- Display a message in the console for debugging
+function Msg(value)
+  if console then
+    reaper.ShowConsoleMsg(tostring(value) .. "\n")
+  end
+end
+
+--------------------------------------------------------- END OF UTILITIES
+
+
+-- Main function
+function Main()
+
+  for i, item in ipairs(init_sel_items) do
+    reaper.SelectAllMediaItems( 0, false )
+    reaper.SetMediaItemSelected( item, true )
+    local take = reaper.GetActiveTake( item )
+    if take then
+      reaper.Main_OnCommand(41332, 0) -- Take: Set active take to one random color
+    else
+      reaper.Main_OnCommand(40705, 0) -- Item: Set to random colors
+    end
+  end
+
+end
+
+
+-- INIT
+function Init()
+  -- See if there is items selected
+  count_sel_items = reaper.CountSelectedMediaItems(0)
+  if count_sel_items == 0 then return false end
+
+  reaper.PreventUIRefresh(1)
+
+  reaper.Undo_BeginBlock()
+
+  init_sel_items = SaveSelectedItems()
+
+  Main()
+
+  RestoreSelectedItems(init_sel_items)
+
+  reaper.Undo_EndBlock(undo_text, -1)
+
+  reaper.UpdateArrange()
+
+  reaper.PreventUIRefresh(-1)
+  
+end
+
+if not preset_file_init then
+  Init() 
+end
+
+
