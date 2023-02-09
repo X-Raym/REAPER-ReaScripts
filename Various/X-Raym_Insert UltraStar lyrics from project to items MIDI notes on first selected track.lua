5daec1c6c843e7278fc41f3cc8595a75fb66db1a
X-Raym <extremraym@yahoo.fr>
2023-02-09 15:27:19 +0100

    Update X-Raym_Insert UltraStar lyrics from project to items MIDI notes on first selected track.lua

--- Various/X-Raym_Insert UltraStar lyrics from project to items MIDI notes on first selected track.lua
+++ Various/X-Raym_Insert UltraStar lyrics from project to items MIDI notes on first selected track.lua
@@ -10,11 +10,16 @@
  * Forum Thread: Scripts: Creating Karaoke Songs for UltraStar and Vocaluxe with REAPER
  * Forum Thread URI: https://forum.cockos.com/showthread.php?t=202430
  * REAPER: 5.0
- * Version: 1.0.2
+ * Version: 1.0.3
 --]]
 
 --[[
  * Changelog:
+ * v1.0.3 (2023-02-09)
+  # replace SetTrackMIDILyrics with regular MIDI functions to avoid crash and be more precized
+  # add markers at lyrics break line, removing existing unamed markers (deactivable in user config area)
+ * v1.0.2 (2023-02-09)
+  # disable snap MIDI if activated
  * v1.0.2 (2023-02-09)
   + Fallback to Lyrics tracks is no tracks selected
  * v1.0.1 (2018-02-03)
@@ -22,6 +27,34 @@
  * v1.0 (2018-01-25)
   + Initial Release
 --]]
+
+-- USER CONFIG AREA ---------------------------------------
+
+console = true
+add_markers = true
+delete_unamed_markers = true
+
+------------------------------- END IOF USER CONFIG AREA --
+
+function Msg(val)
+  if console then
+    reaper.ShowConsoleMsg(tostring( val ) .. "\n" )
+  end
+end
+
+function DeleteAllUnamedMarkers()
+  local i=0
+  repeat
+    local iRetval, bIsrgnOut, iPosOut, iRgnendOut, sNameOut, iMarkrgnindexnumberOut, iColorOur = reaper.EnumProjectMarkers3(0,i)
+    if iRetval >= 1 then
+      if not bIsrgnOut and sNameOut == "" then
+        reaper.DeleteProjectMarkerByIndex( 0, i )
+        i = i - 1
+      end
+      i = i+1
+    end
+  until iRetval == 0
+end
 
 function GetSelectLyricsTrack()
   local count_tracks = reaper.CountTracks( 0 )
@@ -46,22 +79,47 @@ end
 function Main()
 
   local item_num = reaper.CountTrackMediaItems(track)
+  
+  snap = reaper.GetToggleCommandStateEx( 32060, 1014 ) -- View: Toggle snap to grid
+  if snap == 1 then
+    reaper.GetToggleCommandStateEx( 32060, 1014 ) -- View: Toggle snap to grid
+  end
+  
+  if delete_unamed_markers then DeleteAllUnamedMarkers() end
 
   -- ACTIONS
+  txt_evt_idx = 1
   for j = 0, item_num-1 do
     local item = reaper.GetTrackMediaItem(track, j)
     local take = reaper.GetActiveTake(item)
     if take and reaper.TakeIsMIDI( take ) then
 
       local retval, notes, ccs, sysex = reaper.MIDI_CountEvts(take)
+
+      -- Remove existing lyrics events
+      for k = sysex-1, 0, -1 do
+        local retval, selected, muted, ppqpos, msg_type, msg = reaper.MIDI_GetTextSysexEvt( take, k )
+        if msg_type == 5 then
+          reaper.MIDI_DeleteTextSysexEvt( take, k )
+        end
+      end
+      
       -- GET SELECTED NOTES (from 0 index)
       for k = 0, notes-1 do
         local retval, sel, muted, startppqposOut, endppqposOut, chan, pitch, vel = reaper.MIDI_GetNote(take, k)
-          table.insert(events, reaper.MIDI_GetProjTimeFromPPQPos( take, startppqposOut ) )
+        if add_markers and test[txt_evt_idx] and test[txt_evt_idx]:find("\n") then
+          reaper.AddProjectMarker( 0, false, reaper.MIDI_GetProjTimeFromPPQPos( take, startppqposOut-1 ), 0, "", -1 )
+        end
+        reaper.MIDI_InsertTextSysexEvt( take, sel, false, startppqposOut, 5, string.gsub(test[txt_evt_idx], "\n", " ") )
+        txt_evt_idx = txt_evt_idx + 1
+        if txt_evt_idx > #test then break end
+        --table.insert(events, reaper.MIDI_GetProjTimeFromPPQPos( take, startppqposOut ) )
       end
     end
+    if txt_evt_idx > #test then break end
   end -- ENFIF Take is MIDI
-
+  
+  --[[
   str = '' -- "1.1.2\tLyric\t2.1.1\tLyric"
   for i, pos in ipairs( events ) do
     lyric = "bla"
@@ -71,7 +129,11 @@ function Main()
     str = str .. reaper.format_timestr_pos( pos, '', 1 ) .. '\t' .. lyric ..'\t'
   end
   str = str:sub(1, -2)
-  reaper.SetTrackMIDILyrics( track, 2, str )
+  reaper.SetTrackMIDILyrics( track, 2, str )]]
+  
+  if snap == 1 then
+    reaper.GetToggleCommandStateEx( 32060, 1014 ) -- View: Toggle snap to grid
+  end
 
 end
 
@@ -109,4 +171,4 @@ reaper.UpdateArrange() -- Update the arrangement (often needed)
 
 reaper.PreventUIRefresh(-1)
 
-reaper.Undo_EndBlock("Insert UltraStar lyrics from project to items MIDI notes on first selected track", 0) -- End of the undo block. Leave it at the bottom of your main function.
+reaper.Undo_EndBlock("Insert UltraStar lyrics from project to items MIDI notes on first selected track", -1) -- End of the undo block. Leave it at the bottom of your main function.
