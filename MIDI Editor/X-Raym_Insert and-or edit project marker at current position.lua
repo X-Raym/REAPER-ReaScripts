2c52785d4c656249e07ed632ab9791002a9c1bc1
X-Raym <extremraym@yahoo.fr>
2023-02-12 18:32:55 +0100

    initial release

---
+++ MIDI Editor/X-Raym_Insert and-or edit project marker at current position.lua
@@ -0,0 +1,18 @@
+--[[
+ * ReaScript Name: Insert and-or edit project marker at current position
+ * Author: X-Raym
+ * Author URI: https://extremraym.com
+ * Repository: GitHub > X-Raym > REAPER-ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2023-02-12)
+  + Initial Release
+--]]
+
+reaper.Main_OnCommand( 40171, 0 ) -- Markers: Insert and/or edit marker at current position
---
+++ MIDI Editor/X-Raym_Remove project marker at current position.lua
@@ -0,0 +1,18 @@
+--[[
+ * ReaScript Name: Remove project marker at current position
+ * Author: X-Raym
+ * Author URI: https://extremraym.com
+ * Repository: GitHub > X-Raym > REAPER-ReaScripts
+ * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
+ * Licence: GPL v3
+ * REAPER: 5.0
+ * Version: 1.0
+--]]
+
+--[[
+ * Changelog:
+ * v1.0 (2023-02-12)
+  + Initial Release
+--]]
+
+reaper.Main_OnCommand( 40613, 0 ) -- Markers: Delete marker near cursor
