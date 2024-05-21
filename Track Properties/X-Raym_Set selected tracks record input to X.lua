28dee4d5b384028bc3f18ec4f01bec8e9970f8bd
X-Raym <X-Raym@users.noreply.github.com>
2024-05-21 10:44:32 +0200

    Update X-Raym_Set selected tracks record input to X.lua

--- Track Properties/X-Raym_Set selected tracks record input to X.lua
+++ Track Properties/X-Raym_Set selected tracks record input to X.lua
@@ -6,18 +6,30 @@
  * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.0
+ * Version: 1.0.1
  * MetaPackage: true
  * Provides:
  *   [main] . > X-Raym_Set selected tracks record input to audio mono 1.lua
  *   [main] . > X-Raym_Set selected tracks record input to audio mono 2.lua
  *   [main] . > X-Raym_Set selected tracks record input to audio mono 3.lua
  *   [main] . > X-Raym_Set selected tracks record input to audio mono 4.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio mono 5.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio mono 6.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio mono 7.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio mono 8.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio stereo 1-2.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio stereo 3-4.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio stereo 5-6.lua
+ *   [main] . > X-Raym_Set selected tracks record input to audio stereo 7-8.lua
  *   [main] . > X-Raym_Set selected tracks record input to all MIDI all channel.lua
 --]]
 
 --[[
  * Changelog:
+ * v1.0.1 (2024-05-21)
+  # Stereo pairs 1-2, 3-4, 5-6, 7-8
+  # All channels for the MIDI version
+  # 8 Mono versions
  * v1.0 (2024-02-06)
   + Initial release
 --]]
@@ -27,16 +39,24 @@
 -- Use Preset Script for safe moding or to create a new action with your own values
 -- https://github.com/X-Raym/REAPER-ReaScripts/tree/master/Templates/Script%20Preset
 
-force_input = 4096+(63<<5)+1 -- 4096+(63<<5)+1 for All MIDI All Channel Fallback
+input_fallback = 4096+(63<<5) -- 4096+(63<<5) for All MIDI All Channel Fallback
+force_script_name = nil -- Override script name. For debugging.
 
 -------------------------------------------------
 
 function Main()
-  script_name = ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
+  script_name = force_script_name or ({reaper.get_action_context()})[2]:match("([^/\\_]+)%.lua$")
   --script_name = "X-Raym_Set selected tracks record input to audio mono 1.lua"
   
-  set_track_input = tonumber( script_name:match("(%d+)") ) or force_input
-  set_track_input = math.max(set_track_input, 0) - 1 -- 0 based
+  input_num = script_name:match("(%d+)")
+  if input_num then
+    if script_name:find("stereo") then
+      offset = 1024
+    end
+    set_track_input = (offset or 0) + math.max( tonumber( input_num ), 0) - 1 -- 0 based
+  else
+    set_track_input = input_fallback
+  end
   
   for i = 0, count_sel_tracks - 1 do
     local track = reaper.GetSelectedTrack( 0, i )
