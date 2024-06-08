744eed16a8f552dce2d8fa3d29e6254be7984e6c
X-Raym <X-Raym@users.noreply.github.com>
2024-06-08 19:54:30 +0200

    update

--- Items Editing/X-Raym_Split selected items into X equal length parts.lua
+++ Items Editing/X-Raym_Split selected items into X equal length parts.lua
@@ -1,6 +1,6 @@
 --[[
- * ReaScript Name: Split selected items every X seconds intervals
- * Screenshot: https://i.imgur.com/OMsDvrG.gif
+ * ReaScript Name: Split selected items into X equal length parts
+ * Screenshot: https://i.imgur.com/nvqhIck.gif
  * Author: X-Raym
  * Author URI: http://www.extremraym.com
  * Repository: GitHub > X-Raym > REAPER-ReaScripts
@@ -9,16 +9,13 @@
  * Forum Thread: Lua Script: Split Media Into Into X Equal Parts
  * Forum Thread URI: http://forum.cockos.com/showthread.php?p=1609741#post1609741
  * REAPER: 5.0
- * Version: 2.0
+ * Version: 1.0
 --]]
 
 --[[
  * Changelog:
- * v2.0 (2024-06-08)
-  # New core
-  # Bug fix (last section could be longer than X)
- * v1.0 (2015-12-19)
-  + Initial Release
+ * v1.0 (2024-06-08)
+  + Initial release
 --]]
 
 -- USER CONFIG AREA ---------------------
@@ -26,19 +23,19 @@ console = true
 popup = true -- User input dialog box
 
 vars = vars or {}
-vars.val = 1
+vars.val = 2
 
 input_title = "Split items"
-undo_text = "Split selected items every X seconds intervals"
+undo_text = "Split selected items into X equal length parts"
 ----------------- END OF USER CONFIG AREA
 
 vars_order = {"val"}
-ext_name = "XR_SplitItemsXSeconds"
+ext_name = "XR_SplitItemsXparts"
 
 separator = "\n"
 
 instructions = {
-  "Value (s)? (x>0)",
+  "Parts (integer)? (x>1)",
   "separator=" .. separator,
 }
 
@@ -119,10 +116,13 @@ end
 function Main()
   for i, item in ipairs( init_sel_items) do
     table.insert( new_sel_items , item )
+    local item_pos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
+    local item_len = reaper.GetMediaItemInfo_Value( item, "D_LENGTH" )
+    local part_len = item_len / vars.val
     local i = 1
     repeat
       local item_pos = reaper.GetMediaItemInfo_Value( item, "D_POSITION" )
-      item = reaper.SplitMediaItem(item, item_pos + i * vars.val)
+      item = reaper.SplitMediaItem(item, item_pos + part_len * i)
       table.insert( new_sel_items , item )
     until item == nil
   end
@@ -142,7 +142,7 @@ function Init()
     if retval then
       vars = ParseRetvalCSV( retvals_csv )
       vars.val = tonumber(vars.val)
-      if vars.val and vars.val < 0 then return false end
+      if vars.val and vars.val <= 1 then return false end
     end
   end
 
@@ -153,18 +153,20 @@ function Init()
       reaper.Undo_BeginBlock() -- Begining of the undo block. Leave it at the top of your main function.
 
       reaper.ClearConsole()
+      
+      vars.val = math.floor( vars.val )
 
       if popup then
         SaveState()
       end
-
+      
       init_sel_items = {}
       SaveSelectedItems(init_sel_items)
-
+      
       new_sel_items = {}
 
       Main() -- Execute your main function
-
+      
       RestoreSelectedItems( new_sel_items )
 
       reaper.Undo_EndBlock(undo_text, -1) -- End of the undo block. Leave it at the bottom of your main function.
