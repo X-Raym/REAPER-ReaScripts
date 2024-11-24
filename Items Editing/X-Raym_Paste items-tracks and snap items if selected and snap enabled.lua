9fd4a6549171c73b17a98ffdad3400e8afc0ffe9
X-Raym <X-Raym@users.noreply.github.com>
2024-11-24 13:13:23 +0100

    Update X-Raym_Paste items-tracks and snap items if selected and snap enabled.lua

--- Items Editing/X-Raym_Paste items-tracks and snap items if selected and snap enabled.lua
+++ Items Editing/X-Raym_Paste items-tracks and snap items if selected and snap enabled.lua
@@ -9,20 +9,46 @@
  * Forum Thread: BUG: MIDI items not pasted correctly to grid
  * Forum Thread URI: https://forum.cockos.com/showthread.php?t=286926
  * REAPER: 7.0
- * Version: 1.0
+ * Version: 1.1
 --]]
 
 --[[
  * Changelog:
+ * v1.1 (2024-11-24)
+  # Only snap MIDI items
  * v1.0 (2024-11-20)
   + Initial Release
 --]]
+
+-- Save item selection
+function SaveSelectedItems(t)
+  local t = t or {}
+  for i = 0, reaper.CountSelectedMediaItems(0)-1 do
+    t[i+1] = reaper.GetSelectedMediaItem(0, i)
+  end
+  return t
+end
+
+function RestoreSelectedItems( items )
+  reaper.SelectAllMediaItems(0, false)
+  for i, item in ipairs( items ) do
+    reaper.SetMediaItemSelected( item, true )
+  end
+end
 
 reaper.Undo_BeginBlock()
 reaper.PreventUIRefresh(1)
 reaper.Main_OnCommand( 42398, 0 ) -- Item: Paste items/tracks
 if reaper.GetToggleCommandState(1157) and reaper.CountSelectedMediaItems(0) > 0 then
+  init_sel_items = SaveSelectedItems()
+  for i, item in ipairs( init_sel_items ) do
+    local take = reaper.GetActiveTake(item)
+    if not take or not reaper.TakeIsMIDI(take) then
+      reaper.SetMediaItemSelected(item, false)
+    end
+  end
   reaper.Main_OnCommand( reaper.NamedCommandLookup( "_SWS_QUANTITESTART2"), 0 ) -- SWS: Quantize item's start to grid (keep length)
+  RestoreSelectedItems( init_sel_items )
 end
 reaper.Undo_EndBlock( "Item: Paste items/tracks", -1 )
 reaper.PreventUIRefresh(1)
