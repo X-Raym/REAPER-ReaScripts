857155b6ef7e0257b9eb972459d1a9b1aa6c373d
X-Raym <X-Raym@users.noreply.github.com>
2026-06-24 11:40:05 +0200

    Update X-Raym_Play from mouse cursor position and solo track under mouse parent and select track.lua

--- Transport/X-Raym_Play from mouse cursor position and solo track under mouse parent and select track.lua
+++ Transport/X-Raym_Play from mouse cursor position and solo track under mouse parent and select track.lua
@@ -7,11 +7,13 @@
  * Repository URI: https://github.com/X-Raym/REAPER-ReaScripts
  * Licence: GPL v3
  * REAPER: 5.0
- * Version: 1.2.1
+ * Version: 1.3.1
 --]]
 
 --[[
  * Changelog:
+ * v1.3.0 (2026-06-23)
+  + Restore track solo after run
  * v1.2.1 (2026-03-21)
   # Use GetThingFromPoiunt to get track
  * v1.2 (2019-07-14)
@@ -19,15 +21,42 @@
   # no SWS dependency
 --]]
 
+stop_at_exit = true
+
+function SetButtonState( set )
+  local is_new_value, filename, sec, cmd, mode, resolution, val = reaper.get_action_context()
+  reaper.SetToggleCommandState( sec, cmd, set or 0 )
+  reaper.RefreshToolbar2( sec, cmd )
+end
+
+function Msg(a)
+  reaper.ShowConsoleMsg( tostring(a) )
+end
+
+function Exit()
+  if not tracks or #tracks == 0 then return end
+  reaper.PreventUIRefresh(1)
+  for i, entry in ipairs( tracks ) do
+    if reaper.ValidatePtr( entry.track, "MediaTrack*" ) then
+      reaper.SetMediaTrackInfo_Value( entry.track , "I_SOLO", entry.solo)
+    end
+  end
+  if stop_at_exit then
+    reaper.Main_OnCommand( 1016, 0 ) -- Transport: Stop
+  end
+  reaper.UpdateArrange()
+  reaper.PreventUIRefresh(-1)
+  SetButtonState()
+end
+
 function PlayFromMouse()
   local pos_init = reaper.GetCursorPosition()
   reaper.SetEditCurPos( pos, false, false )
   reaper.OnPlayButton()
   reaper.SetEditCurPos( pos_init, false, false )
-  reaper.PreventUIRefresh(-1)
 end
 
-function main()
+function Process()
   reaper.PreventUIRefresh(1)
 
   if reaper.GetThingFromPoint then
@@ -45,8 +74,11 @@ function main()
    parent = reaper.GetParentTrack( track )
    if not parent then parent = track end
    count_tracks = reaper.CountTracks()
+   tracks = {}
    for i = 0, count_tracks - 1 do
-  reaper.SetMediaTrackInfo_Value( reaper.GetTrack(0,i), "I_SOLO", 0)
+    local track = reaper.GetTrack(0,i)
+    table.insert( tracks, {track = track, solo = reaper.GetMediaTrackInfo_Value( track, "I_SOLO") } )
+    reaper.SetMediaTrackInfo_Value( track , "I_SOLO", 0)
    end
 
    reaper.SetMediaTrackInfo_Value( parent, "I_SOLO", 1)
@@ -56,9 +88,25 @@ function main()
   reaper.PreventUIRefresh(-1)
 end
 
-if not reaper.BR_TrackAtMouseCursor then
-  reaper.ShowMessageBox("Please install SWS extension", "Warning", 1)
-else
-  reaper.defer(main)
+function Main()
+  if not first_run then
+    Process()
+    first_run = true
+    SetButtonState(1)
+  end
+  
+  if reaper.GetPlayState() > 0 then
+    reaper.defer(Main)
+  end
+  
 end
+
+if not reaper.BR_TrackAtMouseCursor and not GetThingFromPoint then
+  return reaper.ShowMessageBox("Please install SWS extension", "Warning", 1)
+end
+
+reaper.set_action_options( 1 )
+reaper.defer(Main)
+reaper.atexit(Exit)
+
 
